b7c264518bfd9b7a918417d3549fab006473a344
Andrew Larned <andrew.larned@gmail.com>
2015-08-05 07:24:23 -0400

    DB cleanup; notification update; main README updated

--- create-database.sql
+++ create-database.sql
@@ -6,6 +6,4 @@ CREATE DATABASE openchpl
   WITH OWNER = openchpl
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'English_United States.1252'
-       LC_CTYPE = 'English_United States.1252'
        CONNECTION LIMIT = -1;
