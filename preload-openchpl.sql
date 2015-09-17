0d042d1901165c3092d972d003ca93bc4f033a78
Andrew Larned <andrew.larned@gmail.com>
2015-09-17 08:27:45 -0400

    Added 'decertified' as a CP status

--- preload-openchpl.sql
+++ preload-openchpl.sql
@@ -5,7 +5,7 @@ insert into openchpl.cqm_criterion_type (name, description, last_modified_user) 
 insert into openchpl.certification_body (name, last_modified_user) values ('InfoGard', -1), ('CCHIT', -1), ('Drummond Group Inc.', -1), ('SLI Global', -1), ('Surescripts LLC', -1), ('ICSA Labs', -1), ('Pending', -1);
 insert into openchpl.event_type (name, description, last_modified_user) values ('Certification','Product is certified', -1);
 insert into openchpl.cqm_version (version, last_modified_user) values ('v0', -1), ('v1', -1), ('v2', -1), ('v3', -1), ('v4', -1);
-insert into openchpl.certification_status (certification_status, last_modified_user) values ('Active', -1), ('Retired', -1), ('Withdrawn', -1);
+insert into openchpl.certification_status (certification_status, last_modified_user) values ('Active', -1), ('Retired', -1), ('Withdrawn', -1), ('Decertified', -1);
 
 INSERT INTO openchpl.certification_criterion (certification_edition_id, number, title, last_modified_user) VALUES
 (1, '170.302(a)', 'Drug-drug, drug-allergy', -1),
