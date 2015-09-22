f17ea7a506626265b7d0c9642f3b5548c7ffe088
Andrew Larned <andrew.larned@gmail.com>
2015-09-22 12:30:49 -0400

    ocd-171: Update reset-test-only to correctly add pending product tables

--- reset-test-only.sh
+++ reset-test-only.sh
@@ -3,10 +3,10 @@
 
 psql -Upostgres -f drop-openchpl.sql openchpl_test
 psql -Upostgres -f openchpl.sql openchpl_test
+psql -Upostgres -f openchpl_certified_product_pending.sql openchpl_test
 psql -Upostgres -f audit-openchpl.sql openchpl_test
 psql -Upostgres -f preload-openchpl.sql openchpl_test
 psql -Upostgres -f preload-user_contact_acl.sql openchpl_test
 psql -Upostgres -f openchpl_create_view_cert.sql openchpl_test
 psql -Upostgres -f openchpl_create_view_cqm.sql openchpl_test
 psql -Upostgres -f openchpl_create_view_search.sql openchpl_test
-psql -Upostgres -f openchpl_certified_product_pending.sql openchpl_test
