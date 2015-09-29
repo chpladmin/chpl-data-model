965b694ab100c8c9a49be9618b16ec26b7d99a76
chris-watson <cxwatson@gmail.com>
2015-09-29 17:03:34 -0400

    OCD-194: added openchpl_activity.sql

---
+++ openchpl_activity.sql
@@ -0,0 +1,35 @@
+CREATE TABLE openchpl.activity
+(
+   activity_id bigserial NOT NULL, 
+   description character varying(1000), 
+   activity_date timestamp without time zone NOT NULL DEFAULT now(), 
+   activity_object_id bigint NOT NULL, 
+   activity_object_class_id bigint NOT NULL, 
+   creation_date timestamp without time zone NOT NULL DEFAULT now(), 
+   last_modified_date timestamp without time zone NOT NULL DEFAULT now(), 
+   last_modified_user bigint NOT NULL, 
+   deleted boolean NOT NULL DEFAULT false, 
+   CONSTRAINT activity_id_pk PRIMARY KEY (activity_id)
+) 
+WITH (
+  OIDS = FALSE
+)
+;
+ALTER TABLE openchpl.activity
+  OWNER TO openchpl;
+
+
+
+CREATE TABLE openchpl.activity_class
+(
+   activity_class_id bigserial NOT NULL, 
+   class character varying, 
+   CONSTRAINT activity_class_id_pk PRIMARY KEY (activity_class_id)
+) 
+WITH (
+  OIDS = FALSE
+)
+;
+ALTER TABLE openchpl.activity_class
+  OWNER TO openchpl;
+
