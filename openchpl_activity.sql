3445dc1f7448ae096766acbea4cff597e07614c9
chris-watson <cxwatson@gmail.com>
2015-09-30 15:29:51 -0400

    (OCD-194) Work on Activity and ActivityClass objects

--- openchpl_activity.sql
+++ openchpl_activity.sql
@@ -19,19 +19,24 @@ ALTER TABLE openchpl.activity
   OWNER TO openchpl;
 
 
-
+  
 CREATE TABLE openchpl.activity_class
 (
-   activity_class_id bigserial NOT NULL, 
-   class character varying, 
-   CONSTRAINT activity_class_id_pk PRIMARY KEY (activity_class_id)
-) 
-WITH (
-  OIDS = FALSE
+  activity_class_id bigserial NOT NULL,
+  class character varying,
+  creation_date timestamp without time zone NOT NULL DEFAULT now(),
+  last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+  last_modified_user bigint NOT NULL,
+  deleted boolean NOT NULL DEFAULT false,
+  CONSTRAINT activity_class_id_pk PRIMARY KEY (activity_class_id)
 )
-;
+WITH (
+  OIDS=FALSE
+);
 ALTER TABLE openchpl.activity_class
   OWNER TO openchpl;
 
+
   
+ 
 ALTER TABLE openchpl.activity ADD CONSTRAINT activity_object_class_fk FOREIGN KEY (activity_object_class_id) REFERENCES openchpl.activity_class (activity_class_id);
