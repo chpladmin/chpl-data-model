6e9647d6a37af4c637ca29dc2d4e1fe481057327
katy.ekey <kekey@ainq.com>
2016-04-05 11:22:44 -0400

    development: OCD-648: whoops did not mean to commit these files

--- openchpl_api-key.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO openchpl.api_key (api_key, email, name_organization, last_modified_user) values ('12909a978483dfb8ecd0596c98ae9094', 'no-reply@ainq.com', 'Audacious Inquiry', -1);
-INSERT INTO openchpl.api_key (api_key, email, name_organization, last_modified_user) values ('88f231cbf2ae45810b1177f5f4ddf297', 'cwatson@ainq.com', 'Ai', -1);
