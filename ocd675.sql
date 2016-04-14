4f92b6b67de87952110cfcf185b3fb68ad4a1b41
katy.ekey <kekey@ainq.com>
2016-04-14 10:55:19 -0400

    development: add openchpl. to sql

--- ocd675.sql
+++ ocd675.sql
@@ -3,19 +3,19 @@
 
 --add ACB ACLs
 INSERT INTO openchpl.acl_entry (acl_object_identity, ace_order, sid, mask, granting, audit_success, audit_failure) VALUES
-(2, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =2), -2, 16, true, false, false),
-(3, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =3), -2, 16, true, false, false),
-(4, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =4), -2, 16, true, false, false),
-(5, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =5), -2, 16, true, false, false),
-(6, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =6), -2, 16, true, false, false),
-(7, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =7), -2, 16, true, false, false),
-(8, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =8), -2, 16, true, false, false);
+(2, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =2), -2, 16, true, false, false),
+(3, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =3), -2, 16, true, false, false),
+(4, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =4), -2, 16, true, false, false),
+(5, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =5), -2, 16, true, false, false),
+(6, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =6), -2, 16, true, false, false),
+(7, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =7), -2, 16, true, false, false),
+(8, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =8), -2, 16, true, false, false);
 
 -- add ATL ACLs
 INSERT INTO openchpl.acl_entry (acl_object_identity, ace_order, sid, mask, granting, audit_success, audit_failure) VALUES
-(9, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =9), -2, 16, true, false, false),
-(10, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =10), -2, 16, true, false, false),
-(11, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =11), -2, 16, true, false, false),
-(12, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =12), -2, 16, true, false, false),
-(13, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =13), -2, 16, true, false, false),
-(14, (select coalesce(max(ace_order), -1)+1 from acl_entry where acl_object_identity =14), -2, 16, true, false, false);
+(9, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =9), -2, 16, true, false, false),
+(10, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =10), -2, 16, true, false, false),
+(11, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =11), -2, 16, true, false, false),
+(12, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =12), -2, 16, true, false, false),
+(13, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =13), -2, 16, true, false, false),
+(14, (select coalesce(max(ace_order), -1)+1 from openchpl.acl_entry where acl_object_identity =14), -2, 16, true, false, false);
