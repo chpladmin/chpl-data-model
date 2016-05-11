b5b4f1dbffc17a3b07510c90e2c5a26713de2aa1
Andrew Larned <andrew.larned@gmail.com>
2016-05-11 10:36:09 -0400

    OCD-709: Fix space issue with functionality tested

---
+++ ocd-709.2.sql
@@ -0,0 +1,6 @@
+update openchpl.test_functionality
+set number = '(b)(5)(i)(E)'
+where number = '(b)(5)(i)(E) ';
+update openchpl.test_functionality
+set number = '(b)(6)(ii)(E)'
+where number = '(b)(6)(ii)(E) ';
