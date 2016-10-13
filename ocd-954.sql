79fdd6e65943e15eaa08bfba57e7beaa3aa3e42c
Daniel Lucas <daniel.r.lucas@gmail.com>
2016-10-13 17:15:03 -0400

    OCD-954: added SQL to insert ONC Staff role

---
+++ ocd-954.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO openchpl.user_permission(
+            user_permission_id, name, description, authority, creation_date, 
+            last_modified_date, last_modified_user, deleted)
+    SELECT 7, 'ONC STAFF', 
+    'This permission gives a user access to the CMS Download file and report navigation section. It denies editing of Users/Products/CPs/ACBs/etc. No user invitation ability.', 
+    'ROLE ONC STAFF', localtimestamp, localtimestamp, -1, FALSE
+	WHERE NOT EXISTS 
+	(SELECT user_permission_id 
+	FROM openchpl.user_permission 
+	WHERE user_permission_id = 7);
