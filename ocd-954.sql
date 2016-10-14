d949a5a73f759f8cfcb5a63299f126449fc45cf0
Andrew Larned <andrew.larned@gmail.com>
2016-10-14 09:52:04 -0400

    OCD-954: Converting dos file to unix file for EOL markers

--- ocd-954.sql
+++ ocd-954.sql
@@ -1,4 +1,4 @@
-﻿-- Add ONC Staff role to user_permission table to support API ROLE_ONC_STAFF
+-- Add ONC Staff role to user_permission table to support API ROLE_ONC_STAFF
 INSERT INTO openchpl.user_permission(user_permission_id, "name", description, authority, last_modified_user)
     SELECT 7, 'ONC_STAFF' ,'This permission gives a user access to the CMS Download file and report navigation section. It denies editing of Users/Products/CPs/ACBs/etc. No user invitation ability.', 'ROLE_ONC_STAFF' , -1
 	WHERE NOT EXISTS 
