21bdf797674381cde5597ce1a077802422817b45
Andrew Larned <andrew.larned@gmail.com>
2017-03-02 16:01:49 -0500

    OCD-1281: Database instructions

--- maint/dump.sh
+++ maint/dump.sh
@@ -2,10 +2,10 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # this comment is required to trick cygwin into dealing with windows vs. linux EOL characters
 
 if [ $# -ne 2 ]; then
-    pg_dump --host localhost --username openchpl --no-password --format custom --blobs --verbose --file openchpl.backup openchpl
+    host=localhost
+    user=openchpl_dev
 else
     host=$1
     user=$2
-
-    pg_dump --host $host --username $user --no-password --format custom --blobs --verbose --file openchpl.backup openchpl
 fi
+pg_dump --host $host --username $user --no-password --format custom --blobs --verbose --file openchpl.backup openchpl
