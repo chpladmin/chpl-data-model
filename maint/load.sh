14855f2553743f433bb605b2896c4f3160fcdc44
Andrew Larned <andrew.larned@gmail.com>
2017-03-02 10:22:04 -0500

    OCD-1282: added script for disabling pre-existing users

--- maint/load.sh
+++ maint/load.sh
@@ -2,13 +2,12 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # this comment is required to trick cygwin into dealing with windows vs. linux EOL characters
 
 if [ $# -ne 2 ]; then
-    psql --host localhost --username openchpl_dev --no-password -c "drop schema if exists audit cascade;" openchpl
-    psql --host localhost --username openchpl_dev --no-password -c "drop schema if exists openchpl cascade;" openchpl
-    pg_restore --host localhost --username openchpl_dev --no-password --verbose --clean --if-exists --dbname openchpl openchpl.backup
-    psql --host localhost --username openchpl_dev --no-password -f users.sql openchpl
+    host=localhost
+    user=openchpl_dev
 else
     host=$1
     user=$2
-
-    pg_restore --host $host --username $user --no-password --verbose --clean --dbname openchpl openchpl.backup
 fi
+psql --host $host --username $user --no-password -c "drop schema if exists audit cascade;" openchpl
+psql --host $host --username $user --no-password -c "drop schema if exists openchpl cascade;" openchpl
+pg_restore --host $host --username $user --no-password --verbose --clean --if-exists --dbname openchpl openchpl.backup
