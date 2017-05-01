c849b620eb6e86f050ccc3bb747b7e95d03a7a4d
Katy Ekey <kekey@ainq.com>
2017-05-01 08:45:55 -0400

    OCD-1448: grant permissions to openchpl rather than ownership

--- v-next.sql
+++ v-next.sql
@@ -118,7 +118,6 @@ CREATE TABLE openchpl.notification_type(
 	deleted bool NOT NULL DEFAULT false,
 	CONSTRAINT notification_type_pk PRIMARY KEY (id)
 );
-ALTER TABLE openchpl.notification_type OWNER TO openchpl;
 
 INSERT INTO openchpl.notification_type (name, description, last_modified_user)
 VALUES ('ACB Daily Surveillance Broken Rules', 'A daily email of surveillance rules that have been broken within the last day for listings certified by a specific ACB.', -1), 
@@ -146,7 +145,6 @@ CREATE TABLE openchpl.notification_type_permission(
       REFERENCES openchpl.user_permission (user_permission_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE RESTRICT
 );
-ALTER TABLE openchpl.notification_type_permission OWNER TO openchpl;
 
 INSERT INTO openchpl.notification_type_permission (notification_type_id, permission_id, last_modified_user)
 SELECT id, -2, -1 FROM openchpl.notification_type WHERE name = 'ACB Daily Surveillance Broken Rules';
@@ -176,7 +174,6 @@ CREATE TABLE openchpl.notification_recipient(
 	deleted bool NOT NULL DEFAULT false,
 	CONSTRAINT notification_recipient_pk PRIMARY KEY (id)
 );
-ALTER TABLE openchpl.notification_recipient OWNER TO openchpl;
 
 CREATE TRIGGER notification_recipient_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.notification_recipient FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER notification_recipient_timestamp BEFORE UPDATE on openchpl.notification_recipient FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
@@ -206,7 +203,12 @@ CREATE TABLE openchpl.notification_type_recipient_map(
       ON UPDATE NO ACTION ON DELETE RESTRICT
 );
 
-ALTER TABLE openchpl.notification_type_recipient_map OWNER TO openchpl;
-
 CREATE TRIGGER notification_type_recipient_map_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.notification_type_recipient_map FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER notification_type_recipient_map_timestamp BEFORE UPDATE on openchpl.notification_type_recipient_map FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
+
+GRANT ALL ON ALL TABLES IN SCHEMA openchpl TO openchpl;
+GRANT ALL ON ALL TABLES IN SCHEMA audit TO openchpl;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA openchpl TO openchpl;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA audit TO openchpl;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA openchpl TO openchpl;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA audit TO openchpl;
