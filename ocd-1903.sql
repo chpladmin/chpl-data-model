7eb26ae32a43477ecc20cf715b4a95a2dca27b93
Brian Lindsey <brianl9864@gmail.com>
2017-11-20 17:39:41 -0500

    OCD-1903 - create ocd-1903 file

--- ocd-1903.sql
+++ ocd-1903.sql
@@ -3,22 +3,16 @@ SET user_permission_id_user_permission = 2
 WHERE user_id = 22;
 
 DELETE FROM openchpl.global_user_permission_map as gupm
-WHERE gupm.user_id = 5 AND user_permission_id_user_permission = 3;
+WHERE user_permission_id_user_permission = 3;
 
 DELETE FROM openchpl.global_user_permission_map as gupm
-WHERE gupm.user_id = 13 AND user_permission_id_user_permission = 3;
+WHERE user_permission_id_user_permission = 5;
 
-DELETE FROM openchpl.global_user_permission_map as gupm
-WHERE gupm.user_id = 16 AND user_permission_id_user_permission = 3;
+DELETE FROM openchpl.invited_user_permission as iup
+WHERE iup.user_permission_id = 3;
 
-DELETE FROM openchpl.global_user_permission_map as gupm
-WHERE gupm.user_id = 14 AND user_permission_id_user_permission = 3;
-
-DELETE FROM openchpl.global_user_permission_map as gupm
-WHERE gupm.user_id = 17 AND user_permission_id_user_permission = 3;
-
-DELETE FROM openchpl.global_user_permission_map as gupm
-WHERE gupm.user_id = 18 AND user_permission_id_user_permission = 3;
+DELETE FROM openchpl.invited_user_permission as iup
+WHERE iup.user_permission_id = 5;
 
 DELETE FROM openchpl.user_permission as up
 WHERE up.user_permission_id = 5;
