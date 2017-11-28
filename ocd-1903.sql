462591577221910104da667985db02375c48b86e
Brian Lindsey <brianl9864@gmail.com>
2017-11-28 15:29:36 -0500

    OCD-1903 - take out global_user_permission_map updating

--- ocd-1903.sql
+++ ocd-1903.sql
@@ -1,36 +1,10 @@
-UPDATE openchpl.global_user_permission_map as gupm
-SET user_permission_id_user_permission = 2
-WHERE user_permission_id_user_permission = 3
-AND NOT EXISTS (SELECT user_id FROM openchpl.global_user_permission_map WHERE (user_id, 2) IN (SELECT user_id, user_permission_id_user_permission FROM openchpl.global_user_permission_map));
-
-UPDATE openchpl.global_user_permission_map as gupm
-SET user_permission_id_user_permission = 4
-WHERE user_permission_id_user_permission = 5
-AND NOT EXISTS (SELECT user_id FROM openchpl.global_user_permission_map WHERE (user_id, 4) IN (SELECT user_id, user_permission_id_user_permission FROM openchpl.global_user_permission_map));
-
-UPDATE openchpl.global_user_permission_map as gupm
-SET user_permission_id_user_permission = 2
-WHERE user_permission_id_user_permission = 3;
-
-UPDATE openchpl.global_user_permission_map as gupm
-SET user_permission_id_user_permission = 4
-WHERE user_permission_id_user_permission = 5;
-
-DELETE FROM openchpl.global_user_permission_map as gupm
-WHERE user_permission_id_user_permission = 3;
-
-DELETE FROM openchpl.global_user_permission_map as gupm
-WHERE user_permission_id_user_permission = 5;
-
 UPDATE openchpl.invited_user_permission 
 SET user_permission_id = 2
-WHERE user_permission_id = 3
-AND NOT EXISTS (SELECT user_permission_id FROM openchpl.invited_user_permission WHERE (invited_user_id, 2) IN (SELECT invited_user_id, user_permission_id FROM openchpl.invited_user_permission));
+WHERE user_permission_id = 3;
 
 UPDATE openchpl.invited_user_permission 
 SET user_permission_id = 4
-WHERE user_permission_id = 5
-AND NOT EXISTS (SELECT user_permission_id FROM openchpl.invited_user_permission WHERE (invited_user_id, 4) IN (SELECT invited_user_id, user_permission_id FROM openchpl.invited_user_permission));
+WHERE user_permission_id = 5;
 
 DELETE FROM openchpl.invited_user_permission as up
 WHERE up.user_permission_id = 5;
