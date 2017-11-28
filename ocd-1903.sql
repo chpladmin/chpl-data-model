9ec4ebd2b59ea4929404eb6cc2ef650c47ec08ae
Brian Lindsey <brianl9864@gmail.com>
2017-11-28 09:47:20 -0500

    OCD-1903 - change sql script

--- ocd-1903.sql
+++ ocd-1903.sql
@@ -1,26 +1,34 @@
 UPDATE openchpl.global_user_permission_map as gupm
 SET user_permission_id_user_permission = 2
 WHERE user_permission_id_user_permission = 3
-AND (SELECT count(user_id) FROM openchpl.global_user_permission_map WHERE count(user_id) = 1);
+AND NOT EXISTS (SELECT user_id FROM openchpl.global_user_permission_map WHERE (user_id, 2) IN (SELECT user_id, user_permission_id_user_permission FROM openchpl.global_user_permission_map));
 
 UPDATE openchpl.global_user_permission_map as gupm
 SET user_permission_id_user_permission = 4
 WHERE user_permission_id_user_permission = 5
-AND (SELECT count(user_id) FROM openchpl.global_user_permission_map WHERE count(user_id) = 1);
+AND NOT EXISTS (SELECT user_id FROM openchpl.global_user_permission_map WHERE (user_id, 4) IN (SELECT user_id, user_permission_id_user_permission FROM openchpl.global_user_permission_map));
 
-DELETE openchpl.global_user_permission_map as gupm
+DELETE FROM openchpl.global_user_permission_map as gupm
 WHERE user_permission_id_user_permission = 3;
 
-DELETE openchpl.global_user_permission_map as gupm
+DELETE FROM openchpl.global_user_permission_map as gupm
 WHERE user_permission_id_user_permission = 5;
 
 UPDATE openchpl.invited_user_permission 
 SET user_permission_id = 2
-WHERE user_permission_id = 3;
+WHERE user_permission_id = 3
+AND NOT EXISTS (SELECT user_permission_id FROM openchpl.invited_user_permission WHERE (invited_user_id, 2) IN (SELECT invited_user_id, user_permission_id FROM openchpl.invited_user_permission));
 
 UPDATE openchpl.invited_user_permission 
 SET user_permission_id = 4
-WHERE user_permission_id = 5;
+WHERE user_permission_id = 5
+AND NOT EXISTS (SELECT user_permission_id FROM openchpl.invited_user_permission WHERE (invited_user_id, 4) IN (SELECT invited_user_id, user_permission_id FROM openchpl.invited_user_permission));
+
+DELETE FROM openchpl.invited_user_permission as up
+WHERE up.user_permission_id = 5;
+
+DELETE FROM openchpl.invited_user_permission as up
+WHERE up.user_permission_id = 3;
 
 DELETE FROM openchpl.user_permission as up
 WHERE up.user_permission_id = 5;
@@ -43,4 +51,3 @@ WHERE up.user_permission_id = 4;
 UPDATE openchpl.user_permission as up
 SET authority = 'ROLE_ATL'
 WHERE up.user_permission_id = 4;
-
