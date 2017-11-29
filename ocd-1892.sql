f06916d76a89f763b3fbeb82ae171d4fc0b0f528
Katy Ekey <kekey@ainq.com>
2017-11-29 09:01:20 -0500

    OCD-1892: fix logic so the script will run multiple times without error

--- ocd-1892.sql
+++ ocd-1892.sql
@@ -9,9 +9,10 @@ CREATE OR REPLACE FUNCTION openchpl.cleanupPendingTestProcedureTemp() RETURNS vo
 BEGIN
 	--if pending_certification_result_test_procedure_temp still exists then drop pending_certification_result_test_procedure
     PERFORM * FROM openchpl.pending_certification_result_test_procedure_temp;
+	DROP TABLE IF EXISTS openchpl.pending_certification_result_test_procedure;
     EXCEPTION
         WHEN UNDEFINED_TABLE THEN
-			DROP TABLE IF EXISTS openchpl.pending_certification_result_test_procedure;
+		RAISE NOTICE 'pending_certification_result_test_procedure_temp has already been removed. Not removing pending_certification_result_test_procedure';
 END;
 $$ LANGUAGE plpgsql;
 
@@ -19,9 +20,10 @@ CREATE OR REPLACE FUNCTION openchpl.cleanupCertTestProcedureTemp() RETURNS void 
 BEGIN			
 	--if certification_result_test_procedure_temp still exists then drop certification_result_test_procedure
 	PERFORM * FROM openchpl.certification_result_test_procedure_temp;
+	DROP TABLE IF EXISTS openchpl.certification_result_test_procedure;
     EXCEPTION
         WHEN UNDEFINED_TABLE THEN
-			DROP TABLE IF EXISTS openchpl.certification_result_test_procedure;
+		RAISE NOTICE 'certification_result_test_procedure_temp has already been removed. Not removing certification_result_test_procedure';
 END;
 $$ LANGUAGE plpgsql;
 
@@ -29,9 +31,10 @@ CREATE OR REPLACE FUNCTION openchpl.cleanupTestProcedureTemp() RETURNS void AS $
 BEGIN				
 	--if test_procedure_temp still exists then drop test_procedure
 	PERFORM * FROM openchpl.test_procedure_temp;
+	DROP TABLE IF EXISTS openchpl.test_procedure;
     EXCEPTION
         WHEN UNDEFINED_TABLE THEN
-			DROP TABLE IF EXISTS openchpl.test_procedure;
+		RAISE NOTICE 'test_procedure_temp has already been removed. Not removing test_procedure';
 END;
 $$ LANGUAGE plpgsql;
 
@@ -54,42 +57,51 @@ DROP TRIGGER IF EXISTS certification_result_test_procedure_temp_timestamp ON ope
 DROP TRIGGER IF EXISTS pending_certification_result_test_procedure_temp_audit ON openchpl.pending_certification_result_test_procedure_temp;
 DROP TRIGGER IF EXISTS pending_certification_result_test_procedure_temp_timestamp ON openchpl.pending_certification_result_test_procedure_temp;
 
-ALTER INDEX openchpl.test_procedure_temp_pk RENAME TO test_procedure_pk;
-ALTER TABLE openchpl.test_procedure_temp RENAME TO test_procedure;
+ALTER INDEX IF EXISTS openchpl.test_procedure_temp_pk RENAME TO test_procedure_pk;
+ALTER TABLE IF EXISTS openchpl.test_procedure_temp RENAME TO test_procedure;
 
-ALTER INDEX openchpl.test_procedure_criteria_map_temp_pk RENAME TO test_procedure_criteria_map_pk;
-ALTER TABLE openchpl.test_procedure_criteria_map_temp DROP CONSTRAINT IF EXISTS test_procedure_temp_fk;
-ALTER TABLE openchpl.test_procedure_criteria_map_temp DROP CONSTRAINT IF EXISTS test_procedure_fk;
-ALTER TABLE openchpl.test_procedure_criteria_map_temp ADD CONSTRAINT 
+ALTER INDEX IF EXISTS openchpl.test_procedure_criteria_map_temp_pk RENAME TO test_procedure_criteria_map_pk;
+ALTER TABLE IF EXISTS openchpl.test_procedure_criteria_map_temp DROP CONSTRAINT IF EXISTS test_procedure_temp_fk;
+ALTER TABLE IF EXISTS openchpl.test_procedure_criteria_map_temp DROP CONSTRAINT IF EXISTS test_procedure_fk;
+ALTER TABLE IF EXISTS openchpl.test_procedure_criteria_map_temp ADD CONSTRAINT 
 	test_procedure_fk FOREIGN KEY (test_procedure_id) 
 	REFERENCES openchpl.test_procedure (id) 
 	MATCH FULL ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE openchpl.test_procedure_criteria_map_temp RENAME TO test_procedure_criteria_map;
+ALTER TABLE IF EXISTS openchpl.test_procedure_criteria_map_temp RENAME TO test_procedure_criteria_map;
 
 
-ALTER INDEX openchpl.certification_result_test_procedure_temp_pk RENAME TO certification_result_test_procedure_pk;
-ALTER TABLE openchpl.certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS test_procedure_temp_fk;
-ALTER TABLE openchpl.certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS test_procedure_fk;
-ALTER TABLE openchpl.certification_result_test_procedure_temp ADD CONSTRAINT 
+ALTER INDEX IF EXISTS openchpl.certification_result_test_procedure_temp_pk RENAME TO certification_result_test_procedure_pk;
+ALTER TABLE IF EXISTS openchpl.certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS test_procedure_temp_fk;
+ALTER TABLE IF EXISTS openchpl.certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS test_procedure_fk;
+ALTER TABLE IF EXISTS openchpl.certification_result_test_procedure_temp ADD CONSTRAINT 
 	test_procedure_fk FOREIGN KEY (test_procedure_id) 
 	REFERENCES openchpl.test_procedure (id) 
 	MATCH FULL ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE openchpl.certification_result_test_procedure_temp RENAME TO certification_result_test_procedure;
+ALTER TABLE IF EXISTS openchpl.certification_result_test_procedure_temp RENAME TO certification_result_test_procedure;
 
-ALTER INDEX openchpl.pending_certification_result_test_procedure_temp_pk RENAME TO pending_certification_result_test_procedure_pk;
-ALTER TABLE openchpl.pending_certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS pending_certification_result_temp_fk;
-ALTER TABLE openchpl.pending_certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS pending_certification_result_fk;
-ALTER TABLE openchpl.pending_certification_result_test_procedure_temp ADD CONSTRAINT
+ALTER INDEX IF EXISTS openchpl.pending_certification_result_test_procedure_temp_pk RENAME TO pending_certification_result_test_procedure_pk;
+ALTER TABLE IF EXISTS openchpl.pending_certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS pending_certification_result_temp_fk;
+ALTER TABLE IF EXISTS openchpl.pending_certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS pending_certification_result_fk;
+ALTER TABLE IF EXISTS openchpl.pending_certification_result_test_procedure_temp ADD CONSTRAINT
 	pending_certification_result_fk FOREIGN KEY (pending_certification_result_id)
     REFERENCES openchpl.pending_certification_result (pending_certification_result_id) MATCH FULL
     ON UPDATE CASCADE ON DELETE RESTRICT;
-ALTER TABLE openchpl.pending_certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS test_procedure_temp_fk;
-ALTER TABLE openchpl.pending_certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS test_procedure_fk;
-ALTER TABLE openchpl.pending_certification_result_test_procedure_temp ADD CONSTRAINT
+ALTER TABLE IF EXISTS openchpl.pending_certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS test_procedure_temp_fk;
+ALTER TABLE IF EXISTS openchpl.pending_certification_result_test_procedure_temp DROP CONSTRAINT IF EXISTS test_procedure_fk;
+ALTER TABLE IF EXISTS openchpl.pending_certification_result_test_procedure_temp ADD CONSTRAINT
 	test_procedure_fk FOREIGN KEY (test_procedure_id)
 	REFERENCES openchpl.test_procedure (id) MATCH FULL
 	ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE openchpl.pending_certification_result_test_procedure_temp RENAME TO pending_certification_result_test_procedure;
+ALTER TABLE IF EXISTS openchpl.pending_certification_result_test_procedure_temp RENAME TO pending_certification_result_test_procedure;
+
+DROP TRIGGER IF EXISTS test_procedure_audit ON openchpl.test_procedure;
+DROP TRIGGER IF EXISTS test_procedure_timestamp ON openchpl.test_procedure;
+DROP TRIGGER IF EXISTS test_procedure_criteria_map_audit ON openchpl.test_procedure_criteria_map;
+DROP TRIGGER IF EXISTS test_procedure_criteria_map_timestamp ON openchpl.test_procedure_criteria_map;
+DROP TRIGGER IF EXISTS certification_result_test_procedure_audit ON openchpl.certification_result_test_procedure;
+DROP TRIGGER IF EXISTS certification_result_test_procedure_timestamp ON openchpl.certification_result_test_procedure;
+DROP TRIGGER IF EXISTS pending_certification_result_test_procedure_audit ON openchpl.pending_certification_result_test_procedure;
+DROP TRIGGER IF EXISTS pending_certification_result_test_procedure_timestamp ON openchpl.pending_certification_result_test_procedure;
 
 CREATE TRIGGER test_procedure_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.test_procedure FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER test_procedure_timestamp BEFORE UPDATE on openchpl.test_procedure FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();	
