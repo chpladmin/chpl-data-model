5faddfbd40a081d91fe7670f70f3a71059bb3d23
Brian Lindsey <brianl9864@gmail.com>
2017-11-30 10:09:15 -0500

    development - update v-next remove ocd-1903 script

--- v-next.sql
+++ v-next.sql
@@ -1,3 +1,39 @@
+UPDATE openchpl.invited_user_permission 
+SET user_permission_id = 2
+WHERE user_permission_id = 3;
+
+UPDATE openchpl.invited_user_permission 
+SET user_permission_id = 4
+WHERE user_permission_id = 5;
+
+DELETE FROM openchpl.invited_user_permission as up
+WHERE up.user_permission_id = 5;
+
+DELETE FROM openchpl.invited_user_permission as up
+WHERE up.user_permission_id = 3;
+
+DELETE FROM openchpl.user_permission as up
+WHERE up.user_permission_id = 5;
+
+DELETE FROM openchpl.user_permission as up
+WHERE up.user_permission_id = 3;
+
+UPDATE openchpl.user_permission as up
+SET name = 'ACB'
+WHERE up.user_permission_id = 2;
+
+UPDATE openchpl.user_permission as up
+SET authority = 'ROLE_ACB'
+WHERE up.user_permission_id = 2;
+
+UPDATE openchpl.user_permission as up
+SET name = 'ATL'
+WHERE up.user_permission_id = 4;
+
+UPDATE openchpl.user_permission as up
+SET authority = 'ROLE_ATL'
+WHERE up.user_permission_id = 4;
+
 DROP TABLE IF EXISTS openchpl.pending_certification_result_test_procedure_temp;
 DROP TABLE IF EXISTS openchpl.certification_result_test_procedure_temp;
 DROP TABLE IF EXISTS openchpl.test_procedure_criteria_map_temp;
