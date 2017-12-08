ac5fe29c3ce997300b76e8554e2d4751d0c5364a
Katy Ekey <kekey@ainq.com>
2017-12-08 12:26:35 -0500

    OCD-1583: delete duplicate ucd/qms

--- v-next.sql
+++ v-next.sql
@@ -147,6 +147,102 @@ CREATE TRIGGER certification_result_test_procedure_timestamp BEFORE UPDATE on op
 CREATE TRIGGER pending_certification_result_test_procedure_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.pending_certification_result_test_procedure FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER pending_certification_result_test_procedure_timestamp BEFORE UPDATE on openchpl.pending_certification_result_test_procedure FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
 
+--
+-- FIX UCD PROCESSES
+--
+
+-- delete references to ucd processes in certification results if ucd process name is blank
+UPDATE openchpl.certification_result_ucd_process 
+SET deleted = true
+WHERE ucd_process_id IN (SELECT ucd_process_id FROM openchpl.ucd_process WHERE name = '')
+AND deleted != true;
+
+-- delete the ucd_process rows that have blank names
+UPDATE openchpl.ucd_process
+SET deleted = true
+WHERE name = ''
+AND deleted != true;
+
+CREATE OR REPLACE FUNCTION openchpl.replaceAllUcdProcessDuplicates() RETURNS void AS $$
+DECLARE
+    dupName varchar(200);
+BEGIN
+    FOR dupName IN SELECT name FROM openchpl.ucd_process WHERE deleted = false GROUP BY name HAVING count(*) > 1
+    LOOP
+	-- change the references to the duplicate ucd process
+	RAISE NOTICE 'Changing all certification results pointing to ucd process %', dupName;
+
+	UPDATE openchpl.certification_result_ucd_process
+	SET ucd_process_id =
+		-- get the first one of the ucd processes with this same name
+		-- and set all certification results to point to it
+		(SELECT ucd_process_id 
+		FROM
+			(SELECT ROW_NUMBER() OVER() AS row, ucd_process_id FROM openchpl.ucd_process WHERE name = dupName AND deleted = false) as dup_rows
+		WHERE dup_rows.row = 1)
+	WHERE ucd_process_id IN (SELECT ucd_process_id FROM openchpl.ucd_process WHERE name = dupName)
+	AND deleted = false;
+
+	-- delete the duplicate ucd process(es)
+	UPDATE openchpl.ucd_process
+	SET deleted = true
+	WHERE ucd_process_id IN
+		-- get all non-first instances of this ucd process name
+	       (SELECT ucd_process_id 
+		FROM
+			(SELECT ROW_NUMBER() OVER() AS row, ucd_process_id FROM openchpl.ucd_process WHERE name = dupName AND deleted = false) as dup_rows
+		WHERE dup_rows.row > 1)
+	AND deleted = false;
+    END LOOP;
+    RETURN;
+END
+$$ LANGUAGE 'plpgsql' ;
+
+SELECT * FROM openchpl.replaceAllUcdProcessDuplicates();
+DROP FUNCTION openchpl.replaceAllUcdProcessDuplicates();
+
+--
+-- FIX QMS STANDARDS
+--
+
+CREATE OR REPLACE FUNCTION openchpl.replaceAllQmsStandardDuplicates() RETURNS void AS $$
+DECLARE
+    dupName varchar(200);
+BEGIN
+    FOR dupName IN SELECT name FROM openchpl.qms_standard WHERE deleted = false GROUP BY name HAVING count(*) > 1
+    LOOP
+	-- change the references to the duplicate ucd process
+	RAISE NOTICE 'Changing all certified products pointing to qms standard %', dupName;
+
+	UPDATE openchpl.certified_product_qms_standard
+	SET qms_standard_id =
+		-- get the first one of the qms standards with this same name
+		-- and set all certification results to point to it
+		(SELECT qms_standard_id 
+		FROM
+			(SELECT ROW_NUMBER() OVER() AS row, qms_standard_id FROM openchpl.qms_standard WHERE name = dupName AND deleted = false) as dup_rows
+		WHERE dup_rows.row = 1)
+	WHERE qms_standard_id IN (SELECT qms_standard_id FROM openchpl.qms_standard WHERE name = dupName)
+	AND deleted = false;
+
+	-- delete the duplicate qms standard(s)
+	UPDATE openchpl.qms_standard
+	SET deleted = true
+	WHERE qms_standard_id IN
+		-- get all non-first instances of this qms standard name
+	       (SELECT qms_standard_id 
+		FROM
+			(SELECT ROW_NUMBER() OVER() AS row, qms_standard_id FROM openchpl.qms_standard WHERE name = dupName AND deleted = false) as dup_rows
+		WHERE dup_rows.row > 1)
+	AND deleted = false;
+    END LOOP;
+    RETURN;
+END
+$$ LANGUAGE 'plpgsql' ;
+
+SELECT * FROM openchpl.replaceAllQmsStandardDuplicates();
+DROP FUNCTION openchpl.replaceAllQmsStandardDuplicates();
+
 COMMIT;
 
 --re-run grants
