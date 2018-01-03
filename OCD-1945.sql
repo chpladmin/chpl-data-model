b65aad3c2a033a29b08d17301cfdd9716c3f60a7
Brian Lindsey <brianl9864@gmail.com>
2018-01-03 14:11:01 -0500

    OCD-1945 - fix fuzzy choices data model

--- OCD-1945.sql
+++ OCD-1945.sql
@@ -15,7 +15,7 @@ CREATE TABLE openchpl.pending_certified_product_system_update(
 
 CREATE TABLE openchpl.fuzzy_choices(
 	fuzzy_choices_id bigserial not null,
-	fuzzy_type fuzzy_type not null,
+	fuzzy_type openchpl.fuzzy_type not null,
 	choices json not null,
 	creation_date timestamp NOT NULL DEFAULT NOW(),
 	last_modified_date timestamp NOT NULL DEFAULT NOW(),
@@ -24,13 +24,13 @@ CREATE TABLE openchpl.fuzzy_choices(
 	CONSTRAINT fuzzy_choices_pk PRIMARY KEY (fuzzy_choices_id)
 );
 
-INSERT INTO fuzzy_choices(fuzzy_type, choices)
+INSERT INTO openchpl.fuzzy_choices(fuzzy_type, choices)
 VALUES('UCD Processes', '["Multiple Standards","ISO 9241-210:2010 4.2","ISO/IEC 25062:2006","Homegrown","NISTIR 7742","(NISTIR 7741) NIST Guide to the Processes Approach for Improving the Usability of Electronic Health Records","IEC 62366","Internal Process Used","IEC 62366-1","ISO 13407","ISO 16982","ISO/IEC 62367"]');
 
-INSERT INTO fuzzy_choices(fuzzy_type, choices)
+INSERT INTO openchpl.fuzzy_choices(fuzzy_type, choices)
 VALUES('QMS Standards', '["ISO 13485:2003","ISO 13485:2012","21 CFR Part 820","ISO 9001",ISO 13485","IEC 62304","IEEE 730","Homegrown","Food and Drug Administrations Code of Federal Regulations Title 21 Part 820 Quality System Regulation","ISMS/ISO/IEC 27001","ISO 9001:2008","Self-Develop","None","Other Federal or SDO QMS Standard"]');
 
-INSERT INTO fuzzy_choices(fuzzy_type, choices)
+INSERT INTO openchpl.fuzzy_choices(fuzzy_type, choices)
 VALUES('Accessibility Standards', '["WCAG 2.0 Level AA","W3C Web Design and Applications","W3C Web of Devices","Section 508 of the Rehabilitation Act","ISO/IEC 40500:2012","None","170.204(a)(1)","170.204(a)(2)","NIST 7741"]');
 
 
