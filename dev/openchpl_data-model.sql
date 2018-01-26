4ac9a4a25ce40a1f5e3b5d624801db54177bb417
Katy Ekey <kekey@ainq.com>
2018-01-26 13:26:00 -0500

    OCD-1945: remove unused table

--- dev/openchpl_data-model.sql
+++ dev/openchpl_data-model.sql
@@ -1503,20 +1503,6 @@ COMMENT ON TABLE openchpl.pending_certified_product IS 'A product that has been 
 -- ALTER TABLE openchpl.pending_certified_product OWNER TO openchpl;
 -- ddl-end --
 
-CREATE TABLE openchpl.pending_certified_product_system_update(
-	pending_certified_product_system_update_id bigserial NOT NULL,
-	pending_certified_product_id bigint NOT NULL,
-	change_made text,
-	creation_date timestamp NOT NULL DEFAULT NOW(),
-	last_modified_date timestamp NOT NULL DEFAULT NOW(),
-	last_modified_user bigint NOT NULL,
-	deleted bool NOT NULL DEFAULT false,
-	CONSTRAINT pending_certified_product_system_update_pk PRIMARY KEY (pending_certified_product_system_update_id),
-	CONSTRAINT pending_certified_product_fk FOREIGN KEY (pending_certified_product_id)
-      REFERENCES openchpl.pending_certified_product (pending_certified_product_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-);
-
 CREATE TABLE openchpl.fuzzy_choices(
 	fuzzy_choices_id bigserial not null,
 	fuzzy_type openchpl.fuzzy_type not null,
