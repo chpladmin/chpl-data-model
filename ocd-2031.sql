3306852536d1255507b7da5abcda536f4564b4e6
Andrew Larned <andrew.larned@gmail.com>
2018-04-10 14:54:01 -0400

    OCD-2031: Add correct permissions for new Notification Type

--- ocd-2031.sql
+++ ocd-2031.sql
@@ -1,4 +1,15 @@
 --
 -- OCD-2031
---
+    --
 insert into openchpl.notification_type (name, description, requires_acb, last_modified_user) select 'Cache Status Age Notification', 'A notification that is sent to subscribers when the Listing Cache is too old.', false, -1 where not exists (select * from openchpl.notification_type where name = 'Cache Status Age Notification');
+create or replace function openchpl.add_permission() returns void as $$
+    begin
+    if (select count(*) from openchpl.notification_type_permission where notification_type_id =
+	(select id from openchpl.notification_type where name = 'Cache Status Age Notification')) = 0 then
+insert into openchpl.notification_type_permission (notification_type_id, permission_id, last_modified_user)
+select id, -2, -1 from openchpl.notification_type where name = 'Cache Status Age Notification';
+    end if;
+    end;
+    $$ language plpgsql;
+select openchpl.add_permission();
+drop function openchpl.add_permission();
