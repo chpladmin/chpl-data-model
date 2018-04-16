55796feb4a60d514a2c8e08a41d9e4dbc709424f
Andrew Larned <andrew.larned@gmail.com>
2018-04-16 13:13:16 -0400

    OCD-2182: Update dump script to support filename parameter

--- maint/dump.sh
+++ maint/dump.sh
@@ -2,47 +2,53 @@
 
 function usage {
 
-	cat <<EOM
+    cat <<EOM
 Usage: $(basename "$0") [OPTION]...
 
-  -h VALUE    Specifies the host name of the machine on which the server is running
-  -u VALUE    User name to connect as
-  -i          Includes the logged_actions table data
+  -h VALUE    Specifies the host name of the machine on which the server is running (default: localhost)
+  -u VALUE    User name to connect as                                               (default: openchpl_dev)
+  -f VALUE    File name of output                                                   (default: openchpl.backup)
+  -i          Includes the logged_actions table data                                (default: false)
   -h          display help
 EOM
 
-	exit 2
+    exit 2
 }
 
 INCLUDE=0
 HOST=localhost
 USER=openchpl_dev
+FILE=openchpl.backup
 
-while getopts ":h:u:i" OPTION; do
+while getopts ":h:u:f:i" OPTION; do
     case "$OPTION" in
         h)
-			HOST=$OPTARG
+	    HOST=$OPTARG
             ;;
         u)
             USER=$OPTARG
             ;;
-		i) 
-			INCLUDE=1
-			;;
-        ?)
-            usage
+        f)
+            FILE=$OPTARG
             ;;
+	i)
+	    INCLUDE=1
+	    ;;
+        ?)
+        usage
+        ;;
     esac
 done
 shift $((OPTIND-1))
 
 echo "h = $HOST"
 echo "u = $USER"
+echo "f = $FILE"
 echo "i = $INCLUDE"
 
-if [ $INCLUDE -eq 1 ]  
+if [ $INCLUDE -eq 1 ]
 then
-	pg_dump --host $HOST --username $USER --no-password --format custom --blobs --verbose --file openchpl.backup openchpl 
+    pg_dump --host $HOST --username $USER --no-password --format custom --blobs --verbose --file $FILE openchpl
 else
-	pg_dump --host $HOST --username $USER --no-password --format custom --blobs --verbose --exclude-table-data=audit.logged_actions --file openchpl.backup openchpl 
+    pg_dump --host $HOST --username $USER --no-password --format custom --blobs --verbose --exclude-table-data=audit.logged_actions --file $FILE openchpl
 fi
