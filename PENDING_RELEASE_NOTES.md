cdd17191fbc043c617354398e62f0b92e9323730
tmy1313 <tyoung@ainq.com>
2018-05-02 11:56:52 -0400

    Modified based on PR comments
    Updated the pending release notes
    Updated to the v-next.sql so that it could be run multiple times

--- PENDING_RELEASE_NOTES.md
+++ PENDING_RELEASE_NOTES.md
@@ -2,13 +2,8 @@
 ## Version 14.0.1
 _Date TBD_
 
-### Table modifications
-
-### View Modifications
-
 ### Data modifications
 * Add 2 new testing tools
 
-### Script Modifications
 
 ---
--- v-next.sql
+++ v-next.sql
@@ -2,9 +2,11 @@
 -- OCD-2219 - Add 2 new test tools
 --
 INSERT INTO openchpl.test_tool(
-	name, description, creation_date, last_modified_user, deleted, retired)
-	VALUES ('NCQA ONC Health IT Testing', null, current_timestamp, -1, false, false);
+	name, description, last_modified_user)
+SELECT 'NCQA ONC Health IT Testing', null, -1 
+	WHERE NOT EXISTS (SELECT 1 FROM openchpl.test_tool WHERE name = 'NCQA ONC Health IT Testing');
 
 INSERT INTO openchpl.test_tool(
-	name, description, creation_date, last_modified_user, deleted, retired)
-	VALUES ('HIMSS Immunization Integration Program', null, current_timestamp, -1, false, false);
+	name, description, last_modified_user)
+SELECT 'HIMSS Immunization Integration Program', null, -1 
+	WHERE NOT EXISTS (SELECT 1 FROM openchpl.test_tool WHERE name = 'HIMSS Immunization Integration Program');
