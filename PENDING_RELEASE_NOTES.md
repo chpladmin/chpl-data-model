cb2b8fdf957bf9c385dd0b4c474c0fb4c42e1d00
Andrew Larned <alarned@ainq.com>
2018-05-10 12:22:24 -0400

    OCD-2035: Prep for PR

--- PENDING_RELEASE_NOTES.md
+++ PENDING_RELEASE_NOTES.md
@@ -2,6 +2,9 @@
 ## Version 14.1.0
 _Date TBD_
 
+### Table Modifications
+* Added table to support criterion/product chart statistics
+
 ### View Modifications
 * Update `certified_product_search` to respect deleted certification_status_events when getting "latest" status name
 
--- v-next.sql
+++ v-next.sql
@@ -1898,5 +1898,20 @@ LEFT JOIN
    GROUP BY certified_product_id) cqms ON cqms.certified_product_id = cp.certified_product_id
 WHERE cp.deleted <> TRUE;
 
+DROP TABLE IF EXISTS openchpl.criterion_product_statistics;
+
+CREATE TABLE openchpl.criterion_product_statistics (
+        id bigserial NOT NULL,
+        product_count bigint NOT NULL,
+        certification_criterion_id bigint NOT NULL REFERENCES openchpl.certification_criterion (certification_criterion_id),
+        creation_date timestamp without time zone NOT NULL DEFAULT now(),
+        last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+        last_modified_user bigint NOT NULL,
+        deleted boolean NOT NULL DEFAULT false,
+        CONSTRAINT criterion_product_statistics_pk PRIMARY KEY (id)
+        );
+CREATE TRIGGER criterion_product_statistics_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.criterion_product_statistics FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER criterion_product_statistics_timestamp BEFORE UPDATE on openchpl.criterion_product_statistics FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
+
 --re-run grants
 \i dev/openchpl_grant-all.sql
