b213095e1a72163bdd87167cd17d719aa740d0d1
Brian Lindsey <brianl9864@gmail.com>
2018-07-18 10:38:41 -0400

    OCD-2351 - move nc charts table creation to v-dev

---
+++ v-dev.sql
@@ -0,0 +1,16 @@
+-- OCD - 2351 - nonconformity chart statistics
+
+DROP TABLE openchpl.nonconformity_type_statistics;
+CREATE TABLE openchpl.nonconformity_type_statistics
+(
+  	id bigserial NOT NULL,
+  	nonconformity_type varchar(1024),
+	nonconformity_count bigint NOT NULL,
+  	creation_date timestamp without time zone NOT NULL DEFAULT now(),
+  	last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+  	last_modified_user bigint NOT NULL,
+  	deleted boolean NOT NULL DEFAULT false,
+  	CONSTRAINT nonconformity_type_statistics_pk PRIMARY KEY (id)
+);
+CREATE TRIGGER nonconformity_type_statistics_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.nonconformity_type_statistics FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER nonconformity_type_statistics_timestamp BEFORE UPDATE on openchpl.nonconformity_type_statistics FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
