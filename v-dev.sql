0b40d02f06db5df2e0e86a69b541c2d8e14296f0
Brian Lindsey <brianl9864@gmail.com>
2018-07-19 08:58:06 -0400

    OCD-2351 - add grant all script to end of v-dev

--- v-dev.sql
+++ v-dev.sql
@@ -14,3 +14,6 @@ CREATE TABLE openchpl.nonconformity_type_statistics
 );
 CREATE TRIGGER nonconformity_type_statistics_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.nonconformity_type_statistics FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER nonconformity_type_statistics_timestamp BEFORE UPDATE on openchpl.nonconformity_type_statistics FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
+
+--re-run grants	
+\i dev/openchpl_grant-all.sql
