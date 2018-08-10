37ca8520f24d8bc9fa80db788fd5f8983861f633
Andrew Larned <andrew.larned@gmail.com>
2018-08-10 09:48:41 -0400

    Fix name for table PK constraint
    [#OCD-2273]

--- v-dev.sql
+++ v-dev.sql
@@ -107,7 +107,7 @@ CREATE TABLE openchpl.inheritance_errors_report
     last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
     last_modified_user bigint NOT NULL,
     deleted boolean NOT NULL DEFAULT false,
-    CONSTRAINT id_pk PRIMARY KEY (id)
+    CONSTRAINT inheritance_errors_report_id_pk PRIMARY KEY (id)
 );
 CREATE TRIGGER inheritance_errors_report_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.inheritance_errors_report FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER inheritance_errors_report_timestamp BEFORE UPDATE on openchpl.inheritance_errors_report FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
