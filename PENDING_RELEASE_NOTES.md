df717c3cde18e914862c7ea876081e9ab04d9bfd
Brian Lindsey <brianl9864@gmail.com>
2018-08-22 15:27:41 -0400

    OCD-2392 - move whitelist column to v-dev

--- PENDING_RELEASE_NOTES.md
+++ PENDING_RELEASE_NOTES.md
@@ -5,6 +5,5 @@ _Date TBD_
 ### Table Addition
 * Add table for inherited certification status errors report
 * Add table for broken surveillance rules report data
-* Add whitelist column to api key table
 
 ---
---
+++ v-dev.sql
@@ -0,0 +1,8 @@
+-- OCD-2392 - add whitelisting to api keys
+
+ALTER TABLE openchpl.api_key DROP COLUMN IF EXISTS whitelisted;
+ALTER TABLE openchpl.api_key ADD COLUMN whitelisted boolean DEFAULT false;
+UPDATE openchpl.api_key SET whitelisted = true WHERE api_key_id = 1;
+
+--re-run grants
+\i dev/openchpl_grant-all.sql
