81d054a616194b7cbe787ebb89a768777d09da71
Katy Ekey <katy.ekey@gmail.com>
2018-11-19 08:19:42 -0500

    OCD-1665: change how we put in retired flag for acbs

--- ocd-1665.sql
+++
@@ -1,5 +0,0 @@
-UPDATE openchpl.certification_body
-SET deleted = false;
-
-UPDATE openchpl.testing_lab
-SET deleted = false;
--- v-dev.sql
+++ v-dev.sql
@@ -1,15 +1,69 @@
+-- Update certification_body table
 ALTER TABLE openchpl.certification_body DROP COLUMN IF EXISTS retired;
 ALTER TABLE openchpl.certification_body ADD COLUMN retired boolean default false;
 
 UPDATE openchpl.certification_body
-SET retired = deleted;
+SET retired = false
+where name = 'InfoGard';
 
+UPDATE openchpl.certification_body
+SET retired = true
+where name = 'CCHIT';
+
+UPDATE openchpl.certification_body
+SET retired = false
+where name = 'Drummond Group';
+
+UPDATE openchpl.certification_body
+SET retired = false
+where name = 'SLI Compliance';
+
+UPDATE openchpl.certification_body
+SET retired = true
+where name = 'Surescripts LLC';
+
+UPDATE openchpl.certification_body
+SET retired = false
+where name = 'ICSA Labs';
+
+UPDATE openchpl.certification_body
+SET retired = true
+where name = 'Pending';
+
+UPDATE openchpl.certification_body
+SET deleted = false;
+
+-- Update testing labs
 ALTER TABLE openchpl.testing_lab DROP COLUMN IF EXISTS retired;
 ALTER TABLE openchpl.testing_lab ADD COLUMN retired boolean default false;
 
 UPDATE openchpl.testing_lab
-SET retired = deleted;
+SET retired = false
+where name = 'InfoGard';
 
--- All ACBs and ATLs should have their 'deleted' column set to false
--- but this can't be done until the retired column has been released
--- to production. See OCD-1665.sql.
+UPDATE openchpl.testing_lab
+SET retired = true
+where name = 'CCHIT';
+
+UPDATE openchpl.testing_lab
+SET retired = false
+where name = 'Drummond Group';
+
+UPDATE openchpl.testing_lab
+SET retired = false
+where name = 'SLI Compliance';
+
+UPDATE openchpl.testing_lab
+SET retired = false
+where name = 'ICSA Labs';
+
+UPDATE openchpl.testing_lab
+SET retired = true
+where name = 'National Technical Systems';
+
+UPDATE openchpl.testing_lab
+SET retired = false
+where name = 'National Committee for Quality Assurance (NCQA)';
+
+UPDATE openchpl.testing_lab
+SET deleted = false;
