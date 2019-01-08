ecfbbe03a8f3ad5fad86fdce1d686733a1eb5924
Katy Ekey <katy.ekey@gmail.com>
2019-01-08 09:37:37 -0500

    fix: update soft deletes and release notes
    [#OCD-2609]

--- dev/openchpl_soft-delete.sql
+++ dev/openchpl_soft-delete.sql
@@ -2,10 +2,12 @@ CREATE OR REPLACE FUNCTION openchpl.certified_product_soft_delete()
 RETURNS TRIGGER AS $$
 BEGIN
     UPDATE openchpl.certification_event as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
+	UPDATE openchpl.certification_status_event as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
     UPDATE openchpl.certification_result as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
     UPDATE openchpl.certified_product_accessibility_standard as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
     UPDATE openchpl.certified_product_qms_standard as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
     UPDATE openchpl.certified_product_targeted_user as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
+	UPDATE openchpl.certified_product_testing_lab_map as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
     UPDATE openchpl.corrective_action_plan as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
     UPDATE openchpl.cqm_result as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
     UPDATE openchpl.ehr_certification_id_product_map as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
@@ -16,6 +18,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS certified_product_soft_delete on openchpl.certified_product;
 CREATE TRIGGER certified_product_soft_delete AFTER UPDATE of deleted on openchpl.certified_product FOR EACH ROW EXECUTE PROCEDURE openchpl.certified_product_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.certification_result_soft_delete()
@@ -35,6 +38,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS certification_result_soft_delete on openchpl.certification_result;
 CREATE TRIGGER certification_result_soft_delete AFTER UPDATE of deleted on openchpl.certification_result FOR EACH ROW EXECUTE PROCEDURE openchpl.certification_result_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.corrective_action_plan_soft_delete()
@@ -45,6 +49,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS corrective_action_plan_soft_delete on openchpl.corrective_action_plan;
 CREATE TRIGGER corrective_action_plan_soft_delete AFTER UPDATE of deleted on openchpl.corrective_action_plan FOR EACH ROW EXECUTE PROCEDURE openchpl.corrective_action_plan_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.cqm_result_soft_delete()
@@ -54,6 +59,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS cqm_result_soft_delete on openchpl.cqm_result;
 CREATE TRIGGER cqm_result_soft_delete AFTER UPDATE of deleted on openchpl.cqm_result FOR EACH ROW EXECUTE PROCEDURE openchpl.cqm_result_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.surveillance_soft_delete()
@@ -63,6 +69,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS surveillance_soft_delete on openchpl.surveillance;
 CREATE TRIGGER surveillance_soft_delete AFTER UPDATE of deleted on openchpl.surveillance FOR EACH ROW EXECUTE PROCEDURE openchpl.surveillance_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.surveillance_requirement_soft_delete()
@@ -72,6 +79,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS surveillance_requirement_soft_delete on openchpl.surveillance_requirement;
 CREATE TRIGGER surveillance_requirement_soft_delete AFTER UPDATE of deleted on openchpl.surveillance_requirement FOR EACH ROW EXECUTE PROCEDURE openchpl.surveillance_requirement_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.surveillance_nonconformity_soft_delete()
@@ -81,6 +89,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS surveillance_nonconformity_soft_delete on openchpl.surveillance_nonconformity;
 CREATE TRIGGER surveillance_nonconformity_soft_delete AFTER UPDATE of deleted on openchpl.surveillance_nonconformity FOR EACH ROW EXECUTE PROCEDURE openchpl.surveillance_nonconformity_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.certification_result_test_task_soft_delete()
@@ -90,6 +99,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS certification_result_test_task_soft_delete on openchpl.certification_result_test_task;
 CREATE TRIGGER certification_result_test_task_soft_delete AFTER UPDATE of deleted on openchpl.certification_result_test_task FOR EACH ROW EXECUTE PROCEDURE openchpl.certification_result_test_task_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.test_task_soft_delete()
@@ -99,6 +109,7 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS test_task_soft_delete on openchpl.test_task;
 CREATE TRIGGER test_task_soft_delete AFTER UPDATE of deleted on openchpl.test_task FOR EACH ROW EXECUTE PROCEDURE openchpl.test_task_soft_delete();
 
 CREATE OR REPLACE FUNCTION openchpl.test_task_participant_map_soft_delete()
@@ -119,13 +130,5 @@ BEGIN
     RETURN NEW;
 END;
 $$ language 'plpgsql';
+DROP TRIGGER IF EXISTS test_task_participant_map_soft_delete on openchpl.test_task_participant_map;
 CREATE TRIGGER test_task_participant_map_soft_delete AFTER UPDATE of deleted on openchpl.test_task_participant_map FOR EACH ROW EXECUTE PROCEDURE openchpl.test_task_participant_map_soft_delete();
-
-CREATE OR REPLACE FUNCTION openchpl.certified_product_testing_lab_map_soft_delete()
-RETURNS TRIGGER AS $$
-BEGIN
-    UPDATE openchpl.certified_product_testing_lab_map as src SET deleted = NEW.deleted WHERE src.certified_product_id = NEW.certified_product_id;
-    RETURN NEW;
-END;
-$$ language 'plpgsql';
-CREATE TRIGGER certified_product_testing_lab_map AFTER UPDATE of deleted on openchpl.certified_product FOR EACH ROW EXECUTE PROCEDURE openchpl.certified_product_testing_lab_map_soft_delete();
