f768d2469a46ae793d7b9910def2ecce0d2980e9
Andrew Larned <andrew.larned@gmail.com>
2019-01-24 14:54:56 -0500

    feat: set retirement dates for some ACBs/ATLs
    [#OCD-1566]

--- v-next.sql
+++ v-next.sql
@@ -66,3 +66,7 @@ alter table openchpl.certification_body drop column if exists retirement_date;
 alter table openchpl.testing_lab drop column if exists retirement_date;
 alter table openchpl.certification_body add column retirement_date timestamp;
 alter table openchpl.testing_lab add column retirement_date timestamp;
+
+update openchpl.certification_body set retirement_date = '2014-05-01' where name = 'CCHIT';
+update openchpl.certification_body set retirement_date = '2013-04-04' where name = 'Surescripts LLC';
+update openchpl.testing_lab set retirement_date = '2014-05-01' where name = 'CCHIT';
