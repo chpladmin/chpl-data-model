3f445b26a5b02df1163aa9eee6fe25314309eee0
Andrew Larned <andrew.larned@gmail.com>
2019-02-19 10:03:39 -0500

    feat: allow unlimited length numbers
    [#OCD-2697]

--- ocd-2697.sql
+++ ocd-2697.sql
@@ -2,20 +2,20 @@
 -- OCD-2697: accept bad characters in number fields for test/participant data during upload
 --
 alter table openchpl.pending_test_participant
-alter column professional_experience_months type varchar(25),
-alter column computer_experience_months type varchar(25),
-alter column product_experience_months type varchar(25);
+alter column professional_experience_months type text,
+alter column computer_experience_months type text,
+alter column product_experience_months type text;
 
 alter table openchpl.pending_test_task
-alter column task_success_avg_pct type varchar(25),
-alter column task_success_stddev_pct type varchar(25),
-alter column task_path_deviation_observed type varchar(25),
-alter column task_path_deviation_optimal type varchar(25),
-alter column task_time_avg_seconds type varchar(25),
-alter column task_time_stddev_seconds type varchar(25),
-alter column task_time_deviation_observed_avg_seconds type varchar(25),
-alter column task_time_deviation_optimal_avg_seconds type varchar(25),
-alter column task_errors_pct type varchar(25),
-alter column task_errors_stddev_pct type varchar(25),
-alter column task_rating type varchar(25),
-alter column task_rating_stddev type varchar(25);
+alter column task_success_avg_pct type text,
+alter column task_success_stddev_pct type text,
+alter column task_path_deviation_observed type text,
+alter column task_path_deviation_optimal type text,
+alter column task_time_avg_seconds type text,
+alter column task_time_stddev_seconds type text,
+alter column task_time_deviation_observed_avg_seconds type text,
+alter column task_time_deviation_optimal_avg_seconds type text,
+alter column task_errors_pct type text,
+alter column task_errors_stddev_pct type text,
+alter column task_rating type text,
+alter column task_rating_stddev type text;
