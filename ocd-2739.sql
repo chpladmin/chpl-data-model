dbdf741539e5b449e4758b29418a18ae9b4da899
Andrew Larned <andrew.larned@gmail.com>
2019-02-27 11:08:49 -0500

    fix: removed unused compliance column
    [#OCD-2739]

--- ocd-2739.sql
+++ ocd-2739.sql
@@ -7,3 +7,6 @@ drop table if exists openchpl.corrective_action_plan;
 -- event types
 drop table if exists openchpl.certification_event;
 drop table if exists openchpl.event_type;
+
+-- compliance signature
+alter table openchpl.user drop column if exists compliance_signature;
