d4b4b36afb07130956e128bec9f8a3bf5620ea9c
Andrew Larned <andrew.larned@gmail.com>
2019-03-28 15:12:44 -0400

    fix: release version 17.0.1 on 28 March 2019
    [#ver-17.0.1]

--- STAGED_RELEASE_NOTES.md
+++
@@ -1,8 +0,0 @@
-
-## Version 17.0.1
-_Date TBD_
-
-### Data Modifications
-* Mark "Pending" Certification Status as deleted
-
----
--- ocd-2825.sql
+++
@@ -1,4 +0,0 @@
----------------------------------------
--- OCD-2825
----------------------------------------
-update openchpl.certification_status set deleted = true where certification_status = 'Pending';
