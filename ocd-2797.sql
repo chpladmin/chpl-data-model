114379998accb8afffc3d95575b691c243216e65
tmy1313 <tyoung@ainq.com>
2019-04-05 15:20:37 -0400

    fix: update activity concept 'PENDING SURVE...' to 'PENDING_SURVE...'
    OCD-2797

---
+++ ocd-2797.sql
@@ -0,0 +1,2 @@
+update openchpl.activity_concept set concept='PENDING_SURVEILLANCE' where concept='PENDING SURVEILLANCE';
+ 
--- v-next.sql
+++ v-next.sql
@@ -1,4 +1,5 @@
 \i ocd-2809.sql
+\i ocd-2797.sql
 
 \i dev/openchpl_soft-delete.sql
 \i dev/openchpl_views.sql
