e024efe3fbc8a297f47fc5a595f1353af999db0a
Katy Ekey <katy.ekey@gmail.com>
2019-05-14 10:12:06 -0400

    feat: add feature flags to test data for unit test purposes

--- dev/openchpl_preload-test-only.sql
+++ dev/openchpl_preload-test-only.sql
@@ -1,1 +1,7 @@
 SELECT pg_catalog.setval('openchpl.acl_entry_id_seq', 16, true);
+
+--ff4j features needed for tests to run
+INSERT INTO ff4j.features(feat_uid, enable, description, strategy, expression, groupname)
+VALUES 
+('ocd2820', 1, null, null, null, null),
+('better-split', 1, null, null, null, null);
