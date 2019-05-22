b7a64dfc5bffe1923d016c100d171033ca33b259
tmy1313 <tyoung@ainq.com>
2019-05-22 11:35:49 -0400

    feat: add user and user action activity reports to filter_type table
    OCD-2789

--- ocd-2789.sql
+++ ocd-2789.sql
@@ -4,3 +4,10 @@ WHERE NOT EXISTS
 	(SELECT *
 	 FROM openchpl.filter_type
 	 WHERE name = 'User Report');
+
+INSERT INTO openchpl.filter_type (name, last_modified_user)
+SELECT 'User Action Report', -1
+WHERE NOT EXISTS 
+	(SELECT *
+	 FROM openchpl.filter_type
+	 WHERE name = 'User Action Report');
