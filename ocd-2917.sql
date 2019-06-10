f33daac8c8390e188c507e365aacbefa20741e38
Katy Ekey <katy.ekey@gmail.com>
2019-06-10 12:28:57 -0400

    feat: add acb fk to quarterly report table

--- ocd-2917.sql
+++ ocd-2917.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS openchpl.quarterly_report;
 
 CREATE TABLE openchpl.quarterly_report (
 	id bigserial NOT NULL,
+	certification_body_id bigint NOT NULL,
 	year integer NOT NULL,
 	quarter_id bigint NOT NULL,
 	activities_and_outcomes_summary text,
@@ -13,6 +14,9 @@ CREATE TABLE openchpl.quarterly_report (
 	last_modified_user bigint NOT NULL,
 	deleted bool NOT NULL DEFAULT false,
 	CONSTRAINT quarterly_report_pk PRIMARY KEY (id),
+	CONSTRAINT certification_body_fk FOREIGN KEY (certification_body_id)
+      REFERENCES openchpl.certification_body (certification_body_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE RESTRICT,
 	CONSTRAINT quarter_fk FOREIGN KEY (quarter_id)
       REFERENCES openchpl.quarter (id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE RESTRICT
