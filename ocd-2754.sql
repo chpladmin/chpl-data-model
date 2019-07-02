61cb5219686a6f7ac2d5d5c5d0318be1d43080e2
tmy1313 <tyoung@ainq.com>
2019-07-02 19:45:12 -0400

    feat: add Announcement Report to filter_types table
    OCD-2754

---
+++ ocd-2754.sql
@@ -0,0 +1,6 @@
+INSERT INTO openchpl.filter_type (name, last_modified_user)
+SELECT 'Announcement Report', -1
+WHERE NOT EXISTS 
+	(SELECT *
+	 FROM openchpl.filter_type
+	 WHERE name = 'Announcements Report');
