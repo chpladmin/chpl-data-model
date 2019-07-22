992df85ba3becf42e3bc9cd34a8552c86c399691
Katy Ekey <katy.ekey@gmail.com>
2019-07-22 12:13:18 -0400

    feat: release version 17.7.0 on 22 July 2019

--- v-next.sql
+++ v-next.sql
@@ -1,12 +1,3 @@
-\i ocd-2912.sql
-\i ocd-2913.sql
-\i ocd-2915.sql
-\i ocd-2914.sql
-\i ocd-2917.sql
-\i ocd-2918.sql
-\i ocd-2919.sql
-\i ocd-2754.sql
-
 \i dev/openchpl_soft-delete.sql
 \i dev/openchpl_views.sql
 \i dev/openchpl_grant-all.sql
