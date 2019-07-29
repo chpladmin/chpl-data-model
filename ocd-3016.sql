a4959a7a64f207280d25f55797fb8ba2a652aee8
Andrew Larned <andrew.larned@gmail.com>
2019-07-29 12:08:05 -0400

    feat: release version 17.8.0 on 29 July 2019
    [#ver-17.8.0]

--- ocd-3016.sql
+++
@@ -1,7 +0,0 @@
-insert into openchpl.activity_concept (concept, last_modified_user)
-select 'COMPLAINT', -1
-where not exists
-	(select *
-	from openchpl.activity_concept
-	where concept = 'COMPLAINT');
-
