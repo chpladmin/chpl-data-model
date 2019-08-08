b678b47868b3f1726c646550e7e8b4d72ed739b0
Andrew Larned <andrew.larned@gmail.com>
2019-08-08 13:37:30 -0400

    fix: DEVELOPERs can't do much with ACBs
    [#OCD-2158]

--- dev/openchpl_preload.sql
+++ dev/openchpl_preload.sql
@@ -1978,7 +1978,7 @@ INSERT INTO user_permission (user_permission_id, "name", description, authority,
 (4, 'ATL' ,'This permission gives a user write access to their ATLs.', 'ROLE_ATL' , -1),
 (6, 'CMS_STAFF' ,'This permission gives a user read access to CMS reports.', 'ROLE_CMS_STAFF' , -1),
 (7, 'ONC', 'This permission gives ONC users administrative privileges.', 'ROLE_ONC', -1),
-(8, 'DEVELOPER', 'This permission gives a user the ability to submit update requests for their ACBs.', 'ROLE_DEVELOPER', -1);
+(8, 'DEVELOPER', 'This permission gives a user the ability to submit update requests for their organization(s).', 'ROLE_DEVELOPER', -1);
 SELECT pg_catalog.setval('user_permission_user_permission_id_seq', 9, true);
 
 --user contacts.
