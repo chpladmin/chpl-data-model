0a9ed707c4fb7f0d3a8c32d8224dd0b74318d697
Todd Young <tyoung@ainq.com>
2019-08-08 20:54:29 -0400

    qa: Update sql script to fix sql error
    OCD-3018

--- ocd-3018.sql
+++ ocd-3018.sql
@@ -9,9 +9,8 @@ group by address_id
 having count(*) > 1;
 
 do $$
+    declare addr_count integer;
 begin
-	declare addr_count integer;
-
 	--Is addr_id 1 still tied to multiple agencies
 	select count(*) into addr_count from (
 		select address_id from openchpl.vendor where deleted = false
@@ -42,9 +41,9 @@ begin
 		select address_id from openchpl.testing_lab where deleted = false) addr_cnt
 	where address_id = 2
 	group by address_id;
-
-	if addr_count > 1 then
-		insert into openchpl.address (street_line_1, street_line_2, city, state, zipcode, country, last_modified_user)
+    
+    if addr_count > 1 then
+    	insert into openchpl.address (street_line_1, street_line_2, city, state, zipcode, country, last_modified_user)
 		select street_line_1, street_line_2, city, state, zipcode, country, -1
 		from openchpl.address
 		where address_id = 2;
@@ -72,7 +71,7 @@ begin
 		
 		update openchpl.vendor
 		set address_id = lastval(), last_modified_user = -1
-		where vendor_id = 1565;
+		where vendor_id = 1880;
 	end if;
 
 	--Is addr_id 359 still tied to multiple agencies
@@ -137,7 +136,27 @@ begin
 		set address_id = lastval(), last_modified_user = -1
 		where vendor_id = 2032;
 	end if;
+    
+    --Is addr_id 500 still tied to multiple agencies
+	select count(*) into addr_count from (
+		select address_id from openchpl.vendor where deleted = false
+		union all 
+		select address_id from openchpl.certification_body where deleted = false
+		union all 
+		select address_id from openchpl.testing_lab where deleted = false) addr_cnt
+	where address_id = 500
+	group by address_id;
 
+	if addr_count > 1 then
+		insert into openchpl.address (street_line_1, street_line_2, city, state, zipcode, country, last_modified_user)
+		select street_line_1, street_line_2, city, state, zipcode, country, -1
+		from openchpl.address
+		where address_id = 500;
+		
+		update openchpl.vendor
+		set address_id = lastval(), last_modified_user = -1
+		where vendor_id = 1886;
+    end if;
 end $$;
 
 select address_id, count(*) from (
