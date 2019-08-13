255ee4a6f48c174a7d9ef49ff9dd1eded622e8f9
tmy1313 <tyoung@ainq.com>
2019-08-13 15:27:26 -0400

    remove port specification from batch program
    OCD-3013

--- dev/reset-test-only.bat
+++ dev/reset-test-only.bat
@@ -1,11 +1,11 @@
-psql -Upostgres -p 5433 -f drop-openchpl.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_data-model.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_audit.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_soft-delete.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_views.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_preload.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_api-key.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_quartz.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_ff4j.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_grant-all.sql openchpl_test
-psql -Upostgres -p 5433 -f openchpl_preload-test-only.sql openchpl_test
+psql -Upostgres -f drop-openchpl.sql openchpl_test
+psql -Upostgres -f openchpl_data-model.sql openchpl_test
+psql -Upostgres -f openchpl_audit.sql openchpl_test
+psql -Upostgres -f openchpl_soft-delete.sql openchpl_test
+psql -Upostgres -f openchpl_views.sql openchpl_test
+psql -Upostgres -f openchpl_preload.sql openchpl_test
+psql -Upostgres -f openchpl_api-key.sql openchpl_test
+psql -Upostgres -f openchpl_quartz.sql openchpl_test
+psql -Upostgres -f openchpl_ff4j.sql openchpl_test
+psql -Upostgres -f openchpl_grant-all.sql openchpl_test
+psql -Upostgres -f openchpl_preload-test-only.sql openchpl_test
