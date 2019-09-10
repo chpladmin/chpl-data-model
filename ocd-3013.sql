aed6700517df28f48b8b54ad040f9c0c62003da9
Katy Ekey <katy.ekey@gmail.com>
2019-09-10 14:44:09 -0400

    feat: release version 17.10.0 on 10 September 2019

--- ocd-3013.sql
+++
@@ -1,4 +0,0 @@
-alter table openchpl.user drop column if exists last_logged_in_date;
-
-alter table openchpl.user add column last_logged_in_date timestamp;
-
--- v17.10.0.sql
+++ v17.10.0.sql
@@ -1,3 +1,6 @@
+-- Deployment file for version 17.10.0
+--     as of 2019-09-10
+-- ocd-3018-2.sql
 select address_id, count(*) from (
 		select address_id from openchpl.vendor where deleted = false
 		union all 
@@ -41,4 +44,22 @@ select address_id, count(*) from (
 		select address_id from openchpl.testing_lab where deleted = false) addr_cnt
 where address_id is not null
 group by address_id
-having count(*) > 1;
+having count(*) > 1;;
+-- ocd-3013.sql
+alter table openchpl.user drop column if exists last_logged_in_date;
+
+alter table openchpl.user add column last_logged_in_date timestamp;
+
+;
+-- ocd-2748.sql
+insert into openchpl.filter_type (name, last_modified_user)
+select 'API Key Usage Report', -1
+where not exists
+	(select *
+	from openchpl.filter_type
+	where name = 'API Key Usage Report');
+;
+insert into openchpl.data_model_version (version, deploy_date, last_modified_user) values ('17.10.0', '2019-09-10', -1);
+\i dev/openchpl_soft-delete.sql
+\i dev/openchpl_views.sql
+\i dev/openchpl_grant-all.sql
