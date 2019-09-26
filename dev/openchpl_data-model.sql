78277920e16a3181805ec69108e3f9021a902c0b
Katy Ekey <katy.ekey@gmail.com>
2019-09-26 10:04:06 -0400

    feat: Add tables and views to support reporting on questionable urls
    * Add url_check_result table to store all the results of all urls that have been checked
    * Add url_type table as a lookup table of places urls can be found in the system
    * Update certified_product_summary view to have developer contact info and certification date
    
    OCD-2841

--- dev/openchpl_data-model.sql
+++ dev/openchpl_data-model.sql
@@ -2771,6 +2771,33 @@ CREATE TABLE openchpl.broken_surveillance_rules
     CONSTRAINT broken_surveillance_rules_id_pk PRIMARY KEY (id)
 );
 
+CREATE TABLE openchpl.url_type (
+	id bigserial NOT NULL,
+	name varchar(50) NOT NULL,
+	creation_date timestamp NOT NULL DEFAULT NOW(),
+	last_modified_date timestamp NOT NULL DEFAULT NOW(),
+	last_modified_user bigint NOT NULL,
+	deleted bool NOT NULL DEFAULT false,
+	CONSTRAINT url_type_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE openchpl.url_check_result (
+	id bigserial NOT NULL,
+	url_type_id bigint NOT NULL,
+	url text NOT NULL,
+	response_code int, --allow null in case something times out?
+	response_message text, -- if there is some additional text about the response (error message?)
+	checked_date timestamp, -- null to indicate we know about the URL in the system but haven't checked it yet
+	creation_date timestamp NOT NULL DEFAULT NOW(),
+	last_modified_date timestamp NOT NULL DEFAULT NOW(),
+	last_modified_user bigint NOT NULL,
+	deleted bool NOT NULL DEFAULT false,
+	CONSTRAINT url_check_result_pk PRIMARY KEY (id),
+	CONSTRAINT url_type_fk FOREIGN KEY (url_type_id)
+		REFERENCES openchpl.url_type (id)
+		MATCH SIMPLE ON UPDATE NO ACTION ON DELETE RESTRICT
+);
+
 CREATE TABLE openchpl.test_functionality_criteria_map
 (
     id bigserial NOT NULL,
--- dev/openchpl_preload.sql
+++ dev/openchpl_preload.sql
@@ -2307,6 +2307,15 @@ VALUES
 INSERT INTO openchpl.quarter (name, quarter_begin_month, quarter_begin_day, quarter_end_month, quarter_end_day, last_modified_user)
 VALUES ('Q1', 1, 1, 3, 31, -1), ('Q2', 4, 1, 6, 30, -1), ('Q3', 7, 1, 9, 30, -1), ('Q4', 10, 1, 12, 31, -1);
 
+INSERT INTO openchpl.url_type (name, last_modified_user)
+VALUES ('ONC-ATL', -1),
+('ONC-ACB', -1),
+('Developer', -1),
+('Mandatory Disclosure URL', -1), -- transparency_attestation_url
+('Test Results Summary', -1), -- report_file_location
+('Full Usability Report', -1), -- sed_report_file_location 
+('API Documentation', -1); -- api_documentation
+
 INSERT INTO openchpl.change_request_type (name, last_modified_user)
 VALUES
     ('Website Change Request', -1),
