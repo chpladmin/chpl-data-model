dec6755438d65288089428f8370d69ccd1aba844
tmy1313 <tyoung@ainq.com>
2019-09-27 10:18:16 -0400

    add user_permission_id to the change_request_status table
    OCD-3093

--- ocd-3093.sql
+++ ocd-3093.sql
@@ -4,3 +4,19 @@ AS SELECT DISTINCT cp.certification_body_id, dev.vendor_id
      JOIN openchpl.product_version prod_ver ON cp.product_version_id = prod_ver.product_version_id
      JOIN openchpl.product prod ON prod_ver.product_id = prod.product_id
      JOIN openchpl.vendor dev ON prod.vendor_id = dev.vendor_id;
+
+--Need to clear out existing data 
+DELETE FROM openchpl.change_request_status;
+DELETE FROM openchpl.change_request_website;
+DELETE FROM openchpl.change_request;
+
+ALTER TABLE openchpl.change_request_status
+ADD COLUMN user_permission_id BIGINT NOT NULL;
+
+ALTER TABLE openchpl.change_request_status
+DROP CONSTRAINT IF EXISTS user_permission_fk;
+
+ALTER TABLE openchpl.change_request_status
+ADD CONSTRAINT user_permission_fk FOREIGN KEY (user_permission_id)
+	    REFERENCES openchpl.user_permission (user_permission_id)
+        MATCH SIMPLE ON UPDATE NO ACTION ON DELETE RESTRICT;
