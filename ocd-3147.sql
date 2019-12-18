8d457b3645fad596dc23142e7792308b95e1649c
tmy1313 <tyoung@ainq.com>
2019-12-18 09:51:37 -0500

    add a questionable activity concept to the table
    OCD-3147

--- ocd-3147.sql
+++ ocd-3147.sql
@@ -22,4 +22,11 @@ WHERE NOT EXISTS (
     FROM openchpl.questionable_activity_trigger
     WHERE name = 'Certification Criteria 170.315 (b)(3) added without ICS for new listing');
 
+INSERT INTO openchpl.questionable_activity_trigger
+(name, level, last_modified_user)
+SELECT 'ICS added when certification criteria 170.315(b)(3) is on a current listing', 'Listing', -1
+WHERE NOT EXISTS (
+    SELECT *
+    FROM openchpl.questionable_activity_trigger
+    WHERE name = 'ICS added when certification criteria 170.315(b)(3) is on a current listing');
 
