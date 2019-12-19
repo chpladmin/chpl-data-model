d01648013dbc27118448a211c111840eceb9da13
Todd Young <tyoung@ainq.com>
2019-12-19 15:58:35 -0500

    data!: Add questionable activity triggers for adding b3 criteria
    OCD-3147

---
+++ ocd-3147.sql
@@ -0,0 +1,32 @@
+INSERT INTO openchpl.questionable_activity_trigger
+(name, level, last_modified_user)
+SELECT 'Certification Criteria 170.315 (b)(3) added with ICS for current listing', 'Listing', -1
+WHERE NOT EXISTS (
+    SELECT *
+    FROM openchpl.questionable_activity_trigger
+    WHERE name = 'Certification Criteria 170.315 (b)(3) added with ICS for current listing');
+
+INSERT INTO openchpl.questionable_activity_trigger
+(name, level, last_modified_user)
+SELECT 'Certification Criteria 170.315 (b)(3) added without ICS for current listing', 'Listing', -1
+WHERE NOT EXISTS (
+    SELECT *
+    FROM openchpl.questionable_activity_trigger
+    WHERE name = 'Certification Criteria 170.315 (b)(3) added without ICS for current listing');
+
+INSERT INTO openchpl.questionable_activity_trigger
+(name, level, last_modified_user)
+SELECT 'Certification Criteria 170.315 (b)(3) added without ICS for new listing', 'Listing', -1
+WHERE NOT EXISTS (
+    SELECT *
+    FROM openchpl.questionable_activity_trigger
+    WHERE name = 'Certification Criteria 170.315 (b)(3) added without ICS for new listing');
+
+INSERT INTO openchpl.questionable_activity_trigger
+(name, level, last_modified_user)
+SELECT 'ICS added when Certification Criteria 170.315(b)(3) is on a current listing', 'Listing', -1
+WHERE NOT EXISTS (
+    SELECT *
+    FROM openchpl.questionable_activity_trigger
+    WHERE name = 'ICS added when Certification Criteria 170.315(b)(3) is on a current listing');
+
