cd12a76d0cd96b12b5a3c7554fc296eb39cb4598
Andrew Larned <andrew.larned@gmail.com>
2020-01-03 11:30:32 -0500

    feat!: Support four new types of criterion data points
    [#OCD-3224]

---
+++ ocd-3224.sql
@@ -0,0 +1,11 @@
+alter table openchpl.certification_result
+drop column if exists attestation_answer,
+drop column if exists export_documentation,
+drop column if exists documentation_url,
+drop column if exists use_cases;
+
+alter table openchpl.certification_result
+add column attestation_answer bool,
+add column export_documentation varchar(1024),
+add column documentation_url varchar(1024),
+add column use_cases varchar(1024);
