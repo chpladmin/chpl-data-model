3d640fd520342e9a5eef4214fdca3e5b7c6b16f8
Andrew Larned <andrew.larned@gmail.com>
2020-01-06 16:11:55 -0500

    data: add new Certification Criteria
    [#OCD-3224]

--- ocd-3224.sql
+++ ocd-3224.sql
@@ -21,3 +21,12 @@ add column attestation_answer bool,
 add column export_documentation varchar(1024),
 add column documentation_url varchar(1024),
 add column use_cases varchar(1024);
+
+insert into openchpl.certification_criterion (certification_edition_id, number, title, last_modified_user) select 3, '170.315 (b)(10)', 'Clinical Information Export', -1
+       where not exists (select * from openchpl.certification_criterion where number = '170.315 (b)(10)' and title = 'Clinical Information Export');
+insert into openchpl.certification_criterion (certification_edition_id, number, title, last_modified_user) select 3, '170.315 (d)(12)', 'Encrypt Authentication Credentials', -1
+       where not exists (select * from openchpl.certification_criterion where number = '170.315 (d)(12)' and title = 'Encrypt Authentication Credentials');
+insert into openchpl.certification_criterion (certification_edition_id, number, title, last_modified_user) select 3, '170.315 (d)(13)', 'Multi-Factor Authentication', -1
+       where not exists (select * from openchpl.certification_criterion where number = '170.315 (d)(13)' and title = 'Multi-Factor Authentication');
+insert into openchpl.certification_criterion (certification_edition_id, number, title, last_modified_user) select 3, '170.315 (g)(10)', 'Standardized API for Patient and Population Services', -1
+       where not exists (select * from openchpl.certification_criterion where number = '170.315 (g)(10)' and title = 'Standardized API for Patient and Population Services');
