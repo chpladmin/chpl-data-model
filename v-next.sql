a623de10070f5906334a6da883fb6b1fc4311113
Andrew Larned <andrew.larned@gmail.com>
2020-01-27 12:03:14 -0500

    release: deploy version 18.1.0 on 27 January 2020
    [#ver-18.1.0]

--- v-next.sql
+++ v-next.sql
@@ -1,5 +1,3 @@
-\i ocd-3224.sql
-
 \i dev/openchpl_soft-delete.sql
 \i dev/openchpl_views.sql
 \i dev/openchpl_grant-all.sql
--- v18.1.0.sql
+++ v18.1.0.sql
@@ -1,3 +1,6 @@
+-- Deployment file for version 18.1.0
+--     as of 2020-01-27
+-- ocd-3224.sql
 alter table openchpl.certification_result
 drop column if exists attestation_answer cascade,
 drop column if exists export_documentation cascade,
@@ -21,3 +24,8 @@ add column attestation_answer bool,
 add column export_documentation varchar(1024),
 add column documentation_url varchar(1024),
 add column use_cases varchar(1024);
+;
+insert into openchpl.data_model_version (version, deploy_date, last_modified_user) values ('18.1.0', '2020-01-27', -1);
+\i dev/openchpl_soft-delete.sql
+\i dev/openchpl_views.sql
+\i dev/openchpl_grant-all.sql
