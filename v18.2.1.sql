4f575cac5fcf900a1e8dd5f704b00e2e06e4b9b7
tmy1313 <tyoung@ainq.com>
2020-02-20 11:04:21 -0500

    release: deploy version 18.2.1 on 20 February 2020

--- v18.2.1.sql
+++ v18.2.1.sql
@@ -1,3 +1,6 @@
+-- Deployment file for version 18.2.1
+--     as of 2020-02-20
+-- ocd-3238.sql
 INSERT INTO openchpl.url_type
 (name, last_modified_user)
 SELECT 'Export Documentation', -1
@@ -21,3 +24,8 @@ WHERE NOT EXISTS (
     SELECT *
     FROM openchpl.url_type
     WHERE name = 'Use Cases');
+;
+insert into openchpl.data_model_version (version, deploy_date, last_modified_user) values ('18.2.1', '2020-02-20', -1);
+\i dev/openchpl_soft-delete.sql
+\i dev/openchpl_views.sql
+\i dev/openchpl_grant-all.sql
