a86dcf036c97b415135f0ce05f4e5ad54d893575
Andrew Larned <andrew.larned@gmail.com>
2020-03-20 14:22:10 -0400

    refactor: use simpler ID field
    [#OCD-3252]

--- dev/openchpl_data-model.sql
+++ dev/openchpl_data-model.sql
@@ -1413,7 +1413,7 @@ CREATE TABLE openchpl.certification_status_event (
 );
 
 CREATE TABLE openchpl.cures_update_event (
-        cures_update_event_id bigserial NOT NULL,
+        id bigserial NOT NULL,
         cures_update boolean NOT NULL DEFAULT false,
 	certified_product_id bigint NOT NULL,
 	event_date timestamp NOT NULL DEFAULT NOW(),
@@ -1421,7 +1421,7 @@ CREATE TABLE openchpl.cures_update_event (
 	last_modified_date timestamp NOT NULL DEFAULT NOW(),
 	last_modified_user bigint NOT NULL,
 	deleted bool NOT NULL DEFAULT false,
-	CONSTRAINT cures_update_event_pk PRIMARY KEY (cures_update_event_id),
+	CONSTRAINT cures_update_event_pk PRIMARY KEY (id),
 	CONSTRAINT certified_product_fk FOREIGN KEY (certified_product_id) REFERENCES openchpl.certified_product (certified_product_id)
 		MATCH FULL ON DELETE RESTRICT ON UPDATE CASCADE
 );
