09fecc99511898fdda881cd9da8075d71f7bf3de
Katy Ekey <katy.ekey@gmail.com>
2020-04-02 12:36:19 -0400

    fix: use openchpl schema in drop statement

--- ocd-2162.sql
+++ ocd-2162.sql
@@ -2,7 +2,7 @@ insert into openchpl.change_request_type (name, last_modified_user)
 select 'Developer Details Change Request', -1
 where not exists (select * from openchpl.change_request_type where name = 'Developer Details Change Request');
 
-drop table if exists change_request_developer_details;
+drop table if exists openchpl.change_request_developer_details;
 create table openchpl.change_request_developer_details (
     id bigserial NOT NULL,
     change_request_id bigint NOT NULL,
