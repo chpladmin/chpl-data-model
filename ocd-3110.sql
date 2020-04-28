99638f748c1ed12b1cac23e722cc36dc73d498d7
Katy Ekey <katy.ekey@gmail.com>
2020-04-28 08:28:59 -0400

    fix!: Make pending fields 'text' type

---
+++ ocd-3110.sql
@@ -0,0 +1,16 @@
+ALTER TABLE openchpl.pending_certified_product_qms_standard ALTER COLUMN qms_standard_name TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN certification_edition TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN acb_certification_id TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN certification_body_name TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN unique_id TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_name TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN product_name TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN product_version TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_street_address TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_city TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_state TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_zip_code TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_website TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_email TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_contact_name TYPE text;
+ALTER TABLE openchpl.pending_certified_product ALTER COLUMN vendor_phone TYPE text;
