8d95c5f91aa82fd2ad0edc3dacc28ef2f2923ac4
Katy Ekey <katy.ekey@gmail.com>
2020-04-29 14:18:14 -0400

    docs: update comment about missing trigger

--- dev/openchpl_quartz_audit.sql
+++ dev/openchpl_quartz_audit.sql
@@ -7,6 +7,5 @@ CREATE TRIGGER qrtz_blob_triggers_audit AFTER INSERT OR UPDATE OR DELETE on quar
 CREATE TRIGGER qrtz_calendars_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_CALENDARS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER qrtz_paused_trigger_grps_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_PAUSED_TRIGGER_GRPS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER qrtz_fired_triggers_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_FIRED_TRIGGERS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
---this one creates an entry every couple seconds to update the "last checkin time"
---CREATE TRIGGER qrtz_scheduler_state_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_SCHEDULER_STATE FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER qrtz_locks_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_LOCKS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+--not adding a trigger for quartz.QRTZ_SCHEDULER_STATE because it creates an entry every couple seconds to update the "last checkin time"
