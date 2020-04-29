3893e427b3d0e1cf9a6013a835ff96dacf4c6d64
Katy Ekey <katy.ekey@gmail.com>
2020-04-29 14:17:04 -0400

    feat: Add auditing triggers to quartz tables

---
+++ dev/openchpl_quartz_audit.sql
@@ -0,0 +1,12 @@
+CREATE TRIGGER qrtz_job_details_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_JOB_DETAILS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_triggers_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_TRIGGERS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_simple_triggers_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_SIMPLE_TRIGGERS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_cron_triggers_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_CRON_TRIGGERS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_simprop_triggers_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_SIMPROP_TRIGGERS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_blob_triggers_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_BLOB_TRIGGERS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_calendars_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_CALENDARS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_paused_trigger_grps_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_PAUSED_TRIGGER_GRPS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_fired_triggers_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_FIRED_TRIGGERS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+--this one creates an entry every couple seconds to update the "last checkin time"
+--CREATE TRIGGER qrtz_scheduler_state_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_SCHEDULER_STATE FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE TRIGGER qrtz_locks_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_LOCKS FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
--- dev/reset-quartz.sh
+++ dev/reset-quartz.sh
@@ -4,6 +4,7 @@
 if [ $# -lt 2 ]; then
     psql -Upostgres -c "drop schema quartz cascade" openchpl
     psql -Upostgres -f openchpl_quartz.sql openchpl
+	psql -Upostgres -f openchpl_quartz_audit.sql openchpl
     psql -Upostgres -f openchpl_grant-all.sql openchpl
 elif [ $# -eq 2 ]; then
     host=$1
@@ -11,6 +12,7 @@ elif [ $# -eq 2 ]; then
 
     psql -h $host -U $user -c "drop schema quartz cascade" openchpl
     psql -h $host -U $user -f openchpl_quartz.sql openchpl
+	psql -h $host -U $user -f openchpl_quartz_audit.sql openchpl
     psql -h $host -U $user -f openchpl_grant-all.sql openchpl
 elif [ $# -eq 3 ]; then
     host=$1
@@ -19,6 +21,7 @@ elif [ $# -eq 3 ]; then
 
     psql -h $host -U $user -c "drop schema quartz cascade" $db
     psql -h $host -U $user -f openchpl_quartz.sql $db
+	psql -h $host -U $user -f openchpl_quartz_audit.sql $db
     psql -h $host -U $user -f openchpl_grant-all.sql $db
 else
     echo 'This script can be run with 0, 2, or 3 arguments. Please check your input and try again.'
