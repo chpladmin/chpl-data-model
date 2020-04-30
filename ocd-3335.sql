a45903da83f66f2b860367ce885013679d2358b1
Katy Ekey <katy.ekey@gmail.com>
2020-04-30 15:11:46 -0400

    fix: remove spacing

--- ocd-3335.sql
+++ ocd-3335.sql
@@ -33,15 +33,11 @@ CREATE TRIGGER qrtz_locks_audit AFTER INSERT OR UPDATE OR DELETE on quartz.QRTZ_
 -- triggers for ff4j tables
 DROP TRIGGER IF EXISTS ff4j_audit_audit on ff4j.audit;
 CREATE TRIGGER ff4j_audit_audit AFTER INSERT OR UPDATE OR DELETE on ff4j.audit FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
-
 DROP TRIGGER IF EXISTS ff4j_custom_properties_audit on ff4j.custom_properties;
 CREATE TRIGGER ff4j_custom_properties_audit AFTER INSERT OR UPDATE OR DELETE on ff4j.custom_properties FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
-
 DROP TRIGGER IF EXISTS ff4j_features_audit on ff4j.features;
 CREATE TRIGGER ff4j_features_audit AFTER INSERT OR UPDATE OR DELETE on ff4j.features FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
-
 DROP TRIGGER IF EXISTS ff4j_properties_audit on ff4j.properties;
 CREATE TRIGGER ff4j_properties_audit AFTER INSERT OR UPDATE OR DELETE on ff4j.properties FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
-
 DROP TRIGGER IF EXISTS ff4j_roles_audit on ff4j.roles;
 CREATE TRIGGER ff4j_roles_audit AFTER INSERT OR UPDATE OR DELETE on ff4j.roles FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
