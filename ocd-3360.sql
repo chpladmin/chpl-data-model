cf160f86129f92c3027e78ce4ac28fbd31e358b0
Andrew Larned <andrew.larned@gmail.com>
2020-06-08 16:35:22 -0400

    data!: Mark some Questionable Activity concepts as removed
    [#OCD-3360]

---
+++ ocd-3360.sql
@@ -0,0 +1,5 @@
+update openchpl.questionable_activity_trigger set deleted = true where id = 31;
+update openchpl.questionable_activity_trigger set deleted = true where id = 33;
+update openchpl.questionable_activity_trigger set deleted = true where id = 34;
+update openchpl.questionable_activity_trigger set deleted = true where id = 35;
+update openchpl.questionable_activity_trigger set deleted = true where id = 36;
--- v-next.sql
+++ v-next.sql
@@ -1,4 +1,5 @@
 \i ocd-3117.sql
+\i ocd-3360.sql
 
 \i dev/openchpl_soft-delete.sql
 \i dev/openchpl_views.sql
