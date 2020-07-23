916d2236faf1d6d37371c267e0adfaf87aef6bbc
Katy Ekey <katy.ekey@gmail.com>
2020-07-22 20:54:05 -0400

    refactor: make column rename simpler

--- ocd-3395.sql
+++ ocd-3395.sql
@@ -1,16 +1,7 @@
-ALTER TABLE openchpl.api_key
-ADD COLUMN IF NOT EXISTS unrestricted boolean DEFAULT false;
-
 DO $$
 BEGIN
 	IF EXISTS (SELECT * FROM information_schema.columns WHERE table_schema = 'openchpl' AND table_name = 'api_key' AND column_name = 'whitelisted') THEN
-		EXECUTE 'update openchpl.api_key apikey1 set unrestricted = (select whitelisted from openchpl.api_key apikey2 where apikey1.api_key_id = apikey2.api_key_id)';
+		EXECUTE 'ALTER TABLE openchpl.api_key RENAME COLUMN whitelisted TO unrestricted;';
 	END IF;
 END
 $$;
-
-ALTER TABLE openchpl.api_key
-ALTER COLUMN unrestricted SET NOT NULL;
-
-ALTER TABLE openchpl.api_key
-DROP COLUMN IF EXISTS whitelisted;
