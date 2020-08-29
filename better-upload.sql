7112128eb82903c9f663bb3bc26ad865f931776a
Katy Ekey <katy.ekey@gmail.com>
2020-08-29 14:34:42 -0400

    fix: fix issue with script
    [#better-upload]

--- better-upload.sql
+++ better-upload.sql
@@ -1,18 +1,22 @@
+DROP TABLE IF EXISTS openchpl.certified_product_upload;
+
 CREATE TABLE openchpl.certified_product_upload (
 	id bigserial NOT NULL,
 	chpl_product_number text NOT NULL,
 	certification_body_id bigint NOT NULL,
-	error_count integer NOT NULL DEFAULT 0,
-	warning_count integer NOT NULL DEFAULT 0,
-	contents text NOT NULL
+	error_count integer,
+	warning_count integer,
+	contents text NOT NULL,
 	creation_date timestamp NOT NULL DEFAULT NOW(),
 	last_modified_date timestamp NOT NULL DEFAULT NOW(),
 	last_modified_user bigint NOT NULL,
 	deleted bool NOT NULL DEFAULT false,
-	CONSTRAINT certified_product_upload PRIMARY KEY (id),
+	CONSTRAINT certified_product_upload_pk PRIMARY KEY (id),
 	CONSTRAINT certification_body_id_fk FOREIGN KEY (certification_body_id)
       REFERENCES openchpl.certification_body (certification_body_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+\i dev/openchpl_grant-all.sql
 
 --TODO: How to link created certified products to this table? We already have a fk to the old pending table.
