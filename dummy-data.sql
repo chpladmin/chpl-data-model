e54d98abdaecb45de11e77b0d10a6daa7c6d6ec1
tmy1313 <tyoung@ainq.com>
2020-10-28 10:34:29 -0400

    data: switch testing data to be assoc w/ Cures criteria
    OCD-3508

--- dummy-data.sql
+++ dummy-data.sql
@@ -1,22 +1,24 @@
-INSERT INTO openchpl.svap 
+INSERT INTO openchpl.svap
 (regulatory_text_citation, approved_standard_version, last_modified_user)
 VALUES
 ('170.202(a)(2)', 'Applicability Statement for Secure Health Transport, Version 1.2, August 2015 (Direct)', -1);
-    
-INSERT INTO openchpl.svap 
+
+INSERT INTO openchpl.svap
 (regulatory_text_citation, approved_standard_version, last_modified_user)
 VALUES
 ('170.202(d)', 'ONC Implementation Guide for Direct Edge Protocols, Version 1.1, June 25, 2014', -1);
-   
-INSERT INTO openchpl.svap 
+
+INSERT INTO openchpl.svap
 (regulatory_text_citation, approved_standard_version, last_modified_user)
 VALUES
 ('170.205(a)(3)', 'HL7 Implementation Guide for CDA® Release 2: IHE Health Story Consolidation, DSTU Release 1.1 (US Realm) Draft Standard for Trial Use July 2012', -1);
 
+
+
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(1, 16, -1);
+(1, 165, -1);
 
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
@@ -31,7 +33,7 @@ VALUES
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(2, 16, -1);
+(2, 165, -1);
 
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
@@ -41,9 +43,9 @@ VALUES
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(3, 16, -1);
+(3, 165, -1);
 
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(3, 17, -1);
+(3, 166, -1);
