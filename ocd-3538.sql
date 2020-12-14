a485b891367c3e3deedcad81894d9d3db253d702
todd.young <tyoung@ainq.com>
2020-12-14 16:10:24 -0500

    refactor: rearrange and document the table and index changes
    OCD-3538

--- ocd-3538.sql
+++ ocd-3538.sql
@@ -1,6 +1,9 @@
+
+--THIS SCRIPT WILL TAKE A LONG TIME TO RUN!!
+
+--Change datatype so that the column can be indexed on month and year
+ALTER TABLE audit.logged_actions ALTER COLUMN action_tstamp TYPE timestamp WITHOUT time zone;\
+
+--Add indexes on month and year to help with performance
 CREATE INDEX ix_api_key_activity_month_year ON openchpl.api_key_activity (EXTRACT(MONTH FROM creation_date), EXTRACT(YEAR FROM creation_date));
-
---ALTER TABLE audit.logged_actions ALTER COLUMN action_tstamp TYPE timestamp WITHOUT time zone;
-ALTER TABLE audit.logged_actions ALTER COLUMN action_tstamp TYPE timestamp(6) USING action_tstamp::timestamp;
-
 CREATE INDEX ix_logged_actions_month_year ON audit.logged_actions (EXTRACT(MONTH FROM action_tstamp), EXTRACT(YEAR FROM action_tstamp));
