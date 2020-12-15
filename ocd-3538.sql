a8455acb60486f75ad203f3bb041131b2f239121
todd.young <tyoung@ainq.com>
2020-12-15 08:34:57 -0500

    fix: make script re-runnable
    OCD-3538

--- ocd-3538.sql
+++ ocd-3538.sql
@@ -5,5 +5,5 @@
 ALTER TABLE audit.logged_actions ALTER COLUMN action_tstamp TYPE timestamp WITHOUT time zone;
 
 --Add indexes on month and year to help with performance
-CREATE INDEX ix_api_key_activity_month_year ON openchpl.api_key_activity (EXTRACT(MONTH FROM creation_date), EXTRACT(YEAR FROM creation_date));
-CREATE INDEX ix_logged_actions_month_year ON audit.logged_actions (EXTRACT(MONTH FROM action_tstamp), EXTRACT(YEAR FROM action_tstamp));
+CREATE INDEX IF NOT EXISTS ix_api_key_activity_month_year ON openchpl.api_key_activity (EXTRACT(MONTH FROM creation_date), EXTRACT(YEAR FROM creation_date));
+CREATE INDEX IF NOT EXISTS ix_logged_actions_month_year ON audit.logged_actions (EXTRACT(MONTH FROM action_tstamp), EXTRACT(YEAR FROM action_tstamp));
