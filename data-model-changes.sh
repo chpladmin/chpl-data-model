69370a28696868ad55cfad0b63608dafbe8821f4
todd.young <tyoung@ainq.com>
2021-01-06 11:43:20 -0500

    build: add defaulty params for scripts
    OCD-3538

--- data-model-changes.sh
+++ data-model-changes.sh
@@ -2,11 +2,14 @@
 #(set -o igncr) 2>/dev/null
 set -o igncr; # this comment is required to trick cygwin into dealing with windows vs. linux EOL characters
 
+HOST=localhost
+USER=openchpl_dev
+
 function helpFunction {
    echo ""
    echo "Connection options:"
-   echo -e "\t-h database server host or socket directory"
-   echo -e "\t-u database user name"
+   echo -e "\t-h database server host or socket directory.  Default: [localhost]"
+   echo -e "\t-u database user name.  Default: [openchpl_dev]"
    exit 1 # Exit script after printing help
 }
 
