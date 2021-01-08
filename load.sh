1045e6fe489865f60855be6bbbd17fb876e491af
todd.young <tyoung@ainq.com>
2021-01-08 13:30:33 -0500

    fix: add file executions permissions to script
    OCD-3538

--- load.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#(set -o igncr) 2>/dev/null
-set -o igncr; # this comment is required to trick cygwin into dealing with windows vs. linux EOL characters
-
-psql -Uopenchpl_dev -f v-next.sql openchpl
