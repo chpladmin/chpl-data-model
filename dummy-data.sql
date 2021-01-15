1b30f6cdaec90f50fcbfc912bb9ea32769006c3a
todd.young <tyoung@ainq.com>
2021-01-15 11:36:07 -0500

    data: add real SVAP data
    OCD-3508

--- dummy-data.sql
+++ dummy-data.sql
@@ -1,17 +1,22 @@
 INSERT INTO openchpl.svap
-(regulatory_text_citation, approved_standard_version, replaced, last_modified_user)
+(regulatory_text_citation, approved_standard_version, last_modified_user)
 VALUES
-('170.202(a)(2)', 'Applicability Statement for Secure Health Transport, Version 1.2, August 2015 (Direct)', true, -1);
+('170.205(h)(3); 170.205(k)(3)', 'CMS Implementation Guide for Quality Reporting Document Architecture: Category I and Category III; Hospital Quality Reporting, Eligible Clinicians and Eligible Professionals Programs; Implementation Guide for 2021', -1);
 
 INSERT INTO openchpl.svap
 (regulatory_text_citation, approved_standard_version, last_modified_user)
 VALUES
-('170.202(d)', 'ONC Implementation Guide for Direct Edge Protocols, Version 1.1, June 25, 2014', -1);
+('170.204(a)(1)', 'Web Content Accessibility Guidelines (WCAG) 2.1, June 05, 2018 (Level A Conformance)', -1);
 
 INSERT INTO openchpl.svap
 (regulatory_text_citation, approved_standard_version, last_modified_user)
 VALUES
-('170.205(a)(3)', 'HL7 Implementation Guide for CDA® Release 2: IHE Health Story Consolidation, DSTU Release 1.1 (US Realm) Draft Standard for Trial Use July 2012', -1);
+('170.204(a)(2)', 'Web Content Accessibility Guidelines (WCAG) 2.1, June 05, 2018 (Level AA Conformance)', -1);
+
+INSERT INTO openchpl.svap
+(regulatory_text_citation, approved_standard_version, last_modified_user)
+VALUES
+('170.205(s)(1)', 'HL7 CDA® R2 Implementation Guide: National Health Care Surveys (NHCS), R1 STU Release 3 - US Realm', -1);
 
 INSERT INTO openchpl.svap
 (regulatory_text_citation, approved_standard_version, replaced, last_modified_user)
@@ -23,49 +28,29 @@ VALUES
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(1, 165, -1);
+(1, 172, -1);
 
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(1, 59, -1);
+(2, 178, -1);
 
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(1, 60, -1);
+(3, 178, -1);
 
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(2, 165, -1);
+(4, 49, -1);
 
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
-(2, 60, -1);
-
-INSERT INTO openchpl.svap_criteria_map
-(svap_id, criteria_id, last_modified_user)
-VALUES
-(3, 165, -1);
-
-INSERT INTO openchpl.svap_criteria_map
-(svap_id, criteria_id, last_modified_user)
-VALUES
-(3, 166, -1);
-
-INSERT INTO openchpl.svap_criteria_map
-(svap_id, criteria_id, last_modified_user)
-VALUES
-(2, 8, -1);
+(5, 8, -1);
 
 INSERT INTO openchpl.svap_criteria_map
 (svap_id, criteria_id, last_modified_user)
 VALUES
 (3, 8, -1);
-
-INSERT INTO openchpl.svap_criteria_map
-(svap_id, criteria_id, last_modified_user)
-VALUES
-(4, 8, -1);
