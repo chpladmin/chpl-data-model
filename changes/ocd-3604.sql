b2c677829e4e0ff51f76c8aed8db7a92ef2c7d30
Katy Ekey <katy.ekey@gmail.com>
2021-02-01 14:07:45 -0500

    data!: Remove erronously added listing 15.04.04.2964.Inte.18.01.1.200828
    [#OCD-3604]

---
+++ changes/ocd-3604.sql
@@ -0,0 +1,7 @@
+-- load changes to the soft delete triggers before attempting to delete the listing
+\i dev/openchpl_soft-delete.sql
+
+-- delete the listing
+UPDATE openchpl.certified_product
+SET deleted = true
+WHERE certified_product_id = 10439;
