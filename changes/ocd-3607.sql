3b1a99ab1e233f9615cc2d07302a63c9de08949a
Andrew Larned <andrew.larned@gmail.com>
2021-02-15 10:09:15 -0500

    data: add "SVAP Notice URL" type to questionable URL report
    [#OCD-3607]

--- changes/ocd-3607.sql
+++ changes/ocd-3607.sql
@@ -1,2 +1,6 @@
 alter table openchpl.certified_product drop column if exists svap_notice_url cascade;
 alter table openchpl.certified_product add column svap_notice_url text;
+
+insert into openchpl.url_type (name, last_modified_user) select
+'SVAP Notice URL', -1
+where not exists (select name from openchpl.url_type where name = 'SVAP Notice URL');
