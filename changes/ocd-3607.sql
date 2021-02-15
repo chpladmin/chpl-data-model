ae578d2aa28ab5955f5149d24ea8453673a6a640
Andrew Larned <andrew.larned@gmail.com>
2021-02-15 10:08:59 -0500

    ci: cascade drop to support update of views
    [#OCD-3607]

--- changes/ocd-3607.sql
+++ changes/ocd-3607.sql
@@ -1,2 +1,2 @@
-alter table openchpl.certified_product drop column if exists svap_notice_url;
+alter table openchpl.certified_product drop column if exists svap_notice_url cascade;
 alter table openchpl.certified_product add column svap_notice_url text;
