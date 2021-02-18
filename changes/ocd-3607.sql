f1024a47386c2e88941e37fe88fc310eb43cc097
Andrew Larned <andrew.larned@gmail.com>
2021-02-18 11:38:33 -0500

    fix: use correct name for new URL type
    [#OCD-3607]

--- changes/ocd-3607.sql
+++ changes/ocd-3607.sql
@@ -2,8 +2,8 @@ alter table openchpl.certified_product drop column if exists svap_notice_url cas
 alter table openchpl.certified_product add column svap_notice_url text;
 
 insert into openchpl.url_type (name, last_modified_user) select
-'SVAP Notice', -1
-where not exists (select name from openchpl.url_type where name = 'SVAP Notice');
+'Standards Version Advancement Process Notice', -1
+where not exists (select name from openchpl.url_type where name = 'Standards Version Advancement Process Notice');
 
 update openchpl.url_type set name = 'Documentation' where name = 'Documentation URL';
 update openchpl.url_type set name = 'Mandatory Disclosure' where name = 'Mandatory Disclosure URL';
