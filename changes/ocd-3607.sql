a122238f75387a4d3ce3cb659ea44660d2b0522d
Andrew Larned <andrew.larned@gmail.com>
2021-02-18 09:13:08 -0500

    data: change name of Questionable URL URLs
    [#OCD-3607]

--- changes/ocd-3607.sql
+++ changes/ocd-3607.sql
@@ -2,5 +2,8 @@ alter table openchpl.certified_product drop column if exists svap_notice_url cas
 alter table openchpl.certified_product add column svap_notice_url text;
 
 insert into openchpl.url_type (name, last_modified_user) select
-'SVAP Notice URL', -1
-where not exists (select name from openchpl.url_type where name = 'SVAP Notice URL');
+'SVAP Notice', -1
+where not exists (select name from openchpl.url_type where name = 'SVAP Notice');
+
+update openchpl.url_type set name = 'Documentation' where name = 'Documentation URL';
+update openchpl.url_type set name = 'Mandatory Disclosure' where name = 'Mandatory Disclosure URL';
