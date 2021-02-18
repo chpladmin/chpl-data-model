024194498856ba027d470f43f23d9c6c960a9b8a
Katy Ekey <katy.ekey@gmail.com>
2021-02-18 10:04:47 -0500

    feat!: Remove references to unused g1/g2 macra measure tables
    OCD-3573

---
+++ changes/ocd-3573.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS openchpl.certification_result_g1_macra;
+DROP TABLE IF EXISTS openchpl.certification_result_g2_macra;
+DROP TABLE IF EXISTS openchpl.pending_certification_result_g1_macra;
+DROP TABLE IF EXISTS openchpl.pending_certification_result_g2_macra;
--- dev/openchpl_soft-delete.sql
+++ dev/openchpl_soft-delete.sql
@@ -54,8 +54,6 @@ CREATE OR REPLACE FUNCTION openchpl.certification_result_soft_delete()
 RETURNS TRIGGER AS $$
 BEGIN
     UPDATE openchpl.certification_result_additional_software as src SET deleted = NEW.deleted WHERE src.certification_result_id = NEW.certification_result_id;
-    UPDATE openchpl.certification_result_g1_macra as src SET deleted = NEW.deleted WHERE src.certification_result_id = NEW.certification_result_id;
-    UPDATE openchpl.certification_result_g2_macra as src SET deleted = NEW.deleted WHERE src.certification_result_id = NEW.certification_result_id;
     UPDATE openchpl.certification_result_svap as src SET deleted = NEW.deleted WHERE src.certification_result_id = NEW.certification_result_id;
     UPDATE openchpl.certification_result_test_data as src SET deleted = NEW.deleted WHERE src.certification_result_id = NEW.certification_result_id;
     UPDATE openchpl.certification_result_test_functionality as src SET deleted = NEW.deleted WHERE src.certification_result_id = NEW.certification_result_id;
