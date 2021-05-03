badbc6310783bf7d9f5bce2bc4a844ca01f2ab97
Katy Ekey <katy.ekey@gmail.com>
2021-05-03 16:32:53 -0400

    [#OCD-3594]

--- changes/ocd-3594.sql
+++ changes/ocd-3594.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS openchpl.job;
-DROP TABLE IF EXISTS openchpl.job_message;
-DROP TABLE IF EXISTS openchpl.job_status;
-DROP TABLE IF EXISTS openchpl.job_type;
+DROP TABLE IF EXISTS openchpl.job CASCADE;
+DROP TABLE IF EXISTS openchpl.job_message CASCADE;
+DROP TABLE IF EXISTS openchpl.job_status CASCADE;
+DROP TABLE IF EXISTS openchpl.job_type CASCADE;
