805e2027dd31e5d084d9bcbef5550ac50a86776b
Andrew Larned <andrew.larned@gmail.com>
2021-05-28 12:11:52 -0400

    docs: indicate which criteria are set up for OS
    [#OCD-3621]

--- changes/ocd-3621.sql
+++ changes/ocd-3621.sql
@@ -47,12 +47,12 @@ alter table openchpl.certification_criterion_attribute drop column if exists opt
 alter table openchpl.certification_criterion_attribute add column optional_standard bool NOT NULL default false;
 
 -- add rows for criteria that don't yet have attributes
-insert into openchpl.certification_criterion_attribute (criterion_id, last_modified_user) select 8, -1 where not exists (select * from openchpl.certification_criterion_attribute where criterion_id = 8); --a12
-insert into openchpl.certification_criterion_attribute (criterion_id, last_modified_user) select 9, -1 where not exists (select * from openchpl.certification_criterion_attribute where criterion_id = 9); --a13
-insert into openchpl.certification_criterion_attribute (criterion_id, last_modified_user) select 12, -1 where not exists (select * from openchpl.certification_criterion_attribute where criterion_id = 12); --b1
+insert into openchpl.certification_criterion_attribute (criterion_id, last_modified_user) select 8, -1 where not exists (select * from openchpl.certification_criterion_attribute where criterion_id = 8); --a8
+insert into openchpl.certification_criterion_attribute (criterion_id, last_modified_user) select 9, -1 where not exists (select * from openchpl.certification_criterion_attribute where criterion_id = 9); --a9
+insert into openchpl.certification_criterion_attribute (criterion_id, last_modified_user) select 12, -1 where not exists (select * from openchpl.certification_criterion_attribute where criterion_id = 12); --a12
 
 -- update data for new attribute
-update openchpl.certification_criterion_attribute set optional_standard = true where criterion_id = 8; --a12
-update openchpl.certification_criterion_attribute set optional_standard = true where criterion_id = 9; --a13
-update openchpl.certification_criterion_attribute set optional_standard = true where criterion_id = 12; --b1
+update openchpl.certification_criterion_attribute set optional_standard = true where criterion_id = 8; --a8
+update openchpl.certification_criterion_attribute set optional_standard = true where criterion_id = 9; --a9
+update openchpl.certification_criterion_attribute set optional_standard = true where criterion_id = 12; --a12
 update openchpl.certification_criterion_attribute set optional_standard = true where criterion_id = 21; --b6
