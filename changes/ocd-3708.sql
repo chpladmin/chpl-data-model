51ecd0e09142c33aabb14ae9e18cc6ecc77e117a
todd.young <tyoung@ainq.com>
2021-07-02 13:16:36 -0400

    data: change the questionbable activity trigger name
    OCD-3708

--- changes/ocd-3708.sql
+++ changes/ocd-3708.sql
@@ -1,8 +1,11 @@
 INSERT INTO openchpl.questionable_activity_trigger
 (name, level, last_modified_user)
-SELECT 'Real World Testing Added to Listing not Real World Testing Eligible', 'Listing', -1
+SELECT 'Real World Testing Added To Ineligible Listing', 'Listing', -1
 WHERE NOT EXISTS (
     SELECT *
     FROM openchpl.questionable_activity_trigger
-    WHERE name = 'Real World Testing Added to Listing not Real World Testing Eligible'
+    WHERE name = 'Real World Testing Added To Ineligible Listing'
 );
+
+DELETE FROM openchpl.questionable_activity_trigger
+WHERE name = 'Real World Testing Added to Listing not Real World Testing Eligible';
