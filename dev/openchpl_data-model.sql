9e6f5397a780fd75f3585ae82d4c41324af57cb4
Todd Young <tyoung@ainq.com>
2021-10-19 10:11:27 -0400

    feat!: Remove the unused nonconformity_status table and related columns from the database
    OCD-3745

--- dev/openchpl_data-model.sql
+++ dev/openchpl_data-model.sql
@@ -2009,17 +2009,6 @@ CREATE TABLE openchpl.surveillance_result (
 	CONSTRAINT surveillance_result_name_key UNIQUE (name)
 );
 
-CREATE TABLE openchpl.nonconformity_status (
-	id bigserial not null,
-	name varchar(50) not null,
-	creation_date timestamp NOT NULL DEFAULT NOW(),
-	last_modified_date timestamp NOT NULL DEFAULT NOW(),
-	last_modified_user bigint NOT NULL,
-	deleted bool NOT NULL DEFAULT false,
-	CONSTRAINT nonconformity_status_pk PRIMARY KEY (id),
-	CONSTRAINT nonconformity_status_name_key UNIQUE (name)
-);
-
 CREATE TABLE openchpl.surveillance (
 	id bigserial not null,
 	certified_product_id bigint not null,
@@ -2078,7 +2067,6 @@ CREATE TABLE openchpl.surveillance_nonconformity (
 	-- either criteria or type is required
 	certification_criterion_id bigint,
 	nonconformity_type varchar(1024),
-	nonconformity_status_id bigint,
 	non_conformity_close_date date,
 	date_of_determination date not null,
 	corrective_action_plan_approval_date date,
