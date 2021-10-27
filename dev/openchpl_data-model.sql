dc70cb6b58859bd1101acf87b0512de989b29101
Todd Young <tyoung@ainq.com>
2021-10-27 10:35:44 -0400

    feat!: Add tables to support Cures Reporting Charts 4-6
    OCD-3693

--- dev/openchpl_data-model.sql
+++ dev/openchpl_data-model.sql
@@ -3562,6 +3562,44 @@ CREATE UNIQUE INDEX deprecated_api_usage_unique_api_key_and_deprecated_api
 ON openchpl.deprecated_api_usage(api_key_id, deprecated_api_id)
 WHERE deleted = false;
 
+CREATE TABLE openchpl.cures_criteria_statistics_by_acb (
+	id bigserial NOT NULL,
+	certification_body_id bigint NOT NULL,
+	original_criterion_id bigint,
+	cures_criterion_id bigint,
+	original_criterion_upgraded_count bigint,
+	cures_criterion_created_count bigint,
+    criteria_needing_upgrade_count bigint,
+	statistic_date timestamp NOT NULL DEFAULT NOW(),
+	CONSTRAINT cures_criteria_statistics_by_acb_pk PRIMARY KEY (id),
+	CONSTRAINT certification_body_id_fk FOREIGN KEY (certification_body_id)
+      REFERENCES openchpl.certification_body (certification_body_id) MATCH FULL
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT original_criterion_id_fk FOREIGN KEY (original_criterion_id)
+      REFERENCES openchpl.certification_criterion (certification_criterion_id) MATCH FULL
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT cures_criterion_id_fk FOREIGN KEY (cures_criterion_id)
+      REFERENCES openchpl.certification_criterion (certification_criterion_id) MATCH FULL
+      ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE openchpl.cures_listing_statistics_by_acb (
+	id bigserial NOT NULL,
+	certification_body_id bigint NOT NULL,
+	cures_listing_without_cures_criteria_count bigint,
+	cures_listing_withcures_criteria_count bigint,
+	non_cures_listing_count bigint,
+	statistic_date timestamp NOT NULL DEFAULT NOW(),
+	creation_date timestamp NOT NULL DEFAULT NOW(),
+	last_modified_date timestamp NOT NULL DEFAULT NOW(),
+	last_modified_user bigint NOT NULL,
+	deleted bool NOT NULL DEFAULT false,
+	CONSTRAINT cures_listing_statistics_pk PRIMARY KEY (id),
+	CONSTRAINT certification_body_id_fk FOREIGN KEY (certification_body_id)
+      REFERENCES openchpl.certification_body (certification_body_id) MATCH FULL
+      ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
 CREATE TABLE openchpl.deprecated_response_field_api (
 	id bigserial NOT NULL,
 	http_method varchar(10) NOT NULL,
