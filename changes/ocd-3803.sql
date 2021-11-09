720fa98c829343c749ffdef8ae24b897cb3fd6d2
todd.young <tyoung@ainq.com>
2021-11-09 17:12:22 -0500

    refactor: remove extra blank line
    OCD-3803

--- changes/ocd-3803.sql
+++ changes/ocd-3803.sql
@@ -5,4 +5,3 @@ where citation = 'CCDS Ref: 170.207(a)(4)';
 update openchpl.optional_standard
 set description = 'USCDI: SNOMED CT® U.S. Edition, September 2019 Release'
 where citation = 'USCDI Ref: 170.207(a)(4)';
-
