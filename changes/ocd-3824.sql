a5fe2097b67bad65f6403f9b30910925ee7f40b7
Katy Ekey <katy.ekey@gmail.com>
2022-02-02 10:19:40 -0500

    feat: Add notes about what has to be done
    [#OCD-3824]

---
+++ changes/ocd-3824.sql
@@ -0,0 +1,8 @@
+-- delete "developerCollection" cache from ehcache.xml ??
+-- change email address for jira api key to alexandr or Chai CSrinadhu@ainq.com
+-- /collections/developers looks like it needs the developer name and id
+-- delete data from deprecated response field table
+-- delete views 
+-- delete tables
+-- delete columns
+-- delete transparency attestation type (note the name of that type was changed in attestations ticket)
