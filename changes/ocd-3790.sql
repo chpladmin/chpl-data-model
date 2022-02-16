0de9ac8d7927f6258388871705446a116b4bc240
todd.young <tyoung@ainq.com>
2022-02-16 15:01:38 -0500

    feat: truncate developer attestation and change request data, set default at periods
    OCD-3790

--- changes/ocd-3790.sql
+++ changes/ocd-3790.sql
@@ -1,3 +1,15 @@
+DROP TABLE IF EXISTS openchpl.attestation_period_developer_exception;
+
+TRUNCATE openchpl.change_request_website,
+	openchpl.change_request_developer_details,
+	openchpl.change_request_attestation_submission,
+	openchpl.change_request_attestation_response,
+	openchpl.change_request_status,
+	openchpl.change_request,
+	openchpl.developer_attestation_response,
+	openchpl.developer_attestation_submission, 
+	openchpl.attestation_period RESTART IDENTITY;
+
 CREATE TABLE IF NOT EXISTS openchpl.attestation_period_developer_exception (
 	id bigserial NOT NULL,
 	attestation_period_id bigint NOT NULL,
@@ -19,21 +31,19 @@ CREATE TABLE IF NOT EXISTS openchpl.attestation_period_developer_exception (
 CREATE TRIGGER attestation_period_developer_exception_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.attestation_period_developer_exception FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
 CREATE TRIGGER attestation_period_developer_exception_timestamp BEFORE UPDATE on openchpl.attestation_period_developer_exception FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
 
-UPDATE openchpl.attestation_period
-SET period_start='2021-01-01', 
-	period_end='2022-01-31', 
-	submission_start='2022-02-01', 
-	submission_end='2022-02-14', 
-	description='First Period', 
-	creation_date=now(), 
-	last_modified_date=now(), 
-	last_modified_user=-1
-WHERE id = 1;
+
+-- Set the correct attestation periods for go live!
+INSERT INTO openchpl.attestation_period (period_start, period_end, submission_start, submission_end, description, last_modified_user)
+SELECT '2021-01-01', '2022-03-30', '2022-04-01', '2022-04-30', 'First Period', -1
+WHERE NOT EXISTS (
+	SELECT *
+	FROM openchpl.attestation_period
+	WHERE description = 'First Period');
+
 
 INSERT INTO openchpl.attestation_period (period_start, period_end, submission_start, submission_end, description, last_modified_user)
 SELECT '2022-02-01', '2022-09-30', '2022-10-01', '2022-10-30', 'Second Period', -1
 WHERE NOT EXISTS (
 	SELECT *
 	FROM openchpl.attestation_period
-	WHERE period_start = '2022-02-01'
-	AND period_end = '2022-09-30');
+	WHERE description = 'Second Period');
