24c5afd485003b1e08dadf9e74ab15e674995ebe
Katy Ekey <katy.ekey@gmail.com>
2022-02-25 11:00:25 -0500

    data!: Remove deprecated response fields from /search/beta and add /search/beta as a deprecated endpoint
    [#OCD-3850]

--- changes/ocd-3850.sql
+++ changes/ocd-3850.sql
@@ -1,5 +1,4 @@
 -- remove deprecated response field API from /search/beta
--- the deprecated fields are marked deleted from a different table with the soft_delete trigger
 UPDATE openchpl.deprecated_response_field_api
 SET deleted = false
 WHERE api_operation = '/search/beta';
@@ -16,3 +15,20 @@ WHERE deprecated_response_field_api_id IN(
 		WHERE api_operation = '/search/beta'));
 
 -- add /search/beta as deprecated endpoint
+INSERT INTO openchpl.deprecated_api (http_method, api_operation, request_parameter, change_description, removal_date, last_modified_user)
+SELECT 'GET',
+	'/search/beta',
+	NULL,
+	'This endpoint is deprecated and will be removed in a future release. Please use /search/v2 to perform searches.',
+	'2022-09-07',
+	-1
+WHERE NOT EXISTS (SELECT * FROM openchpl.deprecated_api WHERE http_method = 'GET' and api_operation LIKE '/search/beta');
+
+INSERT INTO openchpl.deprecated_api (http_method, api_operation, request_parameter, change_description, removal_date, last_modified_user)
+SELECT 'POST',
+	'/search/beta',
+	NULL,
+	'This endpoint is deprecated and will be removed in a future release. Please POST to /search/v2 to perform searches.',
+	'2022-09-07',
+	-1
+WHERE NOT EXISTS (SELECT * FROM openchpl.deprecated_api WHERE http_method = 'POST' and api_operation LIKE '/search/beta');
