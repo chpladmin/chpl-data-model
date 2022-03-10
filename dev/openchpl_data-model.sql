468e19da6e651a23616176678514e3571b11e8fa
Katy Ekey <katy.ekey@gmail.com>
2022-03-10 14:11:04 -0500

    fix: remove duplicate conformance_method_criteria_map
    [#OCD-3751]

--- dev/openchpl_data-model.sql
+++ dev/openchpl_data-model.sql
@@ -855,24 +855,6 @@ CREATE TABLE openchpl.test_data_criteria_map (
 );
 
 CREATE TABLE openchpl.certification_result_conformance_method (
-	id bigserial NOT NULL,
-	criteria_id bigint NOT NULL,
-	conformance_method_id bigint NOT NULL,
-	creation_date timestamp NOT NULL DEFAULT NOW(),
-	last_modified_date timestamp NOT NULL DEFAULT NOW(),
-	last_modified_user bigint NOT NULL,
-	deleted bool NOT NULL DEFAULT false,
-	CONSTRAINT conformance_method_criteria_map_pk PRIMARY KEY (id),
-	CONSTRAINT conformance_method_criteria_fk FOREIGN KEY (criteria_id)
-		REFERENCES openchpl.certification_criterion (certification_criterion_id)
-		MATCH FULL ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT conformance_method_fk FOREIGN KEY (conformance_method_id)
-		REFERENCES openchpl.conformance_method (id)
-		MATCH FULL ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS openchpl.certification_result_conformance_method
-(
     id bigint NOT NULL,
     certification_result_id bigint NOT NULL,
     conformance_method_id bigint NOT NULL,
