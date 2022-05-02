e09dc0da6cc129aa7e9ab74bf974807dd546e62e
todd.young <tyoung@ainq.com>
2022-05-02 10:43:06 -0400

    feat: rename table and add website column
    OCD-3894

---
+++ changes/ocd-3894.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS openchpl.change_request_website;
+DROP TABLE IF EXISTS openchpl.change_request_developer_details;
+
+CREATE TABLE IF NOT EXISTS openchpl.change_request_developer_demographic (
+	id bigserial NOT NULL,
+	change_request_id bigint NOT NULL,
+	self_developer boolean,
+ 	website text,
+	street_line_1 varchar(250),
+	street_line_2 varchar(250),
+	city varchar(250),
+	state varchar(250),
+	zipcode varchar(25),
+	country varchar(250),
+	full_name varchar(500),
+	email varchar(250),
+	phone_number varchar(100),
+	title varchar(250),
+	creation_date timestamp NOT NULL DEFAULT NOW(),
+	last_modified_date timestamp NOT NULL DEFAULT NOW(),
+	last_modified_user bigint NOT NULL,
+	deleted bool NOT NULL DEFAULT false,
+	CONSTRAINT change_request_developer_demographic_pk PRIMARY KEY (id),
+	CONSTRAINT change_request_fk FOREIGN KEY (change_request_id)
+	    REFERENCES openchpl.change_request (id)
+        MATCH SIMPLE ON UPDATE NO ACTION ON DELETE RESTRICT
+);
+
