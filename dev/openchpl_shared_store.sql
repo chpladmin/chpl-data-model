2c4c26e4f072a63725cff1c3ebd3ad4e26f7e59d
Todd Young <tyoung@ainq.com>
2022-05-12 13:01:06 -0400

    feat!: Add schema and table to support 'shared-data'
    OCD-3934

---
+++ dev/openchpl_shared_store.sql
@@ -0,0 +1,15 @@
+-- SCHEMA: shared_store
+
+CREATE SCHEMA shared_store
+    AUTHORIZATION openchpl_dev;
+
+GRANT ALL ON SCHEMA shared_store TO openchpl_dev;
+GRANT USAGE ON SCHEMA shared_store TO openchpl;
+
+CREATE TABLE shared_store.shared_store (
+	type TEXT NOT NULL,
+	key TEXT NOT NULL,
+	value TEXT,
+	put_date TIMESTAMP NOT NULL DEFAULT NOW()
+	PRIMARY KEY(type, key)
+);
