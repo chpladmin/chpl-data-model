f0e180259b6dd2850590458fa9ca75de5617780b
Andrew Larned <andrew.larned@gmail.com>
2022-08-04 13:12:39 -0400

    fix!: Allow SVAP to be associated with correct criteria
    [#OCD-4004]

---
+++ changes/ocd-4004.sql
@@ -0,0 +1,5 @@
+update openchpl.certification_criterion_attribute
+set svap = true where criterion_id = 181;
+
+update openchpl.certification_criterion_attribute
+set svap = false where criterion_id = 58;
