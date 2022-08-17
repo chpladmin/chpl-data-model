09d8d5f257509f91f86d971b7d37506aeaa29088
todd.young <tyoung@ainq.com>
2022-08-17 14:03:21 -0400

    data: update the optional question with markdown to indicate bold text
    OCD-4021

--- changes/ocd-4021.sql
+++ changes/ocd-4021.sql
@@ -4,3 +4,6 @@ update openchpl.form
 set instructions = 'If "Noncompliant" is selected, you may, but are not required to, indicate the status of a Corrective Action Plan (CAP) under the Certification Program.'
 where description = 'Attestation Period 2022-04-01 to 2022-09-30';
 
+update openchpl.question
+set question = 'For a selection of "Noncompliant", please indicate the status of a Corrective Action Plan (CAP) under the Certification Program. **(optional - check all that apply)**'
+where question = 'For a selection of "Noncompliant", please indicate the status of a Corrective Action Plan (CAP) under the Certification Program. (optional - check all that apply)';
