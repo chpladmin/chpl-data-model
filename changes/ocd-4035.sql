c44c37c0d0d9cb7e90c526bcec3355b90816e9d6
Andrew Larned <andrew.larned@gmail.com>
2022-08-25 11:17:43 -0400

    data!: Allow SVAP to be connected to h1 and h2
    [#OCD-4035]

---
+++ changes/ocd-4035.sql
@@ -0,0 +1,9 @@
+-- not needed?
+--update openchpl.certification_criterion_attribute
+--set svap = true where criterion_id = 170;
+
+update openchpl.certification_criterion_attribute
+set svap = true where criterion_id = 59;
+
+update openchpl.certification_criterion_attribute
+set svap = true where criterion_id = 60;
