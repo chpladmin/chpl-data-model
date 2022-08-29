93e86d8d165ec2d654d677a0477e61029762b5ef
todd.young <tyoung@ainq.com>
2022-08-29 09:37:58 -0400

    feat: add missing column and data conversion
    OCD-4029

--- changes/ocd-4029.sql
+++ changes/ocd-4029.sql
@@ -9,6 +9,9 @@ create table if not exists openchpl.additional_nonconformity_type(
     constraint additional_nonconformity_type_pk primary key (id)
 );
 
+alter table openchpl.surveillance_nonconformity
+add column if not exists nonconformity_type_id bigint;
+
 insert into openchpl.additional_nonconformity_type(name, removed, last_modified_user) 
 select '170.523 (k)(1)', false, -1
 where not exists (
@@ -73,9 +76,9 @@ where not exists (
 );
 
 create or replace view openchpl.nonconformity_type as  
-	select certification_criterion_id as id, certification_edition_id, number, title, removed 
+	select certification_criterion_id as id, certification_edition_id, number, title, removed, 'CRITERION' as classification
 	from openchpl.certification_criterion
 	where certification_edition_id in (3,2)
 	union
-	select id, null, name, null, removed
+	select id, null, name, null, removed, 'REQUIREMENT'
 	from openchpl.additional_nonconformity_type;
