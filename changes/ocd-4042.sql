0ac91d84dd759757d7f4a80d83700e1067c4c919
Katy Ekey <katy.ekey@gmail.com>
2022-09-06 12:49:38 -0400

    data!: Append b10, d12, d13, and g10 with "(Cures Update)" title
    [#OCD-4042]

---
+++ changes/ocd-4042.sql
@@ -0,0 +1,15 @@
+UPDATE openchpl.certification_criterion
+SET title = 'Electronic Health Information Export (Cures Update)'
+WHERE number = '170.315 (b)(10)';
+
+UPDATE openchpl.certification_criterion
+SET title = 'Encrypt Authentication Credentials (Cures Update)'
+WHERE number = '170.315 (d)(12)';
+
+UPDATE openchpl.certification_criterion
+SET title = 'Multi-Factor Authentication (Cures Update)'
+WHERE number = '170.315 (d)(13)';
+
+UPDATE openchpl.certification_criterion
+SET title = 'Standardized API for Patient and Population Services (Cures Update)'
+WHERE number = '170.315 (g)(10)';
