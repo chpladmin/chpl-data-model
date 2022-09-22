b9e07c78a4266acd4cb9e1f1c637d6d45fe1ea56
todd.young <tyoung@ainq.com>
2022-09-22 13:15:01 -0400

    feat: change order of drops to correctl handle dependencies
    OCD-4026

--- changes/ocd-4026.sql
+++ changes/ocd-4026.sql
@@ -3,7 +3,7 @@ alter table openchpl.attestation_submission drop column if exists drop_developer
 drop table if exists openchpl.developer_attestation_response;
 drop table if exists openchpl.developer_attestation_submission;
 drop table if exists openchpl.change_request_attestation_response;
-drop table if exists openchpl.attestation_valid_response;
 drop table if exists openchpl.attestation_form;
-drop table if exists openchpl.attestation_condition;
 drop table if exists openchpl.attestation;
+drop table if exists openchpl.attestation_valid_response;
+drop table if exists openchpl.attestation_condition;
