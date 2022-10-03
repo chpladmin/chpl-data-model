73535feb79ec66d887eb76206a24d98f1abf554c
Andrew Larned <andrew.larned@gmail.com>
2022-10-03 13:36:35 -0400

    release: deploy version 21.1.0 on 03 October 2022
    [#ver-21.1.0]

--- changes/ocd-4026.sql
+++
@@ -1,9 +0,0 @@
-alter table openchpl.attestation_submission drop column if exists drop_developer_attestation_submission_id;
-
-drop table if exists openchpl.developer_attestation_response;
-drop table if exists openchpl.developer_attestation_submission;
-drop table if exists openchpl.change_request_attestation_response;
-drop table if exists openchpl.attestation_form;
-drop table if exists openchpl.attestation;
-drop table if exists openchpl.attestation_valid_response;
-drop table if exists openchpl.attestation_condition;
