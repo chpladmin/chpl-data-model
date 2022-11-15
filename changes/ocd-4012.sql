1545b981e2735208c77fea92d1d35e72642a04d6
todd.young <tyoung@ainq.com>
2022-11-15 14:19:42 -0500

    feat!: remove questionable trigger types that are no longer used
    OCD-4012

--- changes/ocd-4012.sql
+++ changes/ocd-4012.sql
@@ -1,1 +1,4 @@
 drop table if exists openchpl.surveillance_nonconformity_document;
+
+delete from openchpl.questionable_activity_trigger where name = 'Removed Non-Conformity added to Surveillance';
+delete from openchpl.questionable_activity_trigger where name = 'Removed Requirement added to Surveillance';
