f1d8fbbafc72f9da246ca7d1da38e76f59c0996d
todd.young <tyoung@ainq.com>
2022-11-17 12:31:18 -0500

    feat: drop "pending" surveillance tables
    OCD-4012

--- changes/ocd-4012.sql
+++ changes/ocd-4012.sql
@@ -1,4 +1,9 @@
 drop table if exists openchpl.surveillance_nonconformity_document;
 
+drop table if exists openchpl.pending_surveillance_validation;
+drop table if exists openchpl.pending_surveillance_nonconformity;
+drop table if exists openchpl.pending_surveillance_requirement;
+drop table if exists openchpl.pending_surveillance;
+
 delete from openchpl.questionable_activity_trigger where name = 'Removed Non-Conformity added to Surveillance';
 delete from openchpl.questionable_activity_trigger where name = 'Removed Requirement added to Surveillance';
