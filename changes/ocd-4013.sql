16f443cb259c5007776d8da67770c308bd8f4899
Katy Ekey <katy.ekey@gmail.com>
2022-12-01 06:27:46 -0500

    data!: Remove extra space in complainant type name
    Third  Party Organization -> Third Party Organization.
    The UI automatically removes the extra space between "Third" and "Party"
    so when filtering on that complainant type value the UI was sending in a
    value that does not match what is in our database and getting no search
    results.
    
    [#OCD-4013]

--- changes/ocd-4013.sql
+++ changes/ocd-4013.sql
@@ -1,3 +1,7 @@
 UPDATE openchpl.complainant_type
 SET name = 'Other'
 WHERE name = 'Other - [Please Describe]';
+
+UPDATE openchpl.complainant_type
+SET name = 'Third Party Organization'
+WHERE name = 'Third  Party Organization';
