e421699b5b9a7706407890b516d27ba77dfd1a7c
todd.young <tyoung@ainq.com>
2022-12-15 20:54:32 -0500

    data: delete or remove "(m)(*)" surv nonconformities
    OCD-4105

--- changes/ocd-4105.sql
+++ changes/ocd-4105.sql
@@ -1,8 +1,11 @@
-do $$ 
+do $$
 declare
   	m_requirement_count integer := 0;
+  	m_nonconformity_count integer := 0;
   	affected_count integer;
 begin
+	raise notice '******  REQUIREMENT TYPE *******';
+
 	select count(*)
 	into m_requirement_count
 	from openchpl.surveillance_requirement sr
@@ -17,10 +20,10 @@ begin
 
   	raise notice 'The number of surveillance requirements is %', m_requirement_count;
 
-  	if m_requirement_count > 0 then
+  	if m_requirement_count = 0 then
   		raise notice 'Need to "remove" the requirements';
   		update openchpl.additional_requirement_type
-  		set removed = true
+  		set removed = true 
   		where name in ('170.523 (m)(1): Adaptations and updates',
    						'170.523 (m)(2): Adaptations and updates',
    						'170.523 (m)(3): Adaptations and updates',
@@ -31,7 +34,7 @@ begin
   	else
   		raise notice 'Need to "delete" the requirements';
   		update openchpl.additional_requirement_type
-  		set deleted = true
+  		set deleted = true 
   		where name in ('170.523 (m)(1): Adaptations and updates',
    						'170.523 (m)(2): Adaptations and updates',
    						'170.523 (m)(3): Adaptations and updates',
@@ -39,8 +42,76 @@ begin
    						'170.523 (m)(5): Adaptations and updates');
    		get diagnostics affected_count = row_count;
 		raise notice 'Deleted % requirement type(s)', affected_count;
-
   	end if;
 
- 	raise notice 'Completed';
+  	insert into openchpl.additional_requirement_type (requirement_group_type_id, name, removed, last_modified_user)
+  	select 6, '170.523(m) Adaptations and updates', false, -1
+  	where not exists 
+  		(select *
+  		from openchpl.additional_requirement_type
+  		where name = '170.523(m) Adaptations and updates');
+
+  	get diagnostics affected_count = row_count;
+
+  	if affected_count = 0 then
+  		raise notice '170.523(m) requirement already exists';
+  	else
+  		raise notice '170.523(m) requirement added';
+  	end if;
+
+  raise notice '******  NONCONFORMITY TYPE *******';
+
+	select count(*)
+	into m_nonconformity_count
+	from openchpl.surveillance_nonconformity sn
+	where nonconformity_type_id in
+   		(select id 
+   		from openchpl.nonconformity_type
+   		where title in ('170.523 (m)(1): Adaptations and updates',
+   						'170.523 (m)(2): Adaptations and updates',
+   						'170.523 (m)(3): Adaptations and updates',
+   						'170.523 (m)(4): Adaptations and updates',
+   						'170.523 (m)(5): Adaptations and updates'));
+
+  	raise notice 'The number of surveillance nonconforities is %', m_nonconformity_count;
+
+  	if m_nonconformity_count = 0 then
+  		raise notice 'Need to "remove" the nonconformities';
+  		update openchpl.additional_nonconformity_type
+  		set removed = true 
+  		where name in ('170.523 (m)(1): Adaptations and updates',
+   						'170.523 (m)(2): Adaptations and updates',
+   						'170.523 (m)(3): Adaptations and updates',
+   						'170.523 (m)(4): Adaptations and updates',
+   						'170.523 (m)(5): Adaptations and updates');
+   		get diagnostics affected_count = row_count;
+		raise notice 'Removed % nonconformity type(s)', affected_count;
+  	else
+  		raise notice 'Need to "delete" the nonconformities';
+  		update openchpl.additional_nonconformity_type
+  		set deleted = true 
+  		where name in ('170.523 (m)(1): Adaptations and updates',
+   						'170.523 (m)(2): Adaptations and updates',
+   						'170.523 (m)(3): Adaptations and updates',
+   						'170.523 (m)(4): Adaptations and updates',
+   						'170.523 (m)(5): Adaptations and updates');
+   		get diagnostics affected_count = row_count;
+		raise notice 'Deleted % nonconformity type(s)', affected_count;
+  	end if;
+
+  	insert into openchpl.additional_nonconformity_type (name, removed, last_modified_user)
+  	select '170.523(m) Adaptations and updates', false, -1
+  	where not exists 
+  		(select *
+  		from openchpl.additional_nonconformity_type
+  		where name = '170.523(m) Adaptations and updates');
+
+  	get diagnostics affected_count = row_count;
+
+  	if affected_count = 0 then
+  		raise notice '170.523(m) nonconfomrity already exists';
+  	else
+  		raise notice '170.523(m) nonconformity added';
+  	end if;
+  	raise notice 'Completed';
 end $$;
