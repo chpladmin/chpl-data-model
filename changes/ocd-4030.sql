71077c4a58c74eed323d88ccda97093a98cd86ac
Katy Ekey <katy.ekey@gmail.com>
2022-12-22 14:05:43 -0500

    data: Do not set the permission id to a negative number.
    [#OCD-4030]

--- changes/ocd-4030.sql
+++ changes/ocd-4030.sql
@@ -1,5 +1,5 @@
-INSERT INTO openchpl.user_permission (user_permission_id, name, description, authority, last_modified_user)
-SELECT -4, 'STARTUP', 'This permission allows a user to use APIs needed for system startup', 'ROLE_STARTUP', -1
+INSERT INTO openchpl.user_permission (name, description, authority, last_modified_user)
+SELECT 'STARTUP', 'This permission allows a user to use APIs needed for system startup', 'ROLE_STARTUP', -1
 WHERE NOT EXISTS (
     SELECT *
     FROM openchpl.user_permission
