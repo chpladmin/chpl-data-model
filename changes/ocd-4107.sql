c89ae1fb1ef8f2f4f9d44af9b18e6251f2f2949a
Katy Ekey <katy.ekey@gmail.com>
2023-01-24 12:39:21 -0500

    feat: Drop many ICS error columns that can be derived from a listing
    Going forward, we plan to treat all "reporting" tables in this way. We
    will do a regular delete of the data and insert all of the new data at
    once. There is no need to keep derivable columns in the reporting tables
    if they can be gotten efficiently from listings via the shared store.
    
    [#OCD-4107]

--- changes/ocd-4107.sql
+++ changes/ocd-4107.sql
@@ -1,26 +1,43 @@
+-- Delete all data. Choosing DELETE over TRUNCATE so we don't start over with the ID sequence
+DELETE FROM openchpl.inheritance_errors_report;
+
+-- remove audit columns and trigger for last_modified_date
+DROP TRIGGER IF EXISTS inheritance_errors_report_timestamp ON openchpl.inheritance_errors_report;
+
+ALTER TABLE openchpl.inheritance_errors_report
+DROP COLUMN IF EXISTS last_modified_date;
+
+ALTER TABLE openchpl.inheritance_errors_report
+DROP COLUMN IF EXISTS last_modified_user;
+
+ALTER TABLE openchpl.inheritance_errors_report
+DROP COLUMN IF EXISTS deleted;
+
+-- dropping "derived" columns
+ALTER TABLE openchpl.inheritance_errors_report
+DROP COLUMN IF EXISTS url;
+
+ALTER TABLE openchpl.inheritance_errors_report
+DROP COLUMN IF EXISTS chpl_product_number;
+
+ALTER TABLE openchpl.inheritance_errors_report
+DROP COLUMN IF EXISTS developer;
+
+ALTER TABLE openchpl.inheritance_errors_report
+DROP COLUMN IF EXISTS product;
+
+ALTER TABLE openchpl.inheritance_errors_report
+DROP COLUMN IF EXISTS version;
+
+-- add new certified_product_id column
 ALTER TABLE openchpl.inheritance_errors_report
 DROP COLUMN IF EXISTS certified_product_id;
 
 ALTER TABLE openchpl.inheritance_errors_report
-ADD COLUMN certified_product_id bigint;
-
--- fill in the data based on the existing 'url' field
-UPDATE openchpl.inheritance_errors_report
-SET certified_product_id = split_part(url, '/', 6)::bigint;
-
-ALTER TABLE openchpl.inheritance_errors_report
-ALTER COLUMN certified_product_id SET NOT NULL;
+ADD COLUMN certified_product_id bigint NOT NULL; 
 
 ALTER TABLE openchpl.inheritance_errors_report 
 ADD CONSTRAINT certified_product_fk 
 	FOREIGN KEY (certified_product_id) 
 	REFERENCES openchpl.certified_product (certified_product_id) 
 	MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
-
--- Allow URL field to be null and we won't fill it in any longer
-ALTER TABLE openchpl.inheritance_errors_report
-ALTER COLUMN url DROP NOT NULL;
-
--- TODO in a future PR
---ALTER TABLE openchpl.inheritance_errors_report
---DROP COLUMN IF EXISTS url;
