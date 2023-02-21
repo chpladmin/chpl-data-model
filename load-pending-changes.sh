faf1afb933538e2a5417a2847aab28be894e3a9b
Todd Young <youngto@pointclickcare.com>
2023-02-21 16:13:12 -0500

    feat: add database param to scripts

--- load-pending-changes.sh
+++ load-pending-changes.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+openchpl_aqa#!/bin/bash
 #(set -o igncr) 2>/dev/null
 set -o igncr; # this comment is required to trick cygwin into dealing with windows vs. linux EOL characters
 
@@ -8,6 +8,7 @@ set -e
 HOST=localhost
 USER=openchpl_dev
 DUMMY=true
+DATABASE=openchpl
 
 function helpFunction {
     echo ""
@@ -15,12 +16,14 @@ function helpFunction {
     echo -e "\t-d do not load dummy data.  Default: [load dummy data]"
     echo -e "\t-h database server host or socket directory.  Default: [localhost]"
     echo -e "\t-u database user name.  Default: [openchpl_dev]"
+    echo -e "\t-b database name.  Default: [openchpl]"
     exit 1 # Exit script after printing help
 }
 
 while getopts 'dh:u:?' flag; do
     case "${flag}" in
         d) DUMMY=false ;;
+        d) DATABASE="${OPTARG}" ;;
         h) HOST="${OPTARG}" ;;
         u) USER="${OPTARG}" ;;
         *) helpFunction ;;
@@ -42,7 +45,7 @@ then
     echo "Executing updates"
     for FILE in ./changes/ocd-????.sql; do
         echo $FILE;
-        psql -U $USER -h $HOST -f $FILE openchpl;
+        psql -U $USER -h $HOST -f $FILE $DATABASE;
     done
 fi
 COUNT_CHANGES_FILES=` find ./changes/ -maxdepth 1 -name 'ocd-????-dummy-data.sql' | wc -l`
@@ -51,13 +54,13 @@ then
     echo "Loading dummy data"
     for FILE in ./changes/ocd-????-dummy-data.sql; do
         echo $FILE;
-        psql -U $USER -h $HOST -f $FILE openchpl;
+        psql -U $USER -h $HOST -f $FILE $DATABASE;
     done
 fi
 
 echo "Updating soft-delete"
-psql -U $USER -h $HOST -f dev/openchpl_soft-delete.sql openchpl
+psql -U $USER -h $HOST -f dev/openchpl_soft-delete.sql $DATABASE
 echo "Updating views"
-psql -U $USER -h $HOST -f dev/openchpl_views.sql openchpl
+psql -U $USER -h $HOST -f dev/openchpl_views.sql $DATABASE
 echo "Updating grant-all"
-psql -U $USER -h $HOST -f dev/openchpl_grant-all.sql openchpl
+psql -U $USER -h $HOST -f dev/openchpl_grant-all.sql $DATABASE
