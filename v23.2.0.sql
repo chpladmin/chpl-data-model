7e158d8d1abddec5caae48bb5804f4ffea7c3241
Andrew Larned <andrew.larned@gmail.com>
2023-02-21 13:44:25 -0500

    release: deploy version 23.2.0 on 21 February 2023

--- v23.2.0.sql
+++ v23.2.0.sql
@@ -1,3 +1,6 @@
+-- Deployment file for version 23.2.0
+--     as of 2023-02-21
+-- ./changes/ocd-4107.sql
 -- Delete all data. Choosing DELETE over TRUNCATE so we don't start over with the ID sequence
 DELETE FROM openchpl.inheritance_errors_report;
 
@@ -41,3 +44,14 @@ ADD CONSTRAINT certified_product_fk
 	FOREIGN KEY (certified_product_id) 
 	REFERENCES openchpl.certified_product (certified_product_id) 
 	MATCH FULL ON UPDATE CASCADE ON DELETE RESTRICT;
+;
+-- ./changes/ocd-4118.sql
+CREATE INDEX IF NOT EXISTS test_task_id_idx ON openchpl.test_task_participant_map (test_task_id);
+CREATE INDEX IF NOT EXISTS test_task_participant_id_idx ON openchpl.test_task_participant_map (test_participant_id);
+CREATE INDEX IF NOT EXISTS test_task_participant_id_and_deleted_idx ON openchpl.test_task_participant_map (test_participant_id, deleted);
+
+DROP TRIGGER IF EXISTS test_task_participant_map_soft_delete on openchpl.test_task_participant_map;;
+insert into openchpl.data_model_version (version, deploy_date, last_modified_user) values ('23.2.0', '2023-02-21', -1);
+\i dev/openchpl_soft-delete.sql
+\i dev/openchpl_views.sql
+\i dev/openchpl_grant-all.sql
