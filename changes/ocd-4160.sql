be4680adfe68c09b2b71daffc060f3d792275c95
Katy Ekey <katy.ekey@gmail.com>
2023-03-19 19:20:44 -0400

    data!: Rename Current Certification Date Changed QA trigger
    [#OCD-4160]

--- changes/ocd-4160.sql
+++ changes/ocd-4160.sql
@@ -50,6 +50,14 @@ ALTER TABLE openchpl.questionable_activity_version
 ADD CONSTRAINT activity_to_questionable_activity_version_fk FOREIGN KEY (activity_id) REFERENCES openchpl.activity (activity_id)
 ON DELETE RESTRICT;
 
+-- Rename trigger to be more specific and correct.
+-- It is detecting when the date associated with the current certification status changes.
+-- This may not be the Certification Date (if the current certification status it not 'Active')
+-- but the current "name" reads as though it is the Certification Date that is being changed.
+UPDATE openchpl.questionable_activity_trigger
+SET name = 'Current Certification Status Date Edited'
+WHERE name = 'Current Certification Date Edited'; 
+
 -- Add reason column to activity table 
 ALTER TABLE openchpl.activity
 DROP COLUMN IF EXISTS reason;
