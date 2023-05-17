83c8f8f7b9d6565b46f84a3f752f44fe9011d527
Katy Ekey <katy.ekey@gmail.com>
2023-05-17 14:07:18 -0400

    feat!: Create tables to support public user subscriptions
    [#OCD-4213]

---
+++ changes/ocd-4213.sql
@@ -0,0 +1,172 @@
+DROP TABLE IF EXISTS openchpl.subscriber;
+DROP TABLE IF EXISTS openchpl.subscriber_status;
+DROP TABLE IF EXISTS openchpl.subscription_type;
+DROP TABLE IF EXISTS openchpl.subscription_subject;
+DROP TABLE IF EXISTS openchpl.subcription_consolidation_method;
+DROP TABLE IF EXISTS openchpl.subscription_reason;
+DROP TABLE IF EXISTS openchpl.subscription;
+DROP TABLE IF EXISTS openchpl.subscription_observation;
+
+
+-- Subscriber. An email address and token that can be used to get to subscriptions related to that email address for management purposes.
+-- A subscriber must be confirmed by clicking a link so we know their email is valid.
+CREATE TABLE openchpl.subscriber (
+	id bigserial NOT NULL,
+	subscriber_status_id bigint NOT NULL,
+	email text NOT NULL,
+	token varchar(500), -- GUID-style thing. Used to confirm a subscriber's email is valid and used in emails about management/unsubscribe
+	creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NOT NULL,
+    deleted boolean NOT NULL DEFAULT false					
+);
+
+-- Subscriber status. Indicates if the subscriber has confirmed their email.
+CREATE TABLE openchpl.subscriber_status (
+	id bigserial NOT NULL,
+	name varchar(100) NOT NULL,
+	creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NOT NULL,
+    deleted boolean NOT NULL DEFAULT false
+);
+
+INSERT INTO openchpl.subscriber_status (name, last_modified_user)
+VALUES ('Pending', -1),
+('Confirmed', -1);
+  
+
+-- Subscription type. Tells whether this subscription on a Listing, Developer, or Product
+CREATE TABLE openchpl.subscription_type (
+	id bigserial NOT NULL,
+	name varchar(200) NOT NULL,
+	creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NOT NULL,
+    deleted boolean NOT NULL DEFAULT false
+);
+
+INSERT INTO openchpl.subscription_type (name, last_modified_user)
+VALUES ('Listing', -1),
+('Developer', -1), 
+('Product', -1);
+
+
+-- Subscription subject. Tells what specific changes we are looking for
+CREATE TABLE openchpl.subscription_subject (
+	id bigserial NOT NULL,
+	subscription_type_id bigint NOT NULL,
+	subject varchar(200) NOT NULL,
+	creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NOT NULL,
+    deleted boolean NOT NULL DEFAULT false
+);
+
+INSERT INTO openchpl.subscription_subject (subscription_type_id, subject, last_modified_user)
+VALUES ((SELECT id FROM openchpl.subscription_type WHERE name = 'Listing'), 'Certification Status Changed', -1),
+((SELECT id FROM openchpl.subscription_type WHERE name = 'Listing'), 'Certification Criterion Added', -1),
+((SELECT id FROM openchpl.subscription_type WHERE name = 'Listing'), 'Certification Criterion Removed', -1),
+((SELECT id FROM openchpl.subscription_type WHERE name = 'Listing'), 'Real World Testing Updated', -1),
+((SELECT id FROM openchpl.subscription_type WHERE name = 'Developer'), 'New Listing Confirmed', -1), 
+((SELECT id FROM openchpl.subscription_type WHERE name = 'Product'), 'New Listing Confirmed', -1);
+-- TODO: Decide to add all these subjects now or later?
+
+
+-- Consolidation Method. Tells the sort of batching that observations will be sent to the subscriber
+CREATE TABLE openchpl.subcription_consolidation_method (
+	id bigserial NOT NULL,
+	method_name varchar(200) NOT NULL,
+	creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NOT NULL,
+    deleted boolean NOT NULL DEFAULT false
+);
+
+INSERT INTO openchpl.subcription_consolidation_method (method_name, last_modified_user)
+VALUES ('Daily', -1),
+('Weekly', -1);
+-- I feel like we could implement 'Push' at some point but maybe it's not useful to put it in here at this time.
+
+
+-- Ask if we want to keep the "Persona" language in the UI. I find the term to be a big turn-off to filling it out.
+-- Ask if we want the persona attached to the subscriber or to the subscription
+CREATE TABLE openchpl.subscription_reason (
+	id bigserial NOT NULL,
+	name varchar(300) NOT NULL,
+	sort_order int NOT NULL,
+	creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NOT NULL,
+    deleted boolean NOT NULL DEFAULT false
+);
+
+INSERT INTO openchpl.subscription_reason (name, sort_order, last_modified_user)
+VALUES ('Researcher', 1, -1),
+('Developer', 2, -1),
+('Other', 3, -1),
+('Prefer Not to Answer', 4, -1);
+
+	
+-- Subscription.
+-- Once the status of the subscriber_id is Confirmed we will log observations for all subscriptions for that subscriber
+CREATE TABLE openchpl.subscription (
+    id bigserial NOT NULL,	
+	subscriber_id bigint NOT NULL, 
+    subscription_reason_id bigint NOT NULL,  
+	subscription_subject_id bigint NOT NULL,
+	subscribed_object_id bigint NOT NULL, -- the ID of the developer, product, listing, etc. 
+	subscription_consolidation_method_id bigint NOT NULL, -- We would always default to Daily for now I think...
+    creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NOT NULL,
+    deleted boolean NOT NULL DEFAULT false
+);
+
+
+-- Subscription Observations. Whenever a relevant change is observed we will save that fact here.
+CREATE TABLE openchpl.subscription_observation (
+    id bigserial NOT NULL,
+	subscription_id bigint NOT NULL,
+	activity_id bigint NOT NULL,  -- May need to be null at some point in the future for negative actions
+	creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NOT NULL,
+    deleted boolean NOT NULL DEFAULT false
+);
+
+-- Workflow
+	-- User clicks button to subscribe to something
+		-- Does a Confirmed subscriber with the email address already exist?
+			-- Create a new subscription for this subscriber
+		-- Does a Pending subscriber with the email address already exist?
+			-- Re-send the email with a link using their same subscriber token
+		-- Does no subscriber with the email address exist?
+			-- Create a Pending subscriber and the subscription for them
+			-- Send them an email with a link using their subscriber token
+		-- If subscription is a duplicate (same subscriber id, subject, object) then do nothing
+	-- User clicks link in email to confirm their email is valid 
+		-- Update their subscriber status to Confirmed
+	-- All subscriptions for Confirmed Subscribers will have Observations saved for future processing
+		
+	
+		
+-- How are we going to know whether an observation is eligible for processing each time the job runs?
+	-- The job will have a parameter that has the consolidation method name
+  	-- We can schedule the system job to run daily for 'Daily' subscriptions with that parameter value,
+    -- weekly for 'Weekly' subscriptions with that paremeter value,
+    -- and a 'Push' subscription would probably just schedule the job to run immediately with 'Push'
+    -- as the parameter value.
+    
+	-- When the job runs it looks at this parameter and gathers non-deleted observations for subscriptions 
+	-- that match it's consolidationMethod parameter and processes them.
+  	-- Processing means sends an email to all the observations for the same email address that apply to the consolidationMethod
+  	-- We probably need to throttle the emails somehow...
+	-- Soft delete observations
+	
+	-- When creating the email
+		-- Each subscription subject has a concrete class that knows how to deal with a certain type of Activity and turn it into what we want to display in the email
+		-- There is an abstract class ObservationFormatter each of these concrete classes extend from
+
+
+
