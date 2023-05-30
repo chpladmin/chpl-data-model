41bc43cb570c9baf8a70ed84093095dce710fe9f
Katy Ekey <katy.ekey@gmail.com>
2023-05-30 13:17:49 -0400

    data!: Remove ROLE_ONC_STAFF and related users
    [#OCD-4225]

--- changes/ocd-4212.sql
+++ changes/ocd-4212.sql
@@ -1,3 +1,5 @@
+-- OCD-4212 Remove ATL Users and Permission
+
 UPDATE openchpl.user_permission
 SET deleted = true
 WHERE name = 'ATL';
@@ -8,3 +10,12 @@ SET deleted = true;
 UPDATE openchpl.user 
 SET deleted = TRUE
 WHERE user_permission_id = (SELECT user_permission_id FROM openchpl.user_permission WHERE name = 'ATL');
+
+-- OCD-4225 Remove ONC_STAFF Users and permission
+UPDATE openchpl.user_permission
+SET deleted = true
+WHERE name = 'ONC_STAFF';
+
+UPDATE openchpl.user 
+SET deleted = TRUE
+WHERE user_permission_id = (SELECT user_permission_id FROM openchpl.user_permission WHERE name = 'ONC_STAFF');
