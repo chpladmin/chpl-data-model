cdabbc9c659d7d1cb0bc829793e52787f93004d2
Katy Ekey <katy.ekey@gmail.com>
2023-06-01 13:14:10 -0400

    feat!: Drop accredidation_number column for ATLs
    [#OCD-4055]

--- changes/ocd-4055.sql
+++ changes/ocd-4055.sql
@@ -3,3 +3,6 @@ ALTER COLUMN retirement_date TYPE date;
 
 ALTER TABLE openchpl.testing_lab
 ALTER COLUMN retirement_date TYPE date;
+
+ALTER TABLE openchpl.testing_lab
+DROP COLUMN IF EXISTS accredidation_number;
