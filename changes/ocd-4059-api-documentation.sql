8e2c93f8ac2a89662b5935847ae3802c3d14cb9f
Katy Ekey <katy.ekey@gmail.com>
2023-06-16 13:27:02 -0400

    data!: Add ApiDoc to certification_criterion_attribute table
    [#OCD-4059]

---
+++ changes/ocd-4059-api-documentation.sql
@@ -0,0 +1,42 @@
+--
+-- Add Api Documentation to criterion attribute table
+--
+
+ALTER TABLE openchpl.certification_criterion_attribute
+ADD COLUMN IF NOT EXISTS api_documentation bool NOT NULL default false;
+
+--
+-- 2015 Criteria
+--
+
+-- g7
+UPDATE openchpl.certification_criterion_attribute
+SET api_documentation = true
+WHERE criterion_id = 56;
+
+-- g8
+UPDATE openchpl.certification_criterion_attribute
+SET api_documentation = true
+WHERE criterion_id = 57;
+
+-- g9
+UPDATE openchpl.certification_criterion_attribute
+SET api_documentation = true
+WHERE criterion_id = 58;
+
+-- g9Cures
+UPDATE openchpl.certification_criterion_attribute
+SET api_documentation = true
+WHERE criterion_id = 181;
+
+-- g10
+UPDATE openchpl.certification_criterion_attribute
+SET api_documentation = true
+WHERE criterion_id = 182;
+
+
+
+
+
+
+
