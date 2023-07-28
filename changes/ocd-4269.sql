eea609e8bb61d38dde1d21540508b5058847293f
Katy Ekey <katy.ekey@gmail.com>
2023-07-28 09:40:59 -0400

    feat: Remove unused columns from certification_criterion table
    Breaking Change
    
    [#OCD-4269]

---
+++ changes/ocd-4269.sql
@@ -0,0 +1,4 @@
+ALTER TABLE openchpl.certification_criterion DROP COLUMN IF EXISTS automated_numerator_capable;
+ALTER TABLE openchpl.certification_criterion DROP COLUMN IF EXISTS automated_measure_capable;
+ALTER TABLE openchpl.certification_criterion DROP COLUMN IF EXISTS requires_sed;
+ALTER TABLE openchpl.certification_criterion DROP COLUMN IF EXISTS parent_criterion_id;
