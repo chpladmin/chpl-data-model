7fd232ee5a6b98105bcb46da9d561382338b9791
Todd Young <todd.young@pointclickcare.com>
2023-08-16 15:27:48 -0400

    feat: add new columns for managing functionality tested
    OCD-4288

---
+++ changes/ocd-4288.sql
@@ -0,0 +1,28 @@
+alter table openchpl.test_tool drop column if exists name;
+
+alter table openchpl.test_tool drop column if exists description;
+
+alter table openchpl.test_tool drop column if exists retired;
+
+--------------------------------------------------------------------------
+
+alter table openchpl.functionality_tested add column if not exists value text;
+
+alter table openchpl.functionality_tested add column if not exists regulatory_text_citation text;
+
+alter table openchpl.functionality_tested add column if not exists start_day date;
+
+alter table openchpl.functionality_tested add column if not exists end_day date;
+
+alter table openchpl.functionality_tested add column if not exists required_day date;
+
+alter table openchpl.functionality_tested add column if not exists rule_id bigint;
+
+alter table openchpl.functionality_tested add constraint rule_fk foreign key (rule_id)
+    references openchpl.rule (id)
+    match simple on update no action on delete restrict;
+
+update openchpl.functionality_tested
+set value = name,
+regulatory_text_citation = number;
+
