74cfd9f59928845ddebf5708cc90eeb3454b2fc3
Katy Ekey <katy.ekey@gmail.com>
2023-08-30 12:19:20 -0400

    data: Make dummy data closer to real data
    [#OCD-4289]

--- changes/ocd-4289-dummy-data.sql
+++ changes/ocd-4289-dummy-data.sql
@@ -1,39 +1,33 @@
 -- set criteria start and end dates
 UPDATE openchpl.certification_criterion
-SET start_day = '2011-01-01'
+SET start_day = '2010-07-28'
 WHERE number LIKE '170.30%';
 
 UPDATE openchpl.certification_criterion
-SET end_day = '2013-12-31'
+SET end_day = '2016-04-13'
 WHERE number LIKE '170.30%';
 
 UPDATE openchpl.certification_criterion
-SET start_day = '2014-01-01'
+SET start_day = '2012-09-04'
 WHERE number LIKE '170.314%';
 
 UPDATE openchpl.certification_criterion
-SET end_day = '2014-12-31'
+SET end_day = '2020-06-29'
 WHERE number LIKE '170.314%';
 
 UPDATE openchpl.certification_criterion
-SET start_day = '2015-01-01'
+SET start_day = '2015-10-16'
 WHERE number LIKE '170.315%' and title NOT LIKE '%Cures Update%';
 
 UPDATE openchpl.certification_criterion
-SET end_day = '2020-06-30'
-WHERE certification_criterion_id IN (8, 19, 20, 6, 7, 11);
+SET end_day = '2022-12-31'
+WHERE certification_criterion_id != 21 
+AND number LIKE '170.315%' 
+AND title NOT LIKE '%Cures Update%';
 
 UPDATE openchpl.certification_criterion
-SET end_day = '2022-01-01'
-WHERE certification_criterion_id IN (10, 13, 41);
-
-UPDATE openchpl.certification_criterion
-SET end_day = '2023-01-01'
-WHERE certification_criterion_id IN (16, 17, 18, 22, 23, 24, 27, 30, 31, 38, 40, 47, 55, 57, 58);
-
-UPDATE openchpl.certification_criterion
-SET end_day = '2024-01-01'
-WHERE certification_criterion_id IN (21);
+SET end_day = '2023-12-31'
+WHERE certification_criterion_id = 21;
 
 UPDATE openchpl.certification_criterion
 SET start_day = '2020-06-30'
