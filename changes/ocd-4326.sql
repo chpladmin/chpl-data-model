c7c9b00b48e5df7ecc419e5824c2ce2b1249a6d8
Katy Ekey <katy.ekey@gmail.com>
2023-11-08 13:51:34 -0500

    data: Add trigger for uploading after cert date
    [#OCD-4326]

--- changes/ocd-4326.sql
+++ changes/ocd-4326.sql
@@ -1,6 +1,13 @@
 -- add to questionable activity trigger
 
 INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_user)
+SELECT 'Uploaded After Certification Date', 'Listing', -1
+WHERE
+NOT EXISTS (
+	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Uploaded After Certification Date'
+);
+
+INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_user)
 SELECT 'Attested to Removed Certification Criteria', 'Listing', -1
 WHERE
 NOT EXISTS (
