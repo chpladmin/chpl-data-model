8aea2f0499069c49f79314315405326376a3106c
Katy Ekey <katy.ekey@gmail.com>
2023-11-08 13:16:47 -0500

    data: Add all new questionable activity types
    [#OCD-4326]

--- changes/ocd-4326.sql
+++ changes/ocd-4326.sql
@@ -7,3 +7,23 @@ NOT EXISTS (
 	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Attested to Removed Certification Criteria'
 );
 
+INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_user)
+SELECT 'Removed Test Tool Added', 'Certification Criteria', -1
+WHERE
+NOT EXISTS (
+	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Removed Test Tool Added'
+);
+
+INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_user)
+SELECT 'Removed Functionality Tested Added', 'Certification Criteria', -1
+WHERE
+NOT EXISTS (
+	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Removed Functionality Tested Added'
+);
+
+INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_user)
+SELECT 'Removed Standard Added', 'Certification Criteria', -1
+WHERE
+NOT EXISTS (
+	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Removed Standard Added'
+);
