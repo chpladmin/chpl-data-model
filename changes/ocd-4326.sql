b510101f7926025be956f5f0f0e0e38f85907ea6
Katy Ekey <katy.ekey@gmail.com>
2023-11-09 15:27:32 -0500

    data: Change 'removed' text to 'expired'
    [#OCD-4326]

--- changes/ocd-4326.sql
+++ changes/ocd-4326.sql
@@ -15,22 +15,22 @@ NOT EXISTS (
 );
 
 INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_user)
-SELECT 'Removed Test Tool Added', 'Certification Criteria', -1
+SELECT 'Expired Test Tool Added', 'Certification Criteria', -1
 WHERE
 NOT EXISTS (
-	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Removed Test Tool Added'
+	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Expired Test Tool Added'
 );
 
 INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_user)
-SELECT 'Removed Functionality Tested Added', 'Certification Criteria', -1
+SELECT 'Expired Functionality Tested Added', 'Certification Criteria', -1
 WHERE
 NOT EXISTS (
-	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Removed Functionality Tested Added'
+	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Expired Functionality Tested Added'
 );
 
 INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_user)
-SELECT 'Removed Standard Added', 'Certification Criteria', -1
+SELECT 'Expired Standard Added', 'Certification Criteria', -1
 WHERE
 NOT EXISTS (
-	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Removed Standard Added'
+	SELECT name FROM openchpl.questionable_activity_trigger WHERE name = 'Expired Standard Added'
 );
