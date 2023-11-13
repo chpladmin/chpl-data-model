b42d0b7d9ea4f9e0d4a9dddf12bdf66c180713ac
Todd Young <todd.young@ainq.com>
2023-11-13 13:24:34 -0500

    feat: add fetaures to allow last_modified_sso_user to work with last_modified_user
    OCD-4377

---
+++ changes/ocd-4377.sql
@@ -0,0 +1,56 @@
+CREATE OR REPLACE FUNCTION "openchpl".column_exists (ptable text, pcolumn text, pschema text DEFAULT 'public')
+    RETURNS boolean
+    LANGUAGE sql
+    STABLE STRICT
+    AS $BODY$
+    -- does the requested table.column exist in schema?
+    SELECT
+        EXISTS (
+            SELECT
+                NULL
+            FROM
+                information_schema.columns
+            WHERE
+                table_name = ptable
+                AND column_name = pcolumn
+                AND table_schema = pschema);
+$BODY$;
+
+
+CREATE OR REPLACE FUNCTION openchpl.last_modified_user_constraint() RETURNS trigger LANGUAGE plpgsql
+AS $function$
+BEGIN
+	IF NEW.last_modified_user IS NULL && NEW.last_modified_sso_user IS NULL THEN
+		RAISE EXCEPTION 'Column last_modified_user or last_modified_sso_user requires a value.';
+	ELSIF  NEW.last_modified_user IS NOT NULL && NEW lsat_modified_sso_user IS NOT NULL THEN
+		RAISE EXCEPTION 'Only one of the columns [last_modified_user , last_modified_sso_user] can have a value.';
+	END IF;
+	RETURN NEW;
+END;
+$function$;
+
+
+DO $$
+DECLARE 
+    row record; 
+    cmd text;
+BEGIN
+    FOR row IN SELECT schemaname, tablename FROM pg_tables WHERE schemaname = 'openchpl' ORDER BY tablename LOOP
+        cmd := format('ALTER TABLE %I.%I ADD COLUMN IF NOT EXISTS last_modified_sso_user uuid;', row.schemaname, row.tablename);
+        RAISE NOTICE '%', cmd;
+        EXECUTE cmd;
+
+	IF openchpl.column_exists (row.tablename, 'last_modified_user', row.schemaname) THEN
+		cmd :=  format('ALTER TABLE %I.%I ALTER COLUMN last_modified_user DROP NOT NULL;', row.schemaname, row.tablename);
+		RAISE NOTICE '%', cmd;
+		EXECUTE cmd;
+	END IF;
+
+	cmd := format('CREATE CONSTRAINT TRIGGER %I_last_modified_user_constraint AFTER INSERT OR UPDATE ON %I.%I DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE openchpl.last_modified_user_constraint();', row.tablename, row.schemaname, row.tablename);
+	RAISE NOTICE '%', cmd;
+    END LOOP;
+END
+$$ LANGUAGE plpgsql;
+
+
+DROP FUNCTION openchpl.column_exists;
