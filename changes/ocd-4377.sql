7e4bf08a88b1771639bbdd2a9dd9e3755f41e960
Todd Young <todd.young@ainq.com>
2023-11-13 15:36:05 -0500

    feat: add constraint trigger
    OCD-4377

--- changes/ocd-4377.sql
+++ changes/ocd-4377.sql
@@ -20,9 +20,9 @@ $BODY$;
 CREATE OR REPLACE FUNCTION openchpl.last_modified_user_constraint() RETURNS trigger LANGUAGE plpgsql
 AS $function$
 BEGIN
-	IF NEW.last_modified_user IS NULL && NEW.last_modified_sso_user IS NULL THEN
+	IF NEW.last_modified_user IS NULL AND NEW.last_modified_sso_user IS NULL THEN
 		RAISE EXCEPTION 'Column last_modified_user or last_modified_sso_user requires a value.';
-	ELSIF  NEW.last_modified_user IS NOT NULL && NEW lsat_modified_sso_user IS NOT NULL THEN
+	ELSIF  NEW.last_modified_user IS NOT NULL AND NEW.last_modified_sso_user IS NOT NULL THEN
 		RAISE EXCEPTION 'Only one of the columns [last_modified_user , last_modified_sso_user] can have a value.';
 	END IF;
 	RETURN NEW;
@@ -31,24 +31,31 @@ $function$;
 
 
 DO $$
-DECLARE 
-    row record; 
+DECLARE
+    row record;
     cmd text;
+    table_name text;
 BEGIN
-    FOR row IN SELECT schemaname, tablename FROM pg_tables WHERE schemaname = 'openchpl' ORDER BY tablename LOOP
-        cmd := format('ALTER TABLE %I.%I ADD COLUMN IF NOT EXISTS last_modified_sso_user uuid;', row.schemaname, row.tablename);
-        RAISE NOTICE '%', cmd;
-        EXECUTE cmd;
+	FOR row IN SELECT schemaname, tablename FROM pg_tables WHERE schemaname = 'openchpl' ORDER BY tablename LOOP
 
-	IF openchpl.column_exists (row.tablename, 'last_modified_user', row.schemaname) THEN
-		cmd :=  format('ALTER TABLE %I.%I ALTER COLUMN last_modified_user DROP NOT NULL;', row.schemaname, row.tablename);
+        	cmd := format('ALTER TABLE %I.%I ADD COLUMN IF NOT EXISTS last_modified_sso_user uuid;', row.schemaname, row.tablename);
+	        RAISE NOTICE '%', cmd;
+	        EXECUTE cmd;
+
+		IF openchpl.column_exists (row.tablename, 'last_modified_user', row.schemaname) THEN
+			cmd :=  format('ALTER TABLE %I.%I ALTER COLUMN last_modified_user DROP NOT NULL;', row.schemaname, row.tablename);
+			RAISE NOTICE '%', cmd;
+			EXECUTE cmd;
+		END IF;
+
+		cmd := format('DROP TRIGGER IF EXISTS %s_last_modified_user_constraint on %I.%I;', row.tablename, row.schemaname, row.tablename);
 		RAISE NOTICE '%', cmd;
 		EXECUTE cmd;
-	END IF;
 
-	cmd := format('CREATE CONSTRAINT TRIGGER %I_last_modified_user_constraint AFTER INSERT OR UPDATE ON %I.%I DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE openchpl.last_modified_user_constraint();', row.tablename, row.schemaname, row.tablename);
-	RAISE NOTICE '%', cmd;
-    END LOOP;
+		cmd := format('CREATE CONSTRAINT TRIGGER %s_last_modified_user_constraint AFTER INSERT OR UPDATE ON %I.%I DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE openchpl.last_modified_user_constraint();', row.tablename, row.schemaname, row.tablename);
+		RAISE NOTICE '%', cmd;
+        	EXECUTE cmd;
+    	END LOOP;
 END
 $$ LANGUAGE plpgsql;
 
