fd0eed8f26c50e6bf692286b06318edb78875770
Todd Young <tyoung@ainq.com>
2024-01-08 15:22:22 -0500

    feat: initial creation of report table
    OCD-4437

---
+++ changes/ocd-4437.sql
@@ -0,0 +1,20 @@
+create table if not exists openchpl.updated_listing_status_report (
+    id bigserial not null,
+    certified_product_id bigint not null,
+    criteria_require_update_count int,
+    days_updated_early int,
+    creation_date timestamp not null default now(),
+    last_modified_date timestamp not null default now(),
+    last_modified_user bigint,
+    last_modified_sso_user bigint,
+    deleted bool not null default false,
+    constraint updated_listing_status_report_pk primary key (id),
+    constraint certified_product_fk foreign key (certified_product_id)
+	    references openchpl.certified_product (certified_product_id)
+	    match simple on update no action on delete restrict
+);
+CREATE or replace TRIGGER updated_listing_status_report_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.updated_listing_status_report FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE or replace TRIGGER updated_listing_status_report_timestamp BEFORE UPDATE on openchpl.updated_listing_status_report FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
+
+DROP TRIGGER IF EXISTS updated_listing_status_report_last_modified_user_constraint ON openchpl.updated_listing_status_report;
+CREATE CONSTRAINT TRIGGER updated_listing_status_report_last_modified_user_constraint AFTER INSERT OR UPDATE ON openchpl.updated_listing_status_report DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE openchpl.last_modified_user_constraint();
