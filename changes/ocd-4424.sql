d8e25b0fd9947e52e7d5fb112565288c49c4d56d
Katy Ekey <katy.ekey@gmail.com>
2024-01-12 11:04:26 -0500

    data!: Set start day of b11 to Feb 8, 2024
    [#OCD-4424]

--- changes/ocd-4424.sql
+++ changes/ocd-4424.sql
@@ -15,3 +15,6 @@ DELETE FROM openchpl.certification_criterion WHERE number = '170.315 (d)(14)';
 
 -- set the sequence back so the next criterion will have the expected ID
 ALTER SEQUENCE openchpl.certification_criterion_certification_criterion_id_seq RESTART WITH 211;
+
+-- set the correct start date for b11
+UPDATE openchpl.certification_criterion SET start_day = '2024-02-08' WHERE certification_criterion_id = 210;
