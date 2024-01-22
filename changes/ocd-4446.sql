f04e351ff894b630a6e53b63c5012384d835456e
Todd Young <tyoung@ainq.com>
2024-01-22 14:17:09 -0500

    data: default code_set to false for active certificates
    OCD-4446

--- changes/ocd-4446.sql
+++ changes/ocd-4446.sql
@@ -7,3 +7,8 @@ where criterion_id in (5, 6, 12, 15, 16, 18, 19, 20, 21, 28, 43, 45, 46, 165, 16
 
 alter table openchpl.certification_result
 add column if not exists code_set bool;
+
+update openchpl.certification_result
+set code_set = false
+where certification_criterion_id in (select criterion_id from openchpl.certification_criterion_attribute where code_set = true)
+and certified_product_id in (select cpd.certified_product_id from openchpl.certified_product_details cpd where cpd.certification_status_name in ('Active', 'Withdrawn by ONC-ACB', 'Withdrawn by Developer'));
