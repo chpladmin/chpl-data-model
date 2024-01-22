079886a784bf3b276b0538c1d74932397ba08e13
Todd Young <tyoung@ainq.com>
2024-01-22 14:20:31 -0500

    data: make RED 2024-01-21
    OCD-4373

--- changes/ocd-4373-dummy-data.sql
+++ changes/ocd-4373-dummy-data.sql
@@ -1,35 +1,35 @@
 update openchpl.certification_criterion
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where number = '170.315 (b)(11)';
 
 update openchpl.standard
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where regulatory_text_citation = '170.205(a)(6)';
 
 update openchpl.standard
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where regulatory_text_citation = '170.205(t)(1)';
 
 update openchpl.standard
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where regulatory_text_citation = '170.205(t)(2)';
 
 update openchpl.standard
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where regulatory_text_citation = '170.205(t)(3)';
 
 update openchpl.standard
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where regulatory_text_citation = '170.205(t)(4)';
 
 update openchpl.standard
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where regulatory_text_citation = '170.213(b)';
 
 update openchpl.standard
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where regulatory_text_citation = '170.215(b)(1)(ii)';
 
 update openchpl.standard
-set start_day = '2024-01-29'
+set start_day = '2024-01-21'
 where regulatory_text_citation = '170.215(c)(2)';
