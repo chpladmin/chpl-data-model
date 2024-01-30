7a0f0cc7a5e08dccca75e39346f0ee2e00e58a17
Katy Ekey <katy.ekey@gmail.com>
2024-01-30 10:46:47 -0500

    refactor: Move drop view commands to ocd file
    This should drop the views for this ticket and get them out of the views
    file.
    
    [#OCD-4452]

---
+++ changes/ocd-4452.sql
@@ -0,0 +1,3 @@
+DROP VIEW IF EXISTS openchpl.product_certification_statuses;
+DROP VIEW IF EXISTS openchpl.developer_certification_statuses;
+DROP VIEW IF EXISTS openchpl.certified_product_search_result;
