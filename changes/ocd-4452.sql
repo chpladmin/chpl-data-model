0adc77aa20e57d9ee4306e7f81ecf130e454eef0
Katy Ekey <katy.ekey@gmail.com>
2024-01-31 11:46:13 -0500

    feat: Add index that is required for the current status calculation
    [#OCD-4452]

--- changes/ocd-4452.sql
+++ changes/ocd-4452.sql
@@ -1,3 +1,5 @@
 DROP VIEW IF EXISTS openchpl.product_certification_statuses;
 DROP VIEW IF EXISTS openchpl.developer_certification_statuses;
 DROP VIEW IF EXISTS openchpl.certified_product_search_result;
+
+CREATE INDEX IF NOT EXISTS cse_certified_product_id_idx ON openchpl.certification_status_event (certified_product_id);
