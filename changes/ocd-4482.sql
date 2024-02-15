4e97b1c593bdfff9f0467d93ddeba27ecd629156
Todd Young <tyoung@ainq.com>
2024-02-14 21:35:15 -0500

    feat: add initial data
    OCD-4482

--- changes/ocd-4482.sql
+++ changes/ocd-4482.sql
@@ -63,3 +63,19 @@ alter table openchpl.certification_criterion_attribute add column if not exists 
 update openchpl.certification_criterion_attribute cca
 set code_set_date = true
 where criterion_id in (5, 6, 12, 15, 16, 18, 19, 20, 21, 28, 43, 45, 46, 165, 167);
+
+insert into openchpl.code_set_date (required_day, last_modified_user)
+select '2025-12-31', -1
+where not exists (select * from openchpl.code_set_date where required_day = '2025-12-31');
+
+insert into openchpl.code_set_date_criteria_map (certification_criterion_id, code_set_date_id, last_modified_user)
+select crit_id,
+	(select id from openchpl.code_set_date where required_day = '2025-12-31'),
+	-1
+from unnest('{5, 6, 12, 15, 16, 18, 19, 20, 21, 28, 43, 45, 46, 165, 167}'::int[]) crit_id
+where not exists (
+	select * 
+	from openchpl.code_set_date_criteria_map 
+	where certification_criterion_id = crit_id
+	and code_set_date_id = (select id from openchpl.code_set_date where required_day = '2025-12-31')
+);
