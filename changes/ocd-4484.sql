fcba9534a908c3091bcad0c017d214dcf4644f5d
Katy Ekey <katy.ekey@gmail.com>
2024-03-12 07:30:53 -0400

    data: Migrate the two uses of 'Other' process types differently
    [#OCD-4484]

--- changes/ocd-4484.sql
+++ changes/ocd-4484.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS openchpl.quarterly_report_surveillance_process_type_m
     id bigserial not null,
     quarterly_report_surveillance_map_id bigint not null,
 	surveillance_process_type_id bigint,
-	surveillance_process_type_other text,
     creation_date timestamp not null default now(),
     last_modified_date timestamp not null default now(),
     last_modified_user bigint,
@@ -25,11 +24,13 @@ CREATE or replace TRIGGER quarterly_report_surveillance_process_type_map_timesta
 DROP TRIGGER IF EXISTS quarterly_report_surveillance_process_type_map_last_modified_user_constraint ON openchpl.quarterly_report_surveillance_process_type_map;
 CREATE CONSTRAINT TRIGGER quarterly_report_surveillance_process_type_map_last_modified_user_constraint AFTER INSERT OR UPDATE ON openchpl.quarterly_report_surveillance_process_type_map DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE openchpl.last_modified_user_constraint();
 
--- migrate the old data into the new mapping table if the new table is empty (otherwise it's already been migrated)
-INSERT INTO openchpl.quarterly_report_surveillance_process_type_map (quarterly_report_surveillance_map_id, surveillance_process_type_id, surveillance_process_type_other, last_modified_user, creation_date, last_modified_date, deleted)
-SELECT id, surveillance_process_type_id, surveillance_process_type_other, last_modified_user, creation_date, last_modified_date, deleted
+-- Migrate the old data into the new mapping table if the new table is empty (otherwise it's already been migrated)
+-- Ignore 'Other' records - we will handle those below
+INSERT INTO openchpl.quarterly_report_surveillance_process_type_map (quarterly_report_surveillance_map_id, surveillance_process_type_id,  last_modified_user, creation_date, last_modified_date, deleted)
+SELECT id, surveillance_process_type_id, last_modified_user, creation_date, last_modified_date, deleted
 FROM openchpl.quarterly_report_surveillance_map
-WHERE NOT EXISTS (
+WHERE surveillance_process_type_id != 5
+AND NOT EXISTS (
 	SELECT * 
 	FROM openchpl.quarterly_report_surveillance_process_type_map 
 );
@@ -53,7 +54,32 @@ WHERE NOT EXISTS (
 	SELECT * FROM openchpl.surveillance_process_type WHERE name = 'Correspondence with Developer'
 );
 
-
--- TODO: There are only 2 records using the 'surveillance process type other' field and they both have the 
--- value 'Correspondence with Developer' so I wonder if we can just make those NOT be 'Other' type and map
+-- There are only 2 records using the 'surveillance process type other' field and they both have the 
+-- value 'Correspondence with Developer'. Katie said we can just make those NOT be 'Other' type and map
 -- them to the new process type added above. 
+INSERT INTO openchpl.quarterly_report_surveillance_process_type_map (quarterly_report_surveillance_map_id, surveillance_process_type_id,  last_modified_user, creation_date, last_modified_date, deleted)
+SELECT id, (SELECT id from openchpl.surveillance_process_type WHERE name = 'Correspondence with Developer'), last_modified_user, creation_date, last_modified_date, deleted
+FROM openchpl.quarterly_report_surveillance_map
+WHERE id = 307
+AND NOT EXISTS (
+	SELECT * 
+	FROM openchpl.quarterly_report_surveillance_process_type_map 
+	WHERE quarterly_report_surveillance_map_id = 307
+	AND surveillance_process_type_id = (SELECT id from openchpl.surveillance_process_type WHERE name = 'Correspondence with Developer')
+);
+
+UPDATE openchpl.quarterly_report_surveillance_map SET surveillance_process_type_other = NULL WHERE id = 307;
+
+INSERT INTO openchpl.quarterly_report_surveillance_process_type_map (quarterly_report_surveillance_map_id, surveillance_process_type_id,  last_modified_user, creation_date, last_modified_date, deleted)
+SELECT id, (SELECT id from openchpl.surveillance_process_type WHERE name = 'Correspondence with Developer'), last_modified_user, creation_date, last_modified_date, deleted
+FROM openchpl.quarterly_report_surveillance_map
+WHERE id = 315
+AND NOT EXISTS (
+	SELECT * 
+	FROM openchpl.quarterly_report_surveillance_process_type_map 
+	WHERE quarterly_report_surveillance_map_id = 315
+	AND surveillance_process_type_id = (SELECT id from openchpl.surveillance_process_type WHERE name = 'Correspondence with Developer')
+);
+
+UPDATE openchpl.quarterly_report_surveillance_map SET surveillance_process_type_other = NULL WHERE id = 315;
+
