43697af2cc7cceb7c00b4af66e614b0657b3150c
Katy Ekey <katy.ekey@gmail.com>
2024-04-15 14:41:29 -0400

    data: Normalize nulls and empty strings in some criteria attributes
    [#OCD-4538]

--- changes/ocd-4538.sql
+++ changes/ocd-4538.sql
@@ -21,3 +21,25 @@ WHERE NOT EXISTS (
 	FROM openchpl.activity_concept 
 	WHERE concept = 'STANDARD'
 );
+
+-- "Normalize" the string fields in the three attributes where we want to record activity.
+-- Sometimes the string fields are NULL and sometimes they are "".
+-- To avoid recording false activity, we need to settle on an empty field always being one or the other. I chose NULL.
+
+UPDATE openchpl.functionality_tested
+SET name = NULL
+WHERE name = '';
+
+UPDATE openchpl.functionality_tested
+SET additional_information = NULL
+WHERE additional_information = '';
+
+UPDATE openchpl.standard
+SET additional_information = NULL
+WHERE additional_information = '';
+
+UPDATE openchpl.standard
+SET group_name = NULL
+WHERE group_name = '';
+
+
