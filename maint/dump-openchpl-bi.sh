8bc8fc9f16d9c7b498f33515b8615f80ee702537
Katy Ekey <katy.ekey@gmail.com>
2024-04-19 09:57:35 -0400

    refactor: Split scripts into dump and load scripts
    These are going to be called by a Bamboo plan and have to run in
    different steps on different agents. There is not one system that can
    access both our DEV and PROD databases.
    
    [#OCD-4510]

---
+++ maint/dump-openchpl-bi.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+function usage {
+
+    cat <<EOM
+Usage: $(basename "$0") [OPTION]...
+
+  -h VALUE    Specifies the host name of the postgres source database machine 		(default: localhost)
+  -p VALUE    Port to connect on the source database machine						(default: 5432)
+  -u VALUE    User name to connect to the source database 							(default: openchpl_dev)    
+  -d VALUE    Database name on the source system									(default: openchpl)    
+  -f VALUE    File name of output                                                   (default: openchpl.powerbi.backup)
+  -?          display help
+EOM
+
+    exit 2
+}
+
+SOURCE_HOST=localhost
+SOURCE_PORT=5432
+SOURCE_USER=openchpl_dev
+SOURCE_DB=openchpl
+FILE=openchpl.powerbi.backup
+
+while getopts "h:p:u:d:f?" OPTION; do
+    case "$OPTION" in
+        h)
+			SOURCE_HOST=$OPTARG
+            ;;
+        p)
+            SOURCE_PORT=$OPTARG
+            ;;
+        u)
+            SOURCE_USER=$OPTARG
+            ;;			
+        d)
+            SOURCE_DB=$OPTARG
+            ;;						
+        f)
+            FILE=$OPTARG
+            ;;
+        ?)
+        usage
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+
+echo "h = $SOURCE_HOST"
+echo "p = $SOURCE_PORT"
+echo "u = $SOURCE_USER"
+echo "d = $SOURCE_DB"
+echo "f = $FILE"
+
+# Dump data that Power BI needs from the source database
+echo "Dumping data from $SOURCE_DB database on $SOURCE_HOST"
+pg_dump --host $SOURCE_HOST --port $SOURCE_PORT --username $SOURCE_USER --no-password --format custom --blobs --verbose --exclude-schema=shared_store --exclude-schema=quartz --exclude-schema=ff4j --exclude-table-data=audit.* --exclude-table-data=openchpl.api_key_activity  --exclude-table-data=openchpl.activity --exclude-table-data=openchpl.activity_concept --exclude-table-data=openchpl.questionable_activity_certification_result --exclude-table-data=openchpl.questionable_activity_developer --exclude-table-data=openchpl.questionable_activity_listing --exclude-table-data=openchpl.questionable_activity_product --exclude-table-data=openchpl.questionable_activity_trigger --exclude-table-data=openchpl.questionable_activity_version --exclude-table-data=openchpl.complaint --exclude-table-data=openchpl.complainant_type --exclude-table-data=openchpl.complaint_criterion_map --exclude-table-data=openchpl.complaint_listing_map --exclude-table-data=openchpl.complaint_surveillance_map --file $FILE $SOURCE_DB
+echo "Completed dumping data from $SOURCE_DB database on $SOURCE_HOST into $FILE"
+
+
+
--- maint/load-openchpl-bi.sh
+++ maint/load-openchpl-bi.sh
@@ -5,24 +5,18 @@ function usage {
     cat <<EOM
 Usage: $(basename "$0") [OPTION]...
 
-  -h VALUE    Specifies the host name of the postgres source database machine 		(default: localhost)
-  -p VALUE    Port to connect on the source database machine						(default: 5432)
-  -u VALUE    User name to connect to the source database 							(default: openchpl_dev)
-  -i VALUE    Specifies the host name of the postgres destination database machine 	(default: localhost)
-  -q VALUE    Port to connect on the destination database machine					(default: 5432)
-  -v VALUE    User name to connect to the destination database for queries			(default: openchpl_bi)  
-  -w VALUE    User name to connect to the destination database for admin			(default: openchpl_bi_admin)    
-  -d VALUE    Database name on the destination system								(default: openchpl_bi)    
-  -f VALUE    File name of output                                                   (default: openchpl.powerbi.backup)
+  -h VALUE    Specifies the host name of the postgres destination database machine 		(default: localhost)
+  -p VALUE    Port to connect on the destination database machine						(default: 5432)
+  -u VALUE    User name to connect to the destination database for queries				(default: openchpl_bi)
+  -v VALUE    User name to connect to the destination database for admin				(default: openchpl_bi_admin)    
+  -d VALUE    Database name on the destination system									(default: openchpl_bi)    
+  -f VALUE    File name to load                                                   		(default: openchpl.powerbi.backup)
   -?          display help
 EOM
 
     exit 2
 }
 
-SOURCE_HOST=localhost
-SOURCE_PORT=5432
-SOURCE_USER=openchpl_dev
 DEST_HOST=localhost
 DEST_PORT=5432
 DEST_QUERY_USER=openchpl_bi
@@ -30,29 +24,20 @@ DEST_ADMIN_USER=openchpl_bi_admin
 DEST_DB=openchpl_bi
 FILE=openchpl.powerbi.backup
 
-while getopts "h:p:u:i:q:v:w:d:f?" OPTION; do
+while getopts "h:p:u:v:d:f?" OPTION; do
     case "$OPTION" in
         h)
-			SOURCE_HOST=$OPTARG
-            ;;
-        p)
-            SOURCE_PORT=$OPTARG
-            ;;
-        u)
-            SOURCE_USER=$OPTARG
-            ;;			
-        i)
 			DEST_HOST=$OPTARG
             ;;
-        q)
+        p)
             DEST_PORT=$OPTARG
-            ;;		
-        v)
+            ;;
+        u)
             DEST_QUERY_USER=$OPTARG
-            ;;		
-        w)
+            ;;			
+        v)
             DEST_ADMIN_USER=$OPTARG
-            ;;				
+            ;;		
         d)
             DEST_DB=$OPTARG
             ;;						
@@ -66,28 +51,20 @@ while getopts "h:p:u:i:q:v:w:d:f?" OPTION; do
 done
 shift $((OPTIND-1))
 
-echo "h = $SOURCE_HOST"
-echo "p = $SOURCE_PORT"
-echo "u = $SOURCE_USER"
-echo "i = $DEST_HOST"
-echo "q = $DEST_PORT"
-echo "v = $DEST_QUERY_USER"
-echo "w = $DEST_ADMIN_USER"
+echo "h = $DEST_HOST"
+echo "p = $DEST_PORT"
+echo "u = $DEST_QUERY_USER"
+echo "v = $DEST_ADMIN_USER"
 echo "d = $DEST_DB"
 echo "f = $FILE"
 
 # We probably need two user accounts on the destination:
 #	1. An ADMIN type of user that has permission to drop and create tables and delete data, and
 #	2. A user that we will connect to Power BI with that can read all the tables, views etc
-# The destination users must exist before continuing. 
 # This should mimic the openchpl_role sql that we use when creating other databases. 
 # It is not checked into this file because creating the users includes assigning a password.
+# The destination users must exist before running this script. 
 # Destination database must exist and be owned by the ADMIN user
-
-# Dump data that Power BI needs from the source database
-echo "Dumping data from openchpl database on $SOURCE_HOST"
-pg_dump --host $SOURCE_HOST --port $SOURCE_PORT --username $SOURCE_USER --no-password --format custom --blobs --verbose --exclude-schema=shared_store --exclude-schema=quartz --exclude-schema=ff4j --exclude-table-data=audit.* --exclude-table-data=openchpl.api_key_activity  --exclude-table-data=openchpl.activity --exclude-table-data=openchpl.activity_concept --exclude-table-data=openchpl.questionable_activity_certification_result --exclude-table-data=openchpl.questionable_activity_developer --exclude-table-data=openchpl.questionable_activity_listing --exclude-table-data=openchpl.questionable_activity_product --exclude-table-data=openchpl.questionable_activity_trigger --exclude-table-data=openchpl.questionable_activity_version --exclude-table-data=openchpl.complaint --exclude-table-data=openchpl.complainant_type --exclude-table-data=openchpl.complaint_criterion_map --exclude-table-data=openchpl.complaint_listing_map --exclude-table-data=openchpl.complaint_surveillance_map --file $FILE openchpl
-echo "Completed dumping data from openchpl database on $SOURCE_HOST into $FILE"
 
 # Clearing the tables in the destination db
 echo "Clearing $DEST_DB database on $DEST_HOST"
