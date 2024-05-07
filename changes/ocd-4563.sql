c513567be20573eadf91206756ba7e3195259a71
Katy Ekey <katy.ekey@gmail.com>
2024-05-06 20:28:02 -0400

    fix: Insert creation date to match the rest of the existing subjects
    [#OCD-4563]

--- changes/ocd-4563.sql
+++ changes/ocd-4563.sql
@@ -25,7 +25,7 @@ CREATE FUNCTION openchpl.create_missing_subscriptions_for_new_subjects()
 		subject_id bigint;
 	BEGIN
 		FOR unique_subscription_rec IN
-			SELECT DISTINCT subscriber_id, subscribed_object_id FROM openchpl.subscription
+			SELECT DISTINCT subscriber_id, subscribed_object_id, creation_date FROM openchpl.subscription
 		LOOP
 		
 			-- RWT Plans URL Subjects
@@ -33,12 +33,12 @@ CREATE FUNCTION openchpl.create_missing_subscriptions_for_new_subjects()
 			
 			IF (SELECT COUNT(*) FROM openchpl.subscription WHERE subscriber_id = unique_subscription_rec.subscriber_id AND subscription_subject_id = subject_id AND subscribed_object_id = unique_subscription_rec.subscribed_object_id AND subscription_consolidation_method_id = 1 AND deleted = false)>0 THEN
 				-- subscriptions already exists, print notice
-				RAISE NOTICE 'Subscription for subscriber %s, subject %s, object %s already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
+				RAISE NOTICE 'Subscription for subscriber %, subject %, object % already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
 			ELSE
 				-- insert the subscription
-				INSERT INTO openchpl.subscription (subscriber_id, subscription_subject_id, subscribed_object_id, subscription_consolidation_method_id, last_modified_user)
-				VALUES (unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id, 1, -1);
-				RAISE NOTICE 'Added subscription for subscriber %s, subject %s, object %s already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
+				INSERT INTO openchpl.subscription (subscriber_id, subscription_subject_id, subscribed_object_id, subscription_consolidation_method_id, creation_date, last_modified_user)
+				VALUES (unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id, 1, unique_subscription_rec.creation_date, -1);
+				RAISE NOTICE 'Added subscription for subscriber %, subject %, object % already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
 			END IF;
 		
 			-- RWT Results URL subjects
@@ -46,12 +46,12 @@ CREATE FUNCTION openchpl.create_missing_subscriptions_for_new_subjects()
 			
 			IF (SELECT COUNT(*) FROM openchpl.subscription WHERE subscriber_id = unique_subscription_rec.subscriber_id AND subscription_subject_id = subject_id AND subscribed_object_id = unique_subscription_rec.subscribed_object_id AND subscription_consolidation_method_id = 1 AND deleted = false)>0 THEN
 				-- subscriptions already exists, print notice
-				RAISE NOTICE 'Subscription for subscriber %s, subject %s, object %s already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
+				RAISE NOTICE 'Subscription for subscriber %, subject %, object % already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
 			ELSE
 				-- insert the subscription
-				INSERT INTO openchpl.subscription (subscriber_id, subscription_subject_id, subscribed_object_id, subscription_consolidation_method_id, last_modified_user)
-				VALUES (unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id, 1, -1);
-				RAISE NOTICE 'Added subscription for subscriber %s, subject %s, object %s already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
+				INSERT INTO openchpl.subscription (subscriber_id, subscription_subject_id, subscribed_object_id, subscription_consolidation_method_id, creation_date, last_modified_user)
+				VALUES (unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id, 1, unique_subscription_rec.creation_date, -1);
+				RAISE NOTICE 'Added subscription for subscriber %, subject %, object % already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
 			END IF;
 			
 			-- Service Base URL List subjects
@@ -59,12 +59,12 @@ CREATE FUNCTION openchpl.create_missing_subscriptions_for_new_subjects()
 			
 			IF (SELECT COUNT(*) FROM openchpl.subscription WHERE subscriber_id = unique_subscription_rec.subscriber_id AND subscription_subject_id = subject_id AND subscribed_object_id = unique_subscription_rec.subscribed_object_id AND subscription_consolidation_method_id = 1 AND deleted = false)>0 THEN
 				-- subscriptions already exists, print notice
-				RAISE NOTICE 'Subscription for subscriber %s, subject %s, object %s already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
+				RAISE NOTICE 'Subscription for subscriber %, subject %, object % already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
 			ELSE
 				-- insert the subscription
-				INSERT INTO openchpl.subscription (subscriber_id, subscription_subject_id, subscribed_object_id, subscription_consolidation_method_id, last_modified_user)
-				VALUES (unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id, 1, -1);
-				RAISE NOTICE 'Added subscription for subscriber %s, subject %s, object %s already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
+				INSERT INTO openchpl.subscription (subscriber_id, subscription_subject_id, subscribed_object_id, subscription_consolidation_method_id, creation_date, last_modified_user)
+				VALUES (unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id, 1, unique_subscription_rec.creation_date, -1);
+				RAISE NOTICE 'Added subscription for subscriber %, subject %, object % already exists', unique_subscription_rec.subscriber_id, subject_id, unique_subscription_rec.subscribed_object_id;
 			END IF;
 			
 
