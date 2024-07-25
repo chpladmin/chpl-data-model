0b37574de35ebc360a1567657bd592640c72d9b1
Katy Ekey <katy.ekey@gmail.com>
2024-07-25 16:06:58 -0400

    data!: Backfill notification timestamp for subscription observations
    If a subscription is not deleted, then the "last modified date" of each
    observation correlates to the time that the observation was marked as
    deleted (which is the same time the user was notified). We can use this
    information to backfill the notified_at column for all existing
    observations.
    
    Currently there are no "deleted" subscriptions. It's likely people don't
    care enough to unsubscribe.
    
    [#OCD-4483]

--- changes/ocd-4483.sql
+++ changes/ocd-4483.sql
@@ -1,2 +1,21 @@
+DROP TRIGGER IF EXISTS subscription_observation_audit ON subscription_observation;
+DROP TRIGGER IF EXISTS subscription_observation_timestamp ON subscription_observation;
+DROP TRIGGER IF EXISTS subscription_observation_last_modified_user_constraint ON openchpl.subscription_observation;
+
 ALTER TABLE openchpl.subscription_observation
 ADD COLUMN IF NOT EXISTS notified_at timestamp;
+
+-- If a subscription is not deleted then the only way it's observations could have been deleted would have
+-- been because the user was notified about them. So for every deleted observation related to every 
+-- non-deleted subscription, we can fill in the notified_at timestamp.
+UPDATE openchpl.subscription_observation obs1 
+SET notified_at = 
+	(SELECT obs2.last_modified_date 
+		FROM openchpl.subscription_observation obs2 
+		JOIN openchpl.subscription sub ON obs2.subscription_id = sub.id AND sub.deleted = false
+		WHERE obs1.id = obs2.id);
+
+
+CREATE OR REPLACE TRIGGER subscription_observation_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.subscription_observation FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE OR REPLACE TRIGGER subscription_observation_timestamp BEFORE UPDATE on openchpl.subscription_observation FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
+CREATE CONSTRAINT TRIGGER subscription_observation_last_modified_user_constraint AFTER INSERT OR UPDATE ON openchpl.subscription_observation DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE openchpl.last_modified_user_constraint();
