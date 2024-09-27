4983cb6f8f822966db254314c7a947e985f71290
Katy Ekey <katy.ekey@gmail.com>
2024-09-27 14:25:41 -0400

    data!: Clean up multiple spaces in user-entered data
    Some fields that users can enter as free-text were found to have
    multiple spaces internally. We were already trimming the fields on the
    ends, but we also want to clean up these additional spaces in the middle
    of the data. We do not believe the spaces have meaning and they have
    never been visible in the UI because HTML displays infinite contiguous
    spaces as one single space.
    
    [#OCD-4690]

---
+++ changes/ocd-4690.sql
@@ -0,0 +1,24 @@
+-- remove unused table
+DROP TABLE IF EXISTS openchpl.optional_functionality_met;
+
+-- remove areas where there is > 1 contiguous space internal to a string
+UPDATE openchpl.address SET street_line_1 = trim(regexp_replace(street_line_1, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certification_result_additional_software SET name = trim(regexp_replace(name, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certification_result_additional_software SET version = trim(regexp_replace(version, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certification_result_test_data SET version = trim(regexp_replace(version, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certification_result_test_data SET alteration = trim(regexp_replace(alteration, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certification_result_test_tool SET version = trim(regexp_replace(version, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certification_result_ucd_process SET ucd_process_details = trim(regexp_replace(ucd_process_details, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certified_product SET sed_intended_user_description = trim(regexp_replace(sed_intended_user_description, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certified_product SET product_additional_software = trim(regexp_replace(product_additional_software, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certified_product_qms_standard SET modification = trim(regexp_replace(modification, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.certified_product_qms_standard SET applicable_criteria = trim(regexp_replace(applicable_criteria, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.contact SET phone_number = trim(regexp_replace(phone_number, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.contact SET title = trim(regexp_replace(title, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.contact SET full_name = trim(regexp_replace(full_name, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.contact SET friendly_name = trim(regexp_replace(friendly_name, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.functionality_tested SET name = trim(regexp_replace(name, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.functionality_tested SET value = trim(regexp_replace(value, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.measure SET measure_name = trim(regexp_replace(measure_name, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.product SET name = trim(regexp_replace(name, '\s+', ' ', 'g')) WHERE deleted = false;
+UPDATE openchpl.targeted_user SET name = trim(regexp_replace(name, '\s+', ' ', 'g')) WHERE deleted = false;
