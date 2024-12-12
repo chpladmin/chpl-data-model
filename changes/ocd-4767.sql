ca10b7c4cd8139649bd36d63b6bd912d20737f17
Todd Young <tyoung@ainq.com>
2024-12-12 12:28:19 -0500

    fix: use CHPL user id instead of Cognito user id
    OCD-4767

--- changes/ocd-4767.sql
+++ changes/ocd-4767.sql
@@ -1,7 +1,7 @@
 update openchpl.url_uptime_monitor_test
 set deleted = true,
-	last_modified_sso_user = '3438c418-50e1-70f0-4d80-804fb08dd7d5',
-	last_modified_user = null
+	last_modified_sso_user = null,
+	last_modified_user = -1
 where passed = false
 and url_uptime_monitor_id in (
 	select id
