318d5f301439c9f77ea323080a17ecf618160724
Todd Young <tyoung@ainq.com>
2025-01-06 22:25:41 -0500

    data: add report metadata to display the RWT report in the dashboard
    OCD-4517

--- changes/ocd-4517.sql
+++ changes/ocd-4517.sql
@@ -44,3 +44,42 @@ CREATE OR replace TRIGGER real_world_testing_results_summary_report_timestamp BE
 DROP TRIGGER IF EXISTS real_world_testing_results_summary_report_last_modified_user_constraint ON openchpl.real_world_testing_results_summary_report;
 CREATE CONSTRAINT TRIGGER real_world_testing_results_summary_report_last_modified_user_constraint AFTER INSERT OR UPDATE ON openchpl.real_world_testing_results_summary_report DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE openchpl.last_modified_user_constraint();
 
+insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_user)
+select 'DEV', 'Real World Testing Summary', 'RealWorldTestingSummary', 'dashboard',
+    'https://app.powerbi.com/view?r=eyJrIjoiNjYwYWQxYWMtYTkxMy00NGZkLWEwMWQtNTFhZmRkOGRlNDVlIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+	'835px', 9, -1
+where not exists (
+    select * 
+	from openchpl.report_metadata
+	where environment = 'DEV'
+	and report_key = 'RealWorldTestingSummary');
+
+insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_user)
+select 'QA', 'Real World Testing Summary', 'RealWorldTestingSummary', 'dashboard',
+    'https://app.powerbi.com/view?r=eyJrIjoiYzRlZTc1YTYtY2QzYy00NmE0LTg3NmEtZTk0YjBhZWVjY2I1IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+	'835px', 9, -1
+where not exists (
+    select * 
+	from openchpl.report_metadata
+	where environment = 'QA'
+	and report_key = 'RealWorldTestingSummary');
+
+insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_user)
+select 'STG', 'Real World Testing Summary', 'RealWorldTestingSummary', 'dashboard',
+    'https://app.powerbi.com/view?r=eyJrIjoiZGQxZTc3MDctM2Y3NC00MjhmLWEzZGItMDdhYjkzYTQ4M2NiIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+	'835px', 9, -1
+where not exists (
+    select * 
+	from openchpl.report_metadata
+	where environment = 'STG'
+	and report_key = 'RealWorldTestingSummary');
+
+insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_user)
+select 'PROD', 'Real World Testing Summary', 'RealWorldTestingSummary', 'dashboard',
+    'https://app.powerbi.com/view?r=eyJrIjoiMmUxZWUwNmQtMTExNi00OGVmLTk1YjItNzY1Y2YzNWM5ZjQ0IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+	'835px', 9, -1
+where not exists (
+    select * 
+	from openchpl.report_metadata
+	where environment = 'PROD'
+	and report_key = 'RealWorldTestingSummary');
