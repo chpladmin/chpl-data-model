90e5cd4b213f552dccf3796268d9a035f2841a64
Todd Young <tyoung@ainq.com>
2025-01-14 13:18:32 -0500

    data: update the hieght of the report frame
    OCD-4520

--- changes/ocd-4520.sql
+++ changes/ocd-4520.sql
@@ -1,7 +1,7 @@
 insert into openchpl.report_metadata (environment, title, report_key, url, height, display_order, last_modified_user)
 select 'DEV', 'Non-Conformity Counts', 'Non-conformityCounts',
     'https://app.powerbi.com/view?r=eyJrIjoiNWFjZGRjZjItZGUyMC00ODc0LWI1NTMtOGVhZjdjZTgyOGJlIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-    '650px', 1, -1
+    '675px', 1, -1
 where not exists (
     select *
 	from openchpl.report_metadata
@@ -11,7 +11,7 @@ where not exists (
 insert into openchpl.report_metadata (environment, title, report_key, url, height, display_order, last_modified_user)
 select 'QA', 'Non-Conformity Counts', 'Non-conformityCounts',
     'https://app.powerbi.com/view?r=eyJrIjoiNDI3ZjhhZTgtMzNmYi00NDI4LWEzNTgtNzY4MjY3YzZkZTRmIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-    '650px', 1, -1
+    '675px', 1, -1
 where not exists (
     select *
 	from openchpl.report_metadata
@@ -21,7 +21,7 @@ where not exists (
 insert into openchpl.report_metadata (environment, title, report_key, url, height, display_order, last_modified_user)
 select 'STG', 'Non-Conformity Counts', 'Non-conformityCounts',
     'https://app.powerbi.com/view?r=eyJrIjoiOTA4YWQ1ZTAtZjdmOC00NzVlLWI3ZTEtMGFkOTdkYzViM2NmIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-    '650px', 1, -1
+    '675px', 1, -1
 where not exists (
     select *
 	from openchpl.report_metadata
@@ -31,7 +31,7 @@ where not exists (
 insert into openchpl.report_metadata (environment, title, report_key, url, height, display_order, last_modified_user)
 select 'PROD', 'Non-Conformity Counts', 'Non-conformityCounts',
     'https://app.powerbi.com/view?r=eyJrIjoiNTg0NzBiMDQtNjg0YS00MGE0LWI2MzMtNzY1ZDg4YzI0MWQxIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-    '650px', 1, -1
+    '675px', 1, -1
 where not exists (
     select *
 	from openchpl.report_metadata
