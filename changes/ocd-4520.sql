ca91e84983120a05957369e793b3f39f10bd2499
Todd Young <tyoung@ainq.com>
2025-01-14 09:51:15 -0500

    data: update the hieght of the Unique Products report
    OCD-4520

--- changes/ocd-4520.sql
+++ changes/ocd-4520.sql
@@ -29,11 +29,15 @@ where not exists (
 	and report_key = 'Non-conformityCounts');
 
 insert into openchpl.report_metadata (environment, title, report_key, url, height, display_order, last_modified_user)
-select 'PROD', 'Non-conformity Counts', 'Non-conformityCounts',
+select 'PROD', 'Non-Conformity Counts', 'Non-conformityCounts',
     'https://app.powerbi.com/view?r=eyJrIjoiNTg0NzBiMDQtNjg0YS00MGE0LWI2MzMtNzY1ZDg4YzI0MWQxIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-    '800px', 1, -1
+    '650px', 1, -1
 where not exists (
     select *
 	from openchpl.report_metadata
 	where environment = 'PROD'
 	and report_key = 'Non-conformityCounts');
+
+update openchpl.report_metadata
+set height = '650px'
+where report_key = 'UniqueProducts';
