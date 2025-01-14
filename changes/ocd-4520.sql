e4294970480500341d9f98452d3db8cff7ebb685
Todd Young <tyoung@ainq.com>
2025-01-14 09:50:42 -0500

    fix: update capitalization
    OCD-4520

--- changes/ocd-4520.sql
+++ changes/ocd-4520.sql
@@ -1,7 +1,7 @@
 insert into openchpl.report_metadata (environment, title, report_key, url, height, display_order, last_modified_user)
-select 'DEV', 'Non-conformity Counts', 'Non-conformityCounts',
+select 'DEV', 'Non-Conformity Counts', 'Non-conformityCounts',
     'https://app.powerbi.com/view?r=eyJrIjoiNWFjZGRjZjItZGUyMC00ODc0LWI1NTMtOGVhZjdjZTgyOGJlIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-    '800px', 1, -1
+    '650px', 1, -1
 where not exists (
     select *
 	from openchpl.report_metadata
@@ -9,9 +9,9 @@ where not exists (
 	and report_key = 'Non-conformityCounts');
 
 insert into openchpl.report_metadata (environment, title, report_key, url, height, display_order, last_modified_user)
-select 'QA', 'Non-conformity Counts', 'Non-conformityCounts',
+select 'QA', 'Non-Conformity Counts', 'Non-conformityCounts',
     'https://app.powerbi.com/view?r=eyJrIjoiNDI3ZjhhZTgtMzNmYi00NDI4LWEzNTgtNzY4MjY3YzZkZTRmIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-    '800px', 1, -1
+    '650px', 1, -1
 where not exists (
     select *
 	from openchpl.report_metadata
@@ -19,9 +19,9 @@ where not exists (
 	and report_key = 'Non-conformityCounts');
 
 insert into openchpl.report_metadata (environment, title, report_key, url, height, display_order, last_modified_user)
-select 'STG', 'Non-conformity Counts', 'Non-conformityCounts',
+select 'STG', 'Non-Conformity Counts', 'Non-conformityCounts',
     'https://app.powerbi.com/view?r=eyJrIjoiOTA4YWQ1ZTAtZjdmOC00NzVlLWI3ZTEtMGFkOTdkYzViM2NmIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-    '800px', 1, -1
+    '650px', 1, -1
 where not exists (
     select *
 	from openchpl.report_metadata
