566291d93a57208e6bcb8f63e8fcda9054fd1401
Katy Ekey <katy.ekey@gmail.com>
2025-01-21 13:48:15 -0500

    release: deploy version 25.6.1 on 21 January 2025
    [#ver-25.6.1]

--- changes/ocd-4785.sql
+++
@@ -1,7 +0,0 @@
-update openchpl.url_uptime_monitor_test uumt
-set deleted = true 
-where uumt.passed = false 
-and uumt.url_uptime_monitor_id = (
-	select uum.id
-	from openchpl.url_uptime_monitor uum
-	where uum.url = 'https://fhir.eclinicalworks.com/ecwopendev/external/practiceList?pageId=1');
