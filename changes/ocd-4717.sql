e1ced9bbe862b9ffbbe935304e893be28882b875
Todd Young <tyoung@ainq.com>
2025-02-02 21:25:56 -0500

    data: add report metadata for the SVAP Usage Power BI report
    OCD-4717

---
+++ changes/ocd-4717.sql
@@ -0,0 +1,51 @@
+insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_user)
+select 'DEV', 
+        'SVAP Usage',
+        'SVAPUsage', 
+        'dashboard', 
+        'https://app.powerbi.com/view?r=eyJrIjoiNmUxMzE0ZGMtZDRkYi00ODI4LWEyZmMtNTRlMDJiNjA4N2VjIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '925px',
+        10,
+        -1
+where not exists (
+        select * from openchpl.report_metadata where environment = 'DEV' and report_key = 'SVAPUsage'
+);
+
+insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_user)
+select 'QA', 
+        'SVAP Usage',
+        'SVAPUsage', 
+        'dashboard', 
+        'https://app.powerbi.com/view?r=eyJrIjoiM2Y3MzQ1NjUtNDQ0YS00NWVhLWI5MDUtMTZlMzE0YzQ0NGFkIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '925px',
+        10,
+        -1
+where not exists (
+        select * from openchpl.report_metadata where environment = 'QA' and report_key = 'SVAPUsage'
+);
+
+insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_user)
+select 'STG', 
+        'SVAP Usage',
+        'SVAPUsage', 
+        'dashboard', 
+        'https://app.powerbi.com/view?r=eyJrIjoiZjZkYzhhZDItYzg4MC00MmQxLWE2ZjQtNjE1ODc5YTgxZjc5IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '925px',
+        10,
+        -1
+where not exists (
+        select * from openchpl.report_metadata where environment = 'STG' and report_key = 'SVAPUsage' 
+);
+
+insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_user)
+select 'PROD', 
+        'SVAP Usage',
+        'SVAPUsage', 
+        'dashboard', 
+        'https://app.powerbi.com/view?r=eyJrIjoiZjRkNmVhOGYtN2JhNC00ODgzLWFmOGEtYTVkYjIyZTcyOWQ4IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '925px',
+        10,
+        -1
+where not exists (
+        select * from openchpl.report_metadata where environment = 'PROD' and report_key = 'SVAPUsage' 
+);
