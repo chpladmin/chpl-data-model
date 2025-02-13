cfab4b4595313d631fc1c70e9f63b39a44cc5620
Katy Ekey <katy.ekey@gmail.com>
2025-02-13 14:45:47 -0500

    feat!: Add new column for quarterly reports - surveillance findings
    [#OCD-4800]

--- changes/ocd-4800.sql
+++ changes/ocd-4800.sql
@@ -9,3 +9,6 @@ ADD COLUMN IF NOT EXISTS post_certification_performance text;
 
 ALTER TABLE openchpl.quarterly_report
 ADD COLUMN IF NOT EXISTS appropriate_design_mark_use text;
+
+ALTER TABLE openchpl.quarterly_report_surveillance_map
+ADD COLUMN IF NOT EXISTS surveillance_findings text;
