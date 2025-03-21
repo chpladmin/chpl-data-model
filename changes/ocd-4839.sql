a96677b460a651ae4d8649f34be489b8bc287fe6
Todd Young <tyoung@ainq.com>
2025-03-20 20:29:40 -0400

    data: update Service Base URL List report URL
    OCD-4839

---
+++ changes/ocd-4839.sql
@@ -0,0 +1,19 @@
+update openchpl.report_metadata
+set url = 'https://app.powerbi.com/view?r=eyJrIjoiNjhmM2U1OWQtYTZmNi00N2M1LWE4MGQtMTc5NjFiMGE5ODgwIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9'
+where report_key = 'ServiceBaseUrlListReport'
+and environment = 'DEV';
+
+update openchpl.report_metadata
+set url = 'https://app.powerbi.com/view?r=eyJrIjoiOTBkMDU5ZjUtYmYzZS00OWUzLThhOTAtZDU5YzQ2YzUzNDE4IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9'
+where report_key = 'ServiceBaseUrlListReport'
+and environment = 'QA';
+
+update openchpl.report_metadata
+set url = 'https://app.powerbi.com/view?r=eyJrIjoiZjNkMjhhZTItNjk1ZC00YmE3LThmMTEtNGQ5ZDBlMjliMzIzIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9'
+where report_key = 'ServiceBaseUrlListReport'
+and environment = 'STG';
+
+update openchpl.report_metadata
+set url = 'https://app.powerbi.com/view?r=eyJrIjoiYTgzYjczMjktYjY1Yi00OWU3LTg0NDMtNzZlNDQ1NWRjMjcxIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9'
+where report_key = 'ServiceBaseUrlListReport'
+and environment = 'PROD';
