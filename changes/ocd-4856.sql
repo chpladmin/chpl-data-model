f54f7e8c05eeaefdf599ad6d8e1e721335a74d08
Katy Ekey <katy.ekey@gmail.com>
2025-04-03 15:17:07 -0400

    data: Add www to new URL
    [#OCD-4856]

--- changes/ocd-4856.sql
+++ changes/ocd-4856.sql
@@ -1,3 +1,3 @@
 UPDATE openchpl.certification_criterion
-SET certification_companion_guide_link = replace(certification_companion_guide_link, 'https://www.healthit.gov', 'https://astp.hhs.gov');
+SET certification_companion_guide_link = replace(certification_companion_guide_link, 'https://www.healthit.gov', 'https://www.astp.hhs.gov');
 
