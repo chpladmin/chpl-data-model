a948b550e35ff626f878baba3781d0339c8b8416
Katy Ekey <katy.ekey@gmail.com>
2025-04-11 10:04:26 -0400

    data!: Remove domain conversion for astp.hhs.gov
    [#OCD-4856]

--- changes/ocd-4856.sql
+++
@@ -1,3 +0,0 @@
-UPDATE openchpl.certification_criterion
-SET certification_companion_guide_link = replace(certification_companion_guide_link, 'https://www.healthit.gov', 'https://www.astp.hhs.gov');
-
