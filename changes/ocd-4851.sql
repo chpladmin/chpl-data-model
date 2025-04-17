47c5dc495df945a814be4e10453e2ad58dfa81be
Todd Young <tyoung@ainq.com>
2025-04-17 10:32:02 -0400

    feat!: add table to store developers for attestation report
    * feat: add table to store developers for attestation report
    
    OCD-4851
    
    * feat: switch table to uee change_request_status_id instead of change_request_status_type_id
    
    OCD-4851
    
    * feat: remove DROP statement that only needed to be run once
    
    OCD-4851
    
    * feat: drop "not null" on column attestation_report.certification_body_id
    
    OCD-4851

---
+++ changes/ocd-4851.sql
@@ -0,0 +1,30 @@
+ALTER TABLE openchpl.attestation_report ALTER COLUMN certification_body_id DROP NOT NULL;
+
+
+CREATE TABLE IF NOT EXISTS
+  openchpl.attestation_report_developer (
+    id bigserial NOT NULL,
+    attestation_report_id bigint NOT NULL,
+    developer_id bigint NOT NULL,
+    change_request_status_id bigint NULL,
+    creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_date timestamp without time zone NOT NULL DEFAULT now(),
+    last_modified_user bigint NULL,
+    deleted boolean NOT NULL DEFAULT false,
+    last_modified_sso_user uuid NULL,
+	CONSTRAINT attestation_report_developer_pk PRIMARY KEY (id),
+        CONSTRAINT attestation_report_fk FOREIGN KEY (attestation_report_id)
+		REFERENCES openchpl.attestation_report (id)
+		MATCH simple ON UPDATE NO ACTION ON DELETE RESTRICT,
+	CONSTRAINT developer_fk FOREIGN KEY (developer_id)
+                REFERENCES openchpl.vendor (vendor_id)
+                MATCH simple ON UPDATE NO ACTION ON DELETE RESTRICT,
+        CONSTRAINT change_request_status_fk FOREIGN KEY (change_request_status_id)
+                REFERENCES openchpl.change_request_status (id)
+                MATCH simple ON UPDATE NO ACTION ON DELETE RESTRICT
+  );
+
+CREATE OR replace TRIGGER attestation_report_developer_audit AFTER INSERT OR UPDATE OR DELETE on openchpl.attestation_report_developer FOR EACH ROW EXECUTE PROCEDURE audit.if_modified_func();
+CREATE OR replace TRIGGER attestation_report_developer_timestamp BEFORE UPDATE on openchpl.attestation_report_developer FOR EACH ROW EXECUTE PROCEDURE openchpl.update_last_modified_date_column();
+DROP TRIGGER IF EXISTS attestation_report_developer_last_modified_user_constraint ON openchpl.attestation_report_developer;
+CREATE CONSTRAINT TRIGGER attestation_report_developer_last_modified_user_constraint AFTER INSERT OR UPDATE ON openchpl.attestation_report_developer DEFERRABLE INITIALLY DEFERRED FOR EACH ROW EXECUTE PROCEDURE openchpl.last_modified_user_constraint();
