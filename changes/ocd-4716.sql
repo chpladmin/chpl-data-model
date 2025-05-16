31e52ade662d1aa810ffbd13c3a9a3df66faed20
Katy Ekey <katy.ekey@gmail.com>
2025-05-16 10:50:19 -0400

    data!: Increase height of SVAP reports
    [#OCD-4716]

---
+++ changes/ocd-4716.sql
@@ -0,0 +1,8 @@
+UPDATE openchpl.report_metadata
+SET height = '1025px'
+WHERE title = 'SVAP Usage by SVAP';
+
+UPDATE openchpl.report_metadata
+SET height = '1025px'
+WHERE title = 'SVAP Usage by Criteria';
+
