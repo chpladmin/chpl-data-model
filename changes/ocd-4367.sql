e187ca58099cc608037863df23c40fdfd2b531bc
Andrew Larned <andrew.larned@gmail.com>
2025-05-19 13:35:42 -0400

    release: deploy version 27.1.0 on 19 May 2025
    [#ver-27.1.0]

--- changes/ocd-4367.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO openchpl.questionable_activity_trigger (name, level, last_modified_sso_user)
-SELECT 'Created ICS Listing With Withdrawn Parent', 'Listing', '6498c4f8-b0f1-70b5-55de-d84faae73402'
-WHERE NOT EXISTS (
-    SELECT *
-    FROM openchpl.questionable_activity_trigger
-    WHERE name = 'Created ICS Listing With Withdrawn Parent');
--- changes/ocd-4862.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE openchpl.test_tool
-DROP COLUMN IF EXISTS regulatory_text_citation;
-
-ALTER TABLE openchpl.test_tool
-DROP COLUMN IF EXISTS rule_id;
