1b0e2978ede0f1f371ac0e3730ddfa4ee13363f3
Katy Ekey <katy.ekey@gmail.com>
2025-06-17 15:40:32 -0400

    feat: Remove listing url type and just use change request type
    [#OCD-4805]

--- changes/ocd-4805.sql
+++ changes/ocd-4805.sql
@@ -1,7 +1,15 @@
-INSERT INTO openchpl.change_request_listing_url_type (name, last_modified_sso_user)
-SELECT 'RWT Plans URL', '6498c4f8-b0f1-70b5-55de-d84faae73402'
-WHERE NOT EXISTS (SELECT * FROM openchpl.change_request_listing_url_type WHERE name = 'RWT Plans URL');
+ALTER TABLE openchpl.change_request_listing_url DROP COLUMN IF EXISTS change_request_listing_url_type_id;
 
-INSERT INTO openchpl.change_request_listing_url_type (name, last_modified_sso_user)
-SELECT 'RWT Results URL', '6498c4f8-b0f1-70b5-55de-d84faae73402'
-WHERE NOT EXISTS (SELECT * FROM openchpl.change_request_listing_url_type WHERE name = 'RWT Results URL');
+DROP TABLE IF EXISTS openchpl.change_request_listing_url_type;
+
+UPDATE openchpl.change_request_type
+SET name = 'Service Base URL List Change Request'
+WHERE name = 'Listing URL Change Request';
+
+INSERT INTO openchpl.change_request_type (name, last_modified_sso_user)
+SELECT 'RWT Plans URL Change Request', '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (SELECT * FROM openchpl.change_request_type WHERE name = 'RWT Plans URL Change Request');
+
+INSERT INTO openchpl.change_request_type (name, last_modified_sso_user)
+SELECT 'RWT Results URL Change Request', '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (SELECT * FROM openchpl.change_request_type WHERE name = 'RWT Results URL Change Request');
