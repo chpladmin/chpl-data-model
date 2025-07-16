d3b7dac6cc14df02e9c2a8544d93d58cca9888dc
Katy Ekey <katy.ekey@gmail.com>
2025-07-16 07:53:29 -0400

    data: Put in real URL and report name
    [#OCD-4895]

--- changes/ocd-4895.sql
+++ changes/ocd-4895.sql
@@ -1,51 +1,51 @@
 insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_sso_user)
 select 'DEV', 
-        'Criteria Up-To-Date',
-        'CriteriaUpToDate', 
+        'Updated Criteria Status',
+        'UpdatedCriteriaStatus', 
         'dashboard', 
-        'https://app.powerbi.com/view?r=eyJrIjoiZDM4ZGYxNWMtYmMzNy00NGQ2LTk5ZDEtZDhkYTU1NWRhNTEyIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-        '1100px',
+        'https://app.powerbi.com/view?r=eyJrIjoiNjBlZjk5MGYtMjQ3Ni00ODdhLWE4MjAtNjNlZWViOWNmN2QzIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '700px',
         2,
         '6498c4f8-b0f1-70b5-55de-d84faae73402'
 where not exists (
-        select * from openchpl.report_metadata where environment = 'DEV' and report_key = 'CriteriaUpToDate'
+        select * from openchpl.report_metadata where environment = 'DEV' and report_key = 'UpdatedCriteriaStatus'
 );
 
 insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_sso_user)
 select 'QA', 
-        'Criteria Up-To-Date',
-        'CriteriaUpToDate', 
+        'Updated Criteria Status',
+        'UpdatedCriteriaStatus', 
         'dashboard', 
-        'https://app.powerbi.com/view?r=eyJrIjoiNmRiNzc4YTMtYjE1MC00MzVmLTkzNzgtNDNiMjExZTAyOGE2IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-        '1100px',
+        'https://app.powerbi.com/view?r=eyJrIjoiMTA5MDM3YjUtMjI1NS00MjllLWIwOGItYTQ5MTljNjRkYmY1IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '700px',
         2,
         '6498c4f8-b0f1-70b5-55de-d84faae73402'
 where not exists (
-        select * from openchpl.report_metadata where environment = 'QA' and report_key = 'CriteriaUpToDate' 
+        select * from openchpl.report_metadata where environment = 'QA' and report_key = 'UpdatedCriteriaStatus' 
 );
 
 insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_sso_user)
 select 'STG', 
-        'Criteria Up-To-Date',
-        'CriteriaUpToDate', 
+        'Updated Criteria Status',
+        'UpdatedCriteriaStatus', 
         'dashboard', 
-        'https://app.powerbi.com/view?r=eyJrIjoiNTk0NTUyZTEtMmNiMy00YjQ2LWI0MmMtNDhlNmYxODk3ZmE1IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-        '1100px',
+        'https://app.powerbi.com/view?r=eyJrIjoiNjk3MmFiZjYtZDA2Ni00MTVmLTg1MDMtMzUxYjk2YjcxZGU2IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '700px',
         2,
         '6498c4f8-b0f1-70b5-55de-d84faae73402'
 where not exists (
-        select * from openchpl.report_metadata where environment = 'STG' and report_key = 'CriteriaUpToDate' 
+        select * from openchpl.report_metadata where environment = 'STG' and report_key = 'UpdatedCriteriaStatus' 
 );
 
 insert into openchpl.report_metadata (environment, title, report_key, report_group, url, height, display_order, last_modified_sso_user)
 select 'PROD', 
-        'Criteria Up-To-Date',
-        'CriteriaUpToDate', 
+        'Updated Criteria Status',
+        'UpdatedCriteriaStatus', 
         'dashboard', 
-        'https://app.powerbi.com/view?r=eyJrIjoiMjRkNzhkNjctNjA0OS00YWZlLTkzNWItMjU5ZTJjM2IyNDg3IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
-        '1100px',
+        'https://app.powerbi.com/view?r=eyJrIjoiYTI1M2Y4ODItMmZmMy00NjljLThkYTgtOGZmZDg5NTAyYTljIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '700px',
         2,
         '6498c4f8-b0f1-70b5-55de-d84faae73402'
 where not exists (
-        select * from openchpl.report_metadata where environment = 'PROD' and report_key = 'CriteriaUpToDate' 
+        select * from openchpl.report_metadata where environment = 'PROD' and report_key = 'UpdatedCriteriaStatus' 
 );
