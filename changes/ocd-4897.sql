7e7b80d8a591c2d4b1e2289b9405242ce158c940
Katy Ekey <katy.ekey@gmail.com>
2025-08-05 13:11:03 -0400

    data!: Remove association between f3 and Attestation conformance method
    [#OCD-4897]

---
+++ changes/ocd-4897.sql
@@ -0,0 +1,6 @@
+-- Conformance method "Attestation" is no longer allowed for f3 criteria. It used to be allowed if GAP was true on that criteria
+
+UPDATE openchpl.conformance_method_criteria_map
+SET deleted = true
+WHERE criteria_id = 45 -- f3
+AND conformance_method_id = 1; -- Attestation
