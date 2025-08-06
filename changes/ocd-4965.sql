c63f016480c8d4c05e557683ca19d37a7ec3bd8a
Katy Ekey <katy.ekey@gmail.com>
2025-08-06 10:51:13 -0400

    fix: Use title instead of start_day to distinguish between b4 criteria
    When using the start_day, if the start_day gets overridden in the Bamboo
    plans to have some other value for testing, we ended up inserting
    multiple b4 criteria
    
    [#OCD-4965]

--- changes/ocd-4965.sql
+++ changes/ocd-4965.sql
@@ -17,19 +17,19 @@ SELECT '170.315 (b)(4)',
 		(SELECT id FROM openchpl.rule WHERE name = 'HTI-4'),
 		'6498c4f8-b0f1-70b5-55de-d84faae73402'
 WHERE NOT EXISTS (
-	SELECT * FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND start_day = '2025-10-01'
+	SELECT * FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND title = 'Real-Time Prescription Benefit'
 );
 
 INSERT INTO openchpl.certification_criterion_attribute (criterion_id, svap, service_base_url_list, optional_standard, test_tool,
 conformance_method, test_procedure, test_data, functionality_tested, privacy_security_framework, additional_software,
 api_documentation, attestation_answer, documentation_url, export_documentation, gap, g1_success, g2_success, sed,
 test_standard, use_cases, risk_management_summary_information, standard, code_set, last_modified_sso_user)
-SELECT (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND start_day = '2025-10-01'),
+SELECT (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND title = 'Real-Time Prescription Benefit'),
 true, false, false, true, true, false, true, false, false, true, false, false, false, false, 
 false, false, false, false, false, false, false, true, true, '6498c4f8-b0f1-70b5-55de-d84faae73402'
 WHERE NOT EXISTS (
 	SELECT * FROM openchpl.certification_criterion_attribute 
-	WHERE criterion_id = (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND start_day = '2025-10-01')
+	WHERE criterion_id = (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND title = 'Real-Time Prescription Benefit')
 );
 
 INSERT INTO openchpl.certification_criterion (number, title, start_day, rule_id, last_modified_sso_user)
@@ -146,12 +146,12 @@ WHERE NOT EXISTS (
 -- add code sets associated with the new criteria
 --
 INSERT INTO openchpl.code_set_criteria_map (certification_criterion_id, code_set_id, last_modified_sso_user)
-SELECT (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND start_day = '2025-10-01'),
+SELECT (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND title = 'Real-Time Prescription Benefit'),
 	(SELECT id FROM openchpl.code_set WHERE required_day = '2025-12-31'),
 	'6498c4f8-b0f1-70b5-55de-d84faae73402'
 WHERE NOT EXISTS (
 	SELECT * FROM openchpl.code_set_criteria_map
-	WHERE certification_criterion_id = (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND start_day = '2025-10-01')
+	WHERE certification_criterion_id = (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND title = 'Real-Time Prescription Benefit')
 	AND code_set_id = (SELECT id FROM openchpl.code_set WHERE required_day = '2025-12-31')
 );
 
@@ -159,12 +159,12 @@ WHERE NOT EXISTS (
 -- add test data associations with the new criteria
 --
 INSERT INTO openchpl.test_data_criteria_map (criteria_id, test_data_id, last_modified_sso_user)
-SELECT (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND start_day = '2025-10-01'),
+SELECT (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND title = 'Real-Time Prescription Benefit'),
 	(SELECT id FROM openchpl.test_data WHERE name = 'ONC Test Method'),
 	'6498c4f8-b0f1-70b5-55de-d84faae73402'
 WHERE NOT EXISTS (
 	SELECT * FROM openchpl.test_data_criteria_map
-	WHERE criteria_id = (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND start_day = '2025-10-01')
+	WHERE criteria_id = (SELECT certification_criterion_id FROM openchpl.certification_criterion WHERE number = '170.315 (b)(4)' AND title = 'Real-Time Prescription Benefit')
 	AND test_data_id = (SELECT id FROM openchpl.test_data WHERE name = 'ONC Test Method')
 );
 
