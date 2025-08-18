6fe7b671f5928a4cb73c8c4d5e8590db2c173f7a
Katy Ekey <katy.ekey@gmail.com>
2025-08-18 13:40:05 -0400

    release: deploy version 27.3.3 on 18 August 2025
    [#ver-27.3.3]

--- v27.3.3.sql
+++ v27.3.3.sql
@@ -1,3 +1,6 @@
+-- Deployment file for version 27.3.3
+--     as of 2025-08-18
+-- ./changes/ocd-4805.sql
 ALTER TABLE openchpl.change_request_listing_url DROP COLUMN IF EXISTS change_request_listing_url_type_id;
 ALTER TABLE openchpl.change_request_listing_url ADD COLUMN IF NOT EXISTS check_date date;
 
@@ -15,3 +18,8 @@ INSERT INTO openchpl.change_request_type (name, last_modified_sso_user)
 SELECT 'RWT Results URL Change Request', '6498c4f8-b0f1-70b5-55de-d84faae73402'
 WHERE NOT EXISTS (SELECT * FROM openchpl.change_request_type WHERE name = 'RWT Results URL Change Request');
 
+;
+insert into openchpl.data_model_version (version, deploy_date, last_modified_user) values ('27.3.3', '2025-08-18', -1);
+\i dev/openchpl_soft-delete.sql
+\i dev/openchpl_views.sql
+\i dev/openchpl_grant-all.sql
