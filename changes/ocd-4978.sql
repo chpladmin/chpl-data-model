7e8e5d213581db490bc611ea6b4b43d5b0a2ab19
Katy Ekey <katy.ekey@gmail.com>
2025-08-19 11:46:19 -0400

    fix: Use correct sql syntax
    [#OCD-4978]

--- changes/ocd-4978.sql
+++ changes/ocd-4978.sql
@@ -11,7 +11,7 @@ AND conformance_method_id = 1; -- Attestation
 -- Add new Conformance Method
 --
 INSERT INTO openchpl.conformance_method (name, last_modified_sso_user)
-SELECT ('Gap certification via 2025 Attestation', '6498c4f8-b0f1-70b5-55de-d84faae73402')
+SELECT 'Gap certification via 2025 Attestation', '6498c4f8-b0f1-70b5-55de-d84faae73402'
 WHERE NOT EXISTS (
 	SELECT * FROM openchpl.conformance_method WHERE name = 'Gap certification via 2025 Attestation'
 );
