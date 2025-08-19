bf175cfb1b8a07d3b6930d5eee1da7faf3c5d81b
Katy Ekey <katy.ekey@gmail.com>
2025-08-19 10:35:28 -0400

    data!: Add new "Gap" conformance method
    [#OCD-4978]

--- changes/ocd-4978.sql
+++ changes/ocd-4978.sql
@@ -1,9 +1,19 @@
--- Conformance method "Attestation" is no longer allowed for f3 criteria. It used to be allowed if GAP was true on that criteria
-
+--
+-- Remove Conformance method "Attestation" association with f3 criterion.
+-- It used to be allowed if GAP was "true" for f3.
+--
 UPDATE openchpl.conformance_method_criteria_map
 SET deleted = true
 WHERE criteria_id = 45 -- f3
 AND conformance_method_id = 1; -- Attestation
 
--- TO DO
--- Add new Conformance Method and associate it with f3 (and other criteria??)
+--
+-- Add new Conformance Method
+--
+INSERT INTO openchpl.conformance_method (name, last_modified_sso_user)
+SELECT ('Gap certification via 2025 Attestation', '6498c4f8-b0f1-70b5-55de-d84faae73402')
+WHERE NOT EXISTS (
+	SELECT * FROM openchpl.conformance_method WHERE name = 'Gap certification via 2025 Attestation'
+);
+
+-- The association between the new conformance method and allowed criteria will be done in the UI by the ADMIN 
