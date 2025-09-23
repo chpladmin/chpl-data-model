702e8ec2f10410369335dea42cae7298e480b9c6
Katy Ekey <katy.ekey@gmail.com>
2025-09-23 10:42:33 -0400

    feat!: Add extension end day to code sets, functested
    [#OCD-5042]

--- changes/ocd-5042.sql
+++ changes/ocd-5042.sql
@@ -1,5 +1,26 @@
+--
+-- standards
+--
 ALTER TABLE openchpl.standard ADD COLUMN IF NOT EXISTS extension_end_day date NULL;
 
 UPDATE openchpl.standard
-SET extension_end_day = '2026-01-31'
-WHERE end_day = '2025-12-31';
+SET extension_end_day = '2026-02-01'
+WHERE required_day = '2025-12-31';
+
+--
+-- code sets
+--
+ALTER TABLE openchpl.code_set ADD COLUMN IF NOT EXISTS extension_end_day date NULL;
+
+UPDATE openchpl.code_set
+SET extension_end_day = '2026-02-01'
+WHERE required_day = '2025-12-31';
+
+--
+-- functionality tested
+--
+ALTER TABLE openchpl.functionality_tested ADD COLUMN IF NOT EXISTS extension_end_day date NULL;
+
+UPDATE openchpl.functionality_tested
+SET extension_end_day = '2026-02-01'
+WHERE required_day = '2025-12-31';
