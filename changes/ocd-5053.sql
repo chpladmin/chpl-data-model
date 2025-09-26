79d72bbf95ddc23e68dafca57ce1ecd5a173a000
Katy Ekey <katy.ekey@gmail.com>
2025-09-26 10:59:38 -0400

    data!: Set correct CCG URL for 170.315 (e)(1)
    [#OCD-5053]

---
+++ changes/ocd-5053.sql
@@ -0,0 +1,3 @@
+UPDATE openchpl.certification_criterion
+SET certification_companion_guide_link = 'https://www.healthit.gov/test-method/view-download-and-transmit-3rd-party'
+WHERE certification_criterion_id = 178;
