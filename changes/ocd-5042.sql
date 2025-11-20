37cecc41ff8d3b8192e36b9d8de5005d6253444b
Katy Ekey <katy.ekey@gmail.com>
2025-11-20 11:10:00 -0500

    data: Set extension end to Feb 28 for all attrs expiring on 12/31
    [#OCD-5042]

--- changes/ocd-5042.sql
+++ changes/ocd-5042.sql
@@ -4,7 +4,7 @@
 ALTER TABLE openchpl.standard ADD COLUMN IF NOT EXISTS extension_end_day date NULL;
 
 UPDATE openchpl.standard
-SET extension_end_day = '2026-02-01', last_modified_sso_user = '6498c4f8-b0f1-70b5-55de-d84faae73402', last_modified_user = NULL
+SET extension_end_day = '2026-02-28', last_modified_sso_user = '6498c4f8-b0f1-70b5-55de-d84faae73402', last_modified_user = NULL
 WHERE required_day = '2025-12-31';
 
 --
@@ -13,7 +13,7 @@ WHERE required_day = '2025-12-31';
 ALTER TABLE openchpl.code_set ADD COLUMN IF NOT EXISTS extension_end_day date NULL;
 
 UPDATE openchpl.code_set
-SET extension_end_day = '2026-02-01', last_modified_sso_user = '6498c4f8-b0f1-70b5-55de-d84faae73402', last_modified_user = NULL
+SET extension_end_day = '2026-02-28', last_modified_sso_user = '6498c4f8-b0f1-70b5-55de-d84faae73402', last_modified_user = NULL
 WHERE required_day = '2025-12-31';
 
 --
@@ -24,7 +24,7 @@ ALTER TABLE openchpl.functionality_tested DROP COLUMN IF EXISTS number;
 ALTER TABLE openchpl.functionality_tested ADD COLUMN IF NOT EXISTS extension_end_day date NULL;
 
 UPDATE openchpl.functionality_tested
-SET extension_end_day = '2026-02-01', last_modified_sso_user = '6498c4f8-b0f1-70b5-55de-d84faae73402', last_modified_user = NULL
+SET extension_end_day = '2026-02-28', last_modified_sso_user = '6498c4f8-b0f1-70b5-55de-d84faae73402', last_modified_user = NULL
 WHERE required_day = '2025-12-31';
 
 --
