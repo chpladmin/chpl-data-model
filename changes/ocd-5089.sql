ecd2a4118c45dc9987c8fecd1d5b9151183b9eca
Katy Ekey <katy.ekey@gmail.com>
2026-01-16 11:47:17 -0500

    fix!: Update powerbi report heights to eliminate internal scrollbar
    [#OCD-5131]
    [#OCD-5089]

---
+++ changes/ocd-5089.sql
@@ -0,0 +1,15 @@
+UPDATE openchpl.report_metadata
+SET height = '800px'
+WHERE title = 'Developer Attestations';
+
+UPDATE openchpl.report_metadata
+SET height = '1350px'
+WHERE title = 'Developer Statistics';
+
+UPDATE openchpl.report_metadata
+SET height = '800px'
+WHERE title = 'Direct Review Statistics';
+
+UPDATE openchpl.report_metadata
+SET height = '1400px'
+WHERE title = 'Product Statistics';
