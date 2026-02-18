35f1348d12b5a80a4323c21de4efc5679af96222
Katy Ekey <katy.ekey@gmail.com>
2026-02-18 11:46:33 -0500

    fix: Add last modified user
    [#OCD-5135]

--- changes/ocd-5135.sql
+++ changes/ocd-5135.sql
@@ -1,3 +1,3 @@
 UPDATE openchpl.certification_result_test_standard
-SET deleted = true
+SET deleted = true, last_modified_user = NULL, last_modified_sso_user = '6498c4f8-b0f1-70b5-55de-d84faae73402'
 WHERE certification_result_test_standard_id IN (13868,13869,13870);
