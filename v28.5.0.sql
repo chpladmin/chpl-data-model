a65cd220c0064330602a450222510a1864f4bd6a
Katy Ekey <katy.ekey@gmail.com>
2026-03-30 13:45:38 -0400

    release: deploy version 28.5.0 on 30 March 2026
    [#ver-28.5.0]

--- v28.5.0.sql
+++ v28.5.0.sql
@@ -1,3 +1,6 @@
+-- Deployment file for version 28.5.0
+--     as of 2026-03-30
+-- ./changes/ocd-4993.sql
 -- This index improves the performance of the get_testing_lab_code function, which is used to generate the chpl product number
 -- This made a significant difference in the speed of getting the chpl product number for each listing
 create index if not exists cptl_idx on openchpl.certified_product_testing_lab_map (certified_product_id);
@@ -9,3 +12,8 @@ create index if not exists developer_idx on openchpl.product (vendor_id);
 analyze openchpl.product;
 create index if not exists product_idx on openchpl.product_version (product_id);
 analyze openchpl.product_version;
+;
+insert into openchpl.data_model_version (version, deploy_date, last_modified_user) values ('28.5.0', '2026-03-30', -1);
+\i dev/openchpl_soft-delete.sql
+\i dev/openchpl_views.sql
+\i dev/openchpl_grant-all.sql
