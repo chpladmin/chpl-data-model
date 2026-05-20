0d22b9b6c8b45d63249fd5d3611f42917a1561d2
Katy Ekey <katy.ekey@gmail.com>
2026-05-20 13:51:43 -0400

    data!: Add updated criteria status dashboard
    [#OCD-5294]

---
+++ changes/ocd-5294.sql
@@ -0,0 +1,135 @@
+--
+-- Add report metadata for the new dashboard report in all environments
+--
+
+INSERT INTO openchpl.report_metadata (environment, title, report_key, report_group, url, height, last_modified_sso_user)
+SELECT 'DEV', 
+        'Updated Criteria Status',
+        'DashboardUpdatedCriteriaStatus', 
+        'onc-dashboard', 
+        'https://app.powerbi.com/view?r=eyJrIjoiMDExZjUxMDctNmIxMi00YjkzLTkwOTItODAxMDZjMDMyYjQ0IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '670px',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata WHERE environment = 'DEV' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard' 
+);
+
+INSERT INTO openchpl.report_metadata (environment, title, report_key, report_group, url, height, last_modified_sso_user)
+SELECT 'QA', 
+        'Updated Criteria Status',
+        'DashboardUpdatedCriteriaStatus', 
+        'onc-dashboard', 
+        'https://app.powerbi.com/view?r=eyJrIjoiZWEyNzg3OTktY2RhNy00Zjc3LWE4MjgtOGEyNTI4NDU1ODNjIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '670px',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata WHERE environment = 'QA' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard' 
+);
+
+INSERT INTO openchpl.report_metadata (environment, title, report_key, report_group, url, height, last_modified_sso_user)
+SELECT 'STG', 
+        'Updated Criteria Status',
+        'DashboardUpdatedCriteriaStatus', 
+        'onc-dashboard', 
+        'https://app.powerbi.com/view?r=eyJrIjoiOTYxYTNlZmYtYzBlNi00MGZkLWE4ZWItMWVhNGVmNTczMjkwIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '670px',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata WHERE environment = 'STG' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard' 
+);
+
+INSERT INTO openchpl.report_metadata (environment, title, report_key, report_group, url, height, last_modified_sso_user)
+SELECT 'PROD', 
+        'Updated Criteria Status',
+        'DashboardUpdatedCriteriaStatus', 
+        'onc-dashboard', 
+        'https://app.powerbi.com/view?r=eyJrIjoiMDU1Yjc5YmItNjNmMC00NDFlLThiZTYtYmZiODdhMGNjYjQ4IiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9',
+        '670px',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata WHERE environment = 'PROD' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard' 
+);
+
+--
+-- Add role mappings for these reports
+-- ADMIN and ONC should have access
+--
+INSERT INTO openchpl.report_metadata_role_map (report_metadata_id, role_name, last_modified_sso_user)
+SELECT (SELECT id FROM openchpl.report_metadata WHERE environment = 'DEV' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard'), 
+        'chpl-admin',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata_role_map 
+		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'DEV' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard') 
+		AND role_name = 'chpl-admin' 
+);
+
+INSERT INTO openchpl.report_metadata_role_map (report_metadata_id, role_name, last_modified_sso_user)
+SELECT (SELECT id FROM openchpl.report_metadata WHERE environment = 'QA' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard'), 
+        'chpl-admin',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata_role_map 
+		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'QA' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard') 
+		AND role_name = 'chpl-admin' 
+);
+
+INSERT INTO openchpl.report_metadata_role_map (report_metadata_id, role_name, last_modified_sso_user)
+SELECT (SELECT id FROM openchpl.report_metadata WHERE environment = 'STG' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard'), 
+        'chpl-admin',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata_role_map 
+		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'STG' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard') 
+		AND role_name = 'chpl-admin' 
+);
+
+INSERT INTO openchpl.report_metadata_role_map (report_metadata_id, role_name, last_modified_sso_user)
+SELECT (SELECT id FROM openchpl.report_metadata WHERE environment = 'PROD' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard'), 
+        'chpl-admin',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata_role_map 
+		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'PROD' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard') 
+		AND role_name = 'chpl-admin' 
+);
+
+INSERT INTO openchpl.report_metadata_role_map (report_metadata_id, role_name, last_modified_sso_user)
+SELECT (SELECT id FROM openchpl.report_metadata WHERE environment = 'DEV' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard'), 
+        'chpl-onc',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata_role_map 
+		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'DEV' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard') 
+		AND role_name = 'chpl-onc' 
+);
+
+INSERT INTO openchpl.report_metadata_role_map (report_metadata_id, role_name, last_modified_sso_user)
+SELECT (SELECT id FROM openchpl.report_metadata WHERE environment = 'QA' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard'), 
+        'chpl-onc',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata_role_map 
+		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'QA' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard') 
+		AND role_name = 'chpl-onc' 
+);
+
+INSERT INTO openchpl.report_metadata_role_map (report_metadata_id, role_name, last_modified_sso_user)
+SELECT (SELECT id FROM openchpl.report_metadata WHERE environment = 'STG' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard'), 
+        'chpl-onc',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata_role_map 
+		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'STG' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard') 
+		AND role_name = 'chpl-onc' 
+);
+
+INSERT INTO openchpl.report_metadata_role_map (report_metadata_id, role_name, last_modified_sso_user)
+SELECT (SELECT id FROM openchpl.report_metadata WHERE environment = 'PROD' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard'), 
+        'chpl-onc',
+        '6498c4f8-b0f1-70b5-55de-d84faae73402'
+WHERE NOT EXISTS (
+        SELECT * FROM openchpl.report_metadata_role_map 
+		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'PROD' AND report_key = 'DashboardUpdatedCriteriaStatus' AND report_group = 'onc-dashboard') 
+		AND role_name = 'chpl-onc' 
+);
