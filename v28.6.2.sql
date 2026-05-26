a6730ee5c6e183a553e372be0766f8bf4cb81810
Andrew Larned <andrew.larned@gmail.com>
2026-05-26 13:34:21 -0400

    release: deploy version 28.6.2 on 26 May 2026
    [#ver-28.6.2]

--- v28.6.2.sql
+++ v28.6.2.sql
@@ -1,3 +1,6 @@
+-- Deployment file for version 28.6.2
+--     as of 2026-05-26
+-- ./changes/ocd-5257.sql
 --
 -- Add the new Dashboard Important Dates report
 -- This will fall under the onc-dashboard group which we can use later to query only those charts
@@ -134,3 +137,8 @@ WHERE NOT EXISTS (
 		WHERE report_metadata_id = (SELECT id FROM openchpl.report_metadata WHERE environment = 'PROD' AND report_key = 'ImportantDates' AND report_group = 'onc-dashboard') 
 		AND role_name = 'chpl-onc' 
 );
+;
+insert into openchpl.data_model_version (version, deploy_date, last_modified_user) values ('28.6.2', '2026-05-26', -1);
+\i dev/openchpl_soft-delete.sql
+\i dev/openchpl_views.sql
+\i dev/openchpl_grant-all.sql
